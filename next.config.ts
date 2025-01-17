68e0fd9049ff4335f7177e4700c149d9689c4ec9
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-17 11:21:30 +0000

    Remove template redirects

--- next.config.ts
+++ next.config.ts
@@ -4,32 +4,6 @@ const nextConfig = {
   images: {
     formats: ['image/webp'],
   },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       has: [
-  //         {
-  //           type: 'host', // Redirect from www.
-  //           value: 'www.my-site.co.uk',
-  //         },
-  //       ],
-  //       destination: 'https://my-site.co.uk/:path*',
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: '/:path*',
-  //       has: [
-  //         {
-  //           type: 'host', // Redirect from Fly.io
-  //           value: 'my-site.fly.dev',
-  //         },
-  //       ],
-  //       destination: 'https://my-site.co.uk/:path*',
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
 }
 
 export default nextConfig
