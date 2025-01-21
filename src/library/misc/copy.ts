e055d080256c133eaa2ee5248021a3235f8d8e99
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-21 12:14:16 +0000

    Refactor to const enum

--- src/library/misc/copy.ts
+++ src/library/misc/copy.ts
@@ -1,5 +1,6 @@
-export enum siteDetails {
-  siteName = 'Simple Order',
-  siteNameWithTag = 'Simple Order - Wholesale Order Management',
-  metaDescription138 = 'A B2B order management website for wholesalers and their customers so you can reduce costs, save time, and eliminate phone call confusion.',
-}
+export const siteDetails = {
+  siteName: 'Simple Order',
+  siteNameWithTag: 'Simple Order - Wholesale Order Management',
+  metaDescription138:
+    'A B2B order management website for wholesalers and their customers so you can reduce costs, save time, and eliminate phone call confusion.',
+} as const
