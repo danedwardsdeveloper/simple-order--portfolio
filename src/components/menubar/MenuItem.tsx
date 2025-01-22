a13115a70c996f5e79e0174d482677bc2a838cb4
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-22 18:11:53 +0000

    Update MenuItem.tsx

--- src/components/menubar/MenuItem.tsx
+++ src/components/menubar/MenuItem.tsx
@@ -1,24 +1,16 @@
-'use client'
-
 import clsx from 'clsx'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-export interface MenuItemProps {
-  text: string
-  href: string
-}
-
-export default function MenuItem({ text, href }: MenuItemProps) {
+export default function MenuItem({ href, text }: { href: string; text: string }) {
   const pathname = usePathname()
-  const isActive = pathname === href || pathname?.endsWith(`${href}/`)
-
+  const isActive = pathname === href
   return (
     <Link
       href={href}
       className={clsx(
-        'inline-block underline-offset-2', //
-        isActive ? 'text-black cursor-default underline' : 'text-zinc-500 hover:underline',
+        'font-medium text-sm transition-colors duration-300',
+        isActive ? 'text-blue-400 cursor-default' : 'text-zinc-600 hover:text-blue-400 active:text-blue-500',
       )}
     >
       {text}
