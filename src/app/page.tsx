af91fe54fcf8ed226770f70b62c69319a2e71c2e
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-22 18:11:01 +0000

    Add AuthorisationCheck

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { AuthorisationCheck } from '@/components/AuthorisationCheck'
+import Dashboard from '@/components/Dashboard'
+import LandingPage from '@/components/LandingPage'
+
 export default function HomePage() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <AuthorisationCheck fallback={<LandingPage />}>
+      <Dashboard />
+    </AuthorisationCheck>
   )
 }
