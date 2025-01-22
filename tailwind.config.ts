b9813abd22eef12be39508cb846e1653ab7891e5
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-22 18:13:08 +0000

    Add theme extensions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,18 @@
-// import aspectRatio from '@tailwindcss/aspect-ratio'
 import { type Config } from 'tailwindcss'
+import { spacing, zIndex } from 'tailwindcss/defaultTheme'
 
 export default {
-  plugins: [
-    // aspectRatio
-  ],
   content: ['./src/**/*.{ts,tsx}'],
   darkMode: 'selector',
   theme: {
     extend: {
-      fontFamily: {
-        // sans: ['var(--font-readex-pro)'],
+      margin: {
+        'menubar-offset': spacing[20],
+      },
+      zIndex: {
+        menubar: zIndex[10],
+        'notification-container': zIndex[20],
+        notification: zIndex[30],
       },
     },
   },
