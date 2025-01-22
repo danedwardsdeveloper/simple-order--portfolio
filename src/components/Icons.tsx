e4fbde6cd7a3bf435590ace184ccef9beb287a6c
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-22 18:11:16 +0000

    Create Icons.tsx

---
+++ src/components/Icons.tsx
@@ -0,0 +1,29 @@
+export default function CompanyLogo() {
+  return (
+    <svg
+      viewBox="0 0 512 512"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-full h-full"
+      aria-hidden="true"
+    >
+      <g>
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="47"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M 46 419c0 26 21 47 47 47h326c26 0 47-21 47-47V93c0-26-21-47-47-47H93c-26 0-47 21-47 47z"
+        />
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="59"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M 121 270l108 108 162-244"
+        />
+      </g>
+    </svg>
+  )
+}
