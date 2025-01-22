05f8d1370fd840b214bfe872f375ab1e3a00cb5e
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-22 18:12:17 +0000

    Remove routing

--- src/components/menubar/RoleModeButton.tsx
+++ src/components/menubar/RoleModeButton.tsx
@@ -1,26 +1,25 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
-
 import { useUi } from '@/providers/ui'
 
 export default function RoleModeButton() {
   const { user, roleMode, toggleRoleMode } = useUi()
-  const router = useRouter()
-  const isMerchant = roleMode === 'merchant'
 
-  const displayText = isMerchant ? 'Switch to merchant mode' : 'Switch to customer mode'
-  const redirectDestination = isMerchant ? '/orders' : `${user?.merchantProfile?.slug}`
+  if (!user) return null
+
+  const isBothMerchantAndCustomer = user.role === 'both'
+
+  if (!isBothMerchantAndCustomer) return null
+
+  const displayText = roleMode === 'customer' ? 'Switch to merchant mode' : 'Switch to customer mode'
 
   function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault()
     toggleRoleMode()
-
-    router.push(redirectDestination)
   }
 
   return (
-    <button onClick={handleClick} className="text-zinc-500 hover:text-zinc-400 active:text-zinc-300">
+    <button onClick={handleClick} className="text-sm text-zinc-500 hover:text-zinc-400 active:text-zinc-300">
       {displayText}
     </button>
   )
