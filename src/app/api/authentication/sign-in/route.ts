e77209bee66a7bdd918116a9deb839d3fe5bcaed
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-23 19:09:09 +0000

    Implement cookie creation

--- src/app/api/authentication/sign-in/route.ts
+++ src/app/api/authentication/sign-in/route.ts
@@ -1,15 +1,13 @@
 import bcrypt from 'bcryptjs'
 import { NextRequest, NextResponse } from 'next/server'
 
-import { findUserByEmail } from '@/library/tempData/users'
+import { cookieDurations, createCookieWithToken, createSessionCookieWithToken } from '@/library/cookies'
+import { createSafeUser, findUserByEmail } from '@/library/tempData/users'
 
 import { authorisationMessages, basicMessages, HttpStatus, SignInPOSTbody, SignInPOSTresponse } from '@/types'
 
-export async function POST(
-  request: NextRequest,
-  response: NextResponse,
-): Promise<NextResponse<SignInPOSTresponse>> {
-  const { email, password }: SignInPOSTbody = await request.json()
+export async function POST(request: NextRequest): Promise<NextResponse<SignInPOSTresponse>> {
+  const { email, password, staySignedIn }: SignInPOSTbody = await request.json()
 
   if (!email || !password) {
     return NextResponse.json(
@@ -48,13 +46,21 @@ export async function POST(
     )
   }
 
-  return NextResponse.json(
+  const response = NextResponse.json(
     {
       message: basicMessages.success,
-      foundUser,
+      foundUser: createSafeUser(foundUser),
     },
     {
       status: HttpStatus.http200ok,
     },
   )
+
+  if (staySignedIn) {
+    response.cookies.set(createCookieWithToken(foundUser.id, cookieDurations.oneYear))
+  } else {
+    response.cookies.set(createSessionCookieWithToken(foundUser.id))
+  }
+
+  return response
 }
