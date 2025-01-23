a73a165b05480676283a7b77935acfdd1eddc5c7
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-23 19:08:55 +0000

    Move /[merchantSlug] to /merchant

--- src/app/[merchant-slug]/customers/components/InviteCustomerButton.tsx
+++
@@ -1,5 +0,0 @@
-import clsx from 'clsx'
-
-export default function InviteCustomerButton({ classes }: { classes?: string }) {
-  return <button className={clsx('px-2 bg-slate-200 rounded', classes)}>Invite customer</button>
-}
--- src/app/[merchant-slug]/settings/components/Status.tsx
+++
@@ -1,8 +0,0 @@
-export default function AccountStatus() {
-  return (
-    <div className="flex justify-between">
-      <span className="text-zinc-500">Membership type</span>
-      <span>Trial</span>
-    </div>
-  )
-}
