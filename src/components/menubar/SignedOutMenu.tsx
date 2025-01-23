a2f0224fe62e9381b90fe1eb89353e817d3ade3a
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-23 12:40:02 +0000

    Add subtle 3D effect to buttons

--- src/components/menubar/SignedOutMenu.tsx
+++ src/components/menubar/SignedOutMenu.tsx
@@ -15,13 +15,13 @@ export default function SignedOutMenu() {
         <div className="flex gap-x-2 items-center">
           <Link
             href="#"
-            className="bg-white text-zinc-600 hover:text-blue-400 active:text-blue-500 hover:bg-neutral-100 active:bg-neutral-200 rounded-lg px-2 py-1 font-medium text-sm border-2 border-blue-200 hover:border-blue-300 active:border-blue-400 transition-colors duration-300"
+            className="bg-white text-zinc-600 hover:text-blue-400 active:text-blue-500  rounded-lg px-2 py-1 font-medium text-sm border-2 border-blue-200 hover:border-blue-300 active:border-blue-400 transition-all duration-300 shadow active:shadow-none"
           >
             Learn more
           </Link>
           <Link
             href="#"
-            className="bg-blue-400 hover:bg-blue-400 active:bg-blue-500 text-white rounded-lg px-2 py-1 font-medium text-sm border-2 border-blue-400 hover:border-blue-500 active:border-blue-700 transition-colors duration-300"
+            className="bg-blue-500 hover:bg-blue-400 active:bg-blue-300 text-white rounded-lg px-2 py-1 font-medium text-sm border-2 border-blue-500 hover:border-blue-400 active:border-blue-300 transition-all duration-300 shadow active:shadow-none"
           >
             Start free trial
           </Link>
