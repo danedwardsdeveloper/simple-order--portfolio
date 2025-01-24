614f8339a29a837a2ccaced4862b11a078de4ad8
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-24 15:45:15 +0000

    Change 'Learn more' to 'How it works' to appease Lightohuse

--- src/components/menubar/SignedOutMenu.tsx
+++ src/components/menubar/SignedOutMenu.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 
+import { websiteCopy } from '@/library/misc/copy'
+
 import HomePageLink from './HomePageLink'
 import MenuContainer from './MenuContainer'
 import MenuItem from './MenuItem'
@@ -12,8 +14,12 @@ export default function SignedOutMenu() {
         <MenuItem href="/articles" text="Articles" />
         <MenuItem href="/sign-in" text="Sign in" />
         <div className="flex gap-x-2 items-center">
-          <Link href="/articles/how-it-works" className="button-cta-secondary">
-            Learn more
+          <Link
+            href="/articles/how-it-works"
+            title={websiteCopy.linkDescriptions.howItWorks}
+            className="button-cta-secondary"
+          >
+            How it works
           </Link>
           <Link href="/free-trial" className="button-cta-primary">
             Start free trial
