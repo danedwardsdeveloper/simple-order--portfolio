941a234d05be9e2284af827fb98eef2d8a77dc79
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-28 11:42:30 +0000

    Add padding for small screens

--- src/components/menubar/MenuContainer.tsx
+++ src/components/menubar/MenuContainer.tsx
@@ -1,6 +1,6 @@
 export default function MenuContainer({ children }: { children: React.ReactNode }) {
   return (
-    <nav className="fixed inset-x-0 top-0 flex h-14 bg-white/70 backdrop-blur border-b-2 border-neutral-100 z-menubar">
+    <nav className="fixed inset-x-0 top-0 flex h-14 bg-white/70 backdrop-blur border-b-2 border-neutral-100 z-menubar px-4 lg:px-0">
       <div className="w-full max-w-4xl mx-auto flex items-center justify-between">{children}</div>
     </nav>
   )
