c8aa1b10b5e1acb59b1ced48bc4bb33555a4ed1c
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-28 12:43:05 +0000

    Remove temporary data

--- src/types/definitions/users.ts
+++ src/types/definitions/users.ts
@@ -2,14 +2,13 @@ import { customerToMerchant, merchantProfiles, users } from '@/library/database/
 
 export type User = typeof users.$inferSelect
 export type NewUser = Required<Omit<typeof users.$inferInsert, 'id'>>
+export type SafeUser = Omit<User, 'hashedPassword'>
 
 export type MerchantProfile = typeof merchantProfiles.$inferSelect
 export type NewMerchantProfile = typeof merchantProfiles.$inferInsert
 
 export type customerToMerchant = typeof customerToMerchant.$inferSelect
 export type NewCustomerToMerchant = typeof customerToMerchant.$inferInsert
-
-export type SafeUser = Omit<User, 'hashedPassword'>
 
 export interface RelationshipItem {
   id: number
@@ -33,40 +32,4 @@ export interface ClientSafeUser {
   emailConfirmed: boolean
   merchantDetails?: ClientMerchantDetails
   merchantsAsCustomer?: RelationshipItem[]
-}
-
-export const exampleClientUserObject: ClientSafeUser = {
-  id: 10,
-  firstName: 'name',
-  lastName: 'lastName',
-  email: 'mybusiness@gmail.com',
-  businessName: 'My Business',
-  emailConfirmed: false,
-  merchantDetails: {
-    slug: 'my-business',
-    freeTrial: {
-      endDate: new Date('2025-01-28T09:59:35.000Z'),
-    },
-    customersAsMerchant: [],
-  },
-  merchantsAsCustomer: [],
-}
-
-export const currentResponse = {
-  message: 'success',
-  safeNewUser: {
-    id: 10,
-    firstName: 'name',
-    lastName: 'lastName',
-    email: 'wedrfgs@gmail.com',
-    businessName: 'My Business',
-    emailConfirmed: false,
-  },
-  newMerchant: { id: 10, userId: 10, slug: 'my-business' },
-  newFreeTrial: {
-    id: 10,
-    merchantProfileId: 10,
-    startDate: '2025-01-28T09:59:35.000Z',
-    endDate: '2025-01-28T09:59:35.000Z',
-  },
 }
