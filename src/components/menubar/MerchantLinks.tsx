debdc0444e0a86d8f0048a100a61bf54d7a28775
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-28 11:42:22 +0000

    Update MerchantLinks.tsx

--- src/components/menubar/MerchantLinks.tsx
+++ src/components/menubar/MerchantLinks.tsx
@@ -1,32 +1,12 @@
 'use client'
 
-import { findUserById } from '@/library/tempData/tempUsers'
-
 import MenuItem from './MenuItem'
 import { useUi } from '@/providers/ui'
 
 export default function MerchantLinks() {
   const { user } = useUi()
-  const isCustomer = user?.role === 'customer' || user?.role === 'both'
 
-  if (!user || !isCustomer || !user.merchantIds) return null
+  if (!user) return null
 
-  if (user.merchantIds.length !== 1) {
-    return <MenuItem href="/merchants" text="Merchants" />
-  }
-
-  return (
-    <>
-      {user.merchantIds.map(merchantId => {
-        const merchantDetails = findUserById(merchantId)
-        return (
-          <MenuItem
-            key={merchantId}
-            href={`/merchant/${merchantDetails?.id}`}
-            text={merchantDetails?.merchantProfile?.businessName || ''}
-          />
-        )
-      })}
-    </>
-  )
+  return <MenuItem href="/merchants" text="Merchants" />
 }
