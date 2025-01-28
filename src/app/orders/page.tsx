4f9404dfc207e0fed809b1cafeabb951484f6ea6
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-28 11:43:28 +0000

    Simplify page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,27 +1,7 @@
-'use client'
-
-import { useUi } from '@/providers/ui'
-
-export default function CustomersOnlyPage() {
-  const { user, merchantMode } = useUi()
-  const displayName = user?.businessNameAsCustomer ? user?.businessNameAsCustomer : user?.firstName
-
-  function DualModeMessage() {
-    if (user?.role !== 'both') return null
-    if (merchantMode) {
-      return <p>{`Orders received as a merchant`}</p>
-    } else {
-      return <p>{`Orders created as a customer`}</p>
-    }
-  }
-
+export default function Orders() {
   return (
     <>
       <h1>Orders</h1>
-      <div className="flex flex-col gap-y-4">
-        <DualModeMessage />
-        <p>{`Welcome ${displayName}`}</p>
-      </div>
     </>
   )
 }
