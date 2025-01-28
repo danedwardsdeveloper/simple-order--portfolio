66a8533ac4280349aa9c1384c628035800f15e1c
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-28 11:36:19 +0000

    Render business name

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,14 @@
+'use client'
+
+import { useUi } from '@/providers/ui'
+
 export default function DashboardPage() {
+  const { user } = useUi()
   return (
     <>
       <div>
         <h1>Dashboard</h1>
+        {user && <p>{`Welcome ${user?.businessName}`}</p>}
       </div>
     </>
   )
