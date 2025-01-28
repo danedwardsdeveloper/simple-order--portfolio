cb935b77425ba45918d122749ae8bcafa558cd7d
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-28 11:39:09 +0000

    Rename and move file

---
+++ src/library/websiteCopy.ts
@@ -0,0 +1,18 @@
+export const websiteCopy = {
+  metadata: {
+    titles: {
+      home41: 'Simple Order | Wholesale order management',
+    },
+    descriptions: {
+      home138:
+        'A B2B order management website for wholesalers and their customers so you can reduce costs, save time, and eliminate phone call confusion.',
+    },
+  },
+  extras: {
+    siteNameWithTag: 'Simple Order | Wholesale Order Management',
+    selfContainedDescription: `Simple Order is an order management website for wholesalers and their customers so you can reduce costs, save time, and eliminate phone call confusion.`,
+  },
+  linkDescriptions: {
+    howItWorks: `Learn more about our B2B order management website`,
+  },
+} as const
