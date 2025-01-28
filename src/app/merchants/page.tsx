ca404037215bf5808d63f53a484f6cb6db9a7614
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-28 11:43:02 +0000

    Update page.tsx

--- src/app/merchants/page.tsx
+++ src/app/merchants/page.tsx
@@ -2,32 +2,38 @@
 
 import Link from 'next/link'
 
-import { findUserById } from '@/library/tempData/tempUsers'
-
 import { useUi } from '@/providers/ui'
 
 export default function Page() {
   const { user } = useUi()
   if (!user) return null
-  const merchants = user.merchantIds?.map(id => findUserById(id)?.merchantProfile) || []
-  if (!merchants || merchants.length === 0) return null
+  const merchants = user.merchantsAsCustomer?.map(id => id) || []
+
+  function MerchantsList() {
+    if (!merchants || merchants.length === 0) {
+      return <span>No merchants found</span>
+    }
+
+    return (
+      <>
+        {merchants.map((merchant, index) => {
+          return (
+            <span
+              key={index}
+              className="block transition-colors duration-300 text-zinc-600 hover:text-blue-400 active:text-blue-500"
+            >
+              {merchant?.businessName}
+            </span>
+          )
+        })}
+      </>
+    )
+  }
 
   return (
     <>
       <h1>Merchants</h1>
-      <div>
-        {merchants.map((merchant, index) => {
-          return (
-            <Link
-              key={index}
-              href={`/merchants/${merchant?.slug}`}
-              className="block transition-colors duration-300 text-zinc-600 hover:text-blue-400 active:text-blue-500"
-            >
-              {merchant?.businessName}
-            </Link>
-          )
-        })}
-      </div>
+      <div>{<MerchantsList />}</div>
     </>
   )
 }
