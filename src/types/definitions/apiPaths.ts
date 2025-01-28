beeb953bfdb9a9350fb5490556fa5ec8852c93eb
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-28 11:38:51 +0000

    Add 2x routes

--- src/types/definitions/apiPaths.ts
+++ src/types/definitions/apiPaths.ts
@@ -1,5 +1,7 @@
 export const apiPaths = {
   authentication: {
     signIn: '/api/authentication/sign-in',
+    createAccount: '/api/authentication/create-account',
+    signOut: '/api/authentication/sign-out',
   },
 } as const
