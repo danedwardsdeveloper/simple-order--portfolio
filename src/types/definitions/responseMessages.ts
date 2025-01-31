281dc92b48d9145bd8e9e207dd31402dcf75a94c
Dan Edwards <daniel.edwards96@yahoo.com>
2025-01-31 12:28:10 +0000

    Sort alphabetically, add additional messages

--- src/types/definitions/responseMessages.ts
+++ src/types/definitions/responseMessages.ts
@@ -1,21 +1,32 @@
 export const basicMessages = {
-  success: 'success',
-  serverError: 'server error',
-  parametersMissing: 'parameters missing',
   databaseError: 'database error',
+  parametersMissing: 'parameters missing',
+  serverError: 'server error',
+  success: 'success',
 } as const
 
 export type BasicMessages = (typeof basicMessages)[keyof typeof basicMessages]
 
 export const authenticationMessages = {
-  invalidCredentials: 'invalid credentials',
-  tokenMissing: 'token missing',
-  tokenInvalid: 'token invalid',
-  tokenExpired: 'token expired',
-  userNotFound: 'user not found',
-  emailNotConfirmed: 'email not confirmed',
-  unauthorised: 'unauthorised',
+  alreadyConfirmed: 'already confirmed',
   authorisationError: 'authorisation error',
+  businessNameMissing: 'businessName missing',
+  businessNameTaken: 'businessName taken',
+  confirmationTokenMissing: 'confirmation token missing',
+  emailMissing: 'email missing',
+  emailNotConfirmed: 'email not confirmed',
+  emailTaken: 'email taken',
+  fistNameMissing: 'firstName missing',
+  invalidCredentials: 'invalid credentials',
+  lastNameMissing: 'lastName missing',
+  passwordMissing: 'password missing',
+  slugTaken: 'slug taken',
+  tokenExpired: 'token expired',
+  tokenInvalid: 'token invalid',
+  tokenMissing: 'token missing',
+  tokenUsed: 'token used',
+  unauthorised: 'unauthorised',
+  userNotFound: 'user not found',
 } as const
 
 export type AuthenticationMessages = (typeof authenticationMessages)[keyof typeof authenticationMessages]
