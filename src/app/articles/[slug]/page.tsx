b4fd861daee6910392d8464cb7e8ab9560c2a819
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-01 12:31:59 +0000

    Await params, add BreadCrumbs

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,16 +1,20 @@
+import BreadCrumbs from '../BreadCrumbs'
 import { articlesData } from '../data'
 
 export function generateStaticParams() {
   return Object.keys(articlesData).map(slug => ({ slug }))
 }
 
-export default function Page({ params }: { params: { slug: string } }) {
-  const article = articlesData[params.slug]
+export default async function Page({ params }: { params: { slug: string } }) {
+  // await is necessary here, despite what VS Code says!
+  const { slug } = await params
+  const article = articlesData[slug]
 
   if (!article) return null
 
   return (
     <>
+      <BreadCrumbs title={article.displayTitle} />
       <h1>{article.displayTitle}</h1>
       <div className="flex flex-col max-w-prose gap-y-4">
         {article.content.map((paragraph, index) => (
