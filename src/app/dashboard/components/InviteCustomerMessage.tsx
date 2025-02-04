921c461452168cb63435168d59b2dc953512a202
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-04 20:29:03 +0000

    Create InviteCustomerMessage.tsx

---
+++ src/app/dashboard/components/InviteCustomerMessage.tsx
@@ -0,0 +1,16 @@
+import { useAuthorisation } from '@/providers/authorisation'
+
+export default function InviteCustomerMessage() {
+  const { clientSafeUser } = useAuthorisation()
+  if (!clientSafeUser) return null
+  if (clientSafeUser.merchantDetails?.customersAsMerchant) return null
+
+  return (
+    <div className="max-w-prose p-3 my-4 border-2 rounded-xl border-blue-300 ">
+      <form action="">
+        <label htmlFor="">Invite a customer</label>
+        <input type="text" />
+      </form>
+    </div>
+  )
+}
