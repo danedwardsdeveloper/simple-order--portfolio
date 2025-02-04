4d5ac0f2ef0cc1cf41754cc88f95b0b5f688efb8
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-04 20:29:00 +0000

    Create EmptyInventoryLink.tsx

---
+++ src/app/dashboard/components/EmptyInventoryLink.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import Link from 'next/link'
+
+import { useAuthorisation } from '@/providers/authorisation'
+
+export default function EmptyInventoryMessage() {
+  const { clientSafeUser } = useAuthorisation()
+
+  if (!clientSafeUser || clientSafeUser.inventory) return null
+
+  return (
+    <div className="max-w-prose p-3 my-4 border-2 rounded-xl border-blue-300 ">
+      <Link href="/inventory" className="text-blue-500 hover:text-blue-600 active:text-blue-700 transition-colors duration-300">
+        Add items to your inventory
+      </Link>
+    </div>
+  )
+}
