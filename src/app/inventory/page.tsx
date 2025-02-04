7331f9e79e06575e829ac583e623f7fd9e143cb7
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-04 20:29:44 +0000

    Add components

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,9 +1,14 @@
+import AddInventoryForm from './components/AddInventoryForm'
+import InventoryControlPanel from './components/InventoryControlPanel'
+import InventoryList from './components/InventoryList'
+
 export default function InventoryPage() {
   return (
-    <>
-      <div className="">
-        <h1>Inventory page</h1>
-      </div>
-    </>
+    <div className="flex flex-col gap-y-4">
+      <h1>Inventory</h1>
+      <InventoryControlPanel />
+      <InventoryList />
+      <AddInventoryForm />
+    </div>
   )
 }
