156bdc268e02078936b1b3904434f906b33ad102
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-04 20:28:21 +0000

    Return null if !show

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -4,13 +4,17 @@ import CompanyLogo from './Icons'
 import Spinner from './Spinner'
 
 export default function SplashScreen({ show }: { show: boolean }) {
+  if (!show) {
+    return null
+  }
+
+  // ToDo: animate and then remove completely!
   return (
-    // ToDo: Abstract the z-index
     <div
       data-component="SplashScreen"
       className={clsx(
-        'fixed inset-0 z-50 bg-white flex items-center justify-center transition-opacity duration-500 pointer-events-none',
-        show ? 'opacity-100' : 'opacity-0',
+        'fixed inset-0 z-splash-screen bg-white flex items-center justify-center transition-opacity duration-500 pointer-events-none',
+        // show ? 'opacity-100' : 'opacity-0',
       )}>
       <div className="flex items-center gap-x-4 h-12 text-blue-600">
         <div className="size-6 text-blue-600">
