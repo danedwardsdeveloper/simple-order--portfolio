4a7b424ad0a03a62c7cffd39e217733ac36f845a
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-04 20:24:09 +0000

    Create database.ts

---
+++ src/types/definitions/database.ts
@@ -0,0 +1,4 @@
+import { drizzle } from 'drizzle-orm/node-postgres'
+
+type DrizzleClient = ReturnType<typeof drizzle>
+export type DrizzleTransaction = Parameters<Parameters<DrizzleClient['transaction']>[0]>[0]
