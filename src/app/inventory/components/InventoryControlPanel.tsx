464f84746879b277072c4fdb17f3032b6a5bc1c8
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-04 20:29:11 +0000

    Create InventoryControlPanel.tsx

---
+++ src/app/inventory/components/InventoryControlPanel.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import ToggleWithLabel from '@/components/ToggleWithLabel'
+
+import { useUi } from '@/providers/ui'
+
+export default function InventoryControlPanel() {
+  const { includeVat, toggleIncludeVat } = useUi()
+  return (
+    <div className="flex border-2 border-blue-200 p-3 rounded-xl max-w-xl -mx-3">
+      <ToggleWithLabel enabled={includeVat} setEnabled={toggleIncludeVat} label="Include VAT" />
+    </div>
+  )
+}
