a47d19af910a73f2a6cf0a3a2efcbb37e2969319
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-04 20:29:33 +0000

    Add padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body>
         <Providers>
           <MenuBar />
-          <div className="max-w-4xl w-full mx-auto mt-menubar-offset px-4 lg:px-0 pt-8">{children}</div>
+          <div className="max-w-4xl w-full mx-auto mt-menubar-offset px-4 lg:px-0 pt-8 pb-60">{children}</div>
         </Providers>
       </body>
     </html>
