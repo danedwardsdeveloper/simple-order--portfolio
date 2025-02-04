75556ec5a1f8c37e989c5bac02a6ee1f374bd36b
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-04 20:26:27 +0000

    Create NotificationContainer.tsx

---
+++ src/components/notifications/NotificationContainer.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import clsx from 'clsx'
+
+import NotificationItem from './NotificationItem'
+import { useNotifications } from '@/providers/notifications'
+
+export default function NotificationsContainer() {
+  const { notifications, removeNotification } = useNotifications()
+
+  return (
+    <div
+      data-component="NotificationsContainer"
+      aria-live="assertive"
+      className={clsx(
+        'pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6',
+        'mt-menu-bar z-notifications',
+      )}>
+      <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
+        {notifications &&
+          notifications.map(notification => (
+            <NotificationItem
+              key={notification.id}
+              notification={notification}
+              onClose={removeNotification}
+            />
+          ))}
+      </div>
+    </div>
+  )
+}
