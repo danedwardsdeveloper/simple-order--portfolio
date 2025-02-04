7376cd24e45cffb57da7bae2e4445dac2d74797c
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-04 20:21:43 +0000

    Create sendEmail.ts

---
+++ src/library/email/sendEmail.ts
@@ -0,0 +1,48 @@
+import { MailgunMessageData } from 'mailgun.js'
+
+import { myPersonalEmail } from '@/library/environment/serverVariables'
+import logger from '@/library/logger'
+
+import emailClient from './client'
+
+export interface SendEmailBody {
+  to?: string
+  subject: string
+  textVersion: string
+  htmlVersion: string
+}
+
+interface SendEmailResponse {
+  success: boolean
+}
+
+export const sendEmail = async ({ to = myPersonalEmail, subject, htmlVersion, textVersion }: SendEmailBody): Promise<SendEmailResponse> => {
+  const messageData: MailgunMessageData = {
+    from: 'Simple Order <noreply@simpleorder.co.uk>',
+    to,
+    subject,
+    text: textVersion,
+    html: htmlVersion,
+  }
+  const response = await emailClient.messages.create('simpleorder.co.uk', messageData)
+
+  if (response.status === 200) {
+    logger.info(`Sent email to ${to}`)
+    return { success: true }
+  } else {
+    logger.errorUnknown(response.message, 'Error sending email')
+    return { success: false }
+  }
+}
+
+// sendEmail({
+//   to: myPersonalEmail,
+//   ...createNewMerchantEmail({
+//     recipientName: 'Dan',
+//     confirmationURL: 'www.google.com',
+//   }),
+// })
+
+/* 
+pnpm tsx src/library/email/sendEmail
+*/
