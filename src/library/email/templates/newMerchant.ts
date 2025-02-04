975cb4493d4393396ab9dee48124466f871de5c7
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-04 20:21:39 +0000

    Create newMerchant.ts

---
+++ src/library/email/templates/newMerchant.ts
@@ -0,0 +1,45 @@
+import { createParagraph } from '../utilities'
+
+interface NewMerchantEmailResponse {
+  subject: string
+  htmlVersion: string
+  textVersion: string
+}
+
+interface NewMerchantEmailProps {
+  recipientName: string
+  confirmationURL: string
+}
+
+export function createNewMerchantEmail({
+  recipientName,
+  confirmationURL,
+}: NewMerchantEmailProps): NewMerchantEmailResponse {
+  const greeting = `Hello ${recipientName}`
+  const intro = 'Thank you for choosing Simple Order. Please confirm your email by clicking the link below.'
+  const link = `<a href="${confirmationURL}">Confirm your email</a>`
+  const securityMessage = `You can safely ignore this email if you didn't register for an account. This link will expire in 24 hours.`
+  const thankYou = 'Many thanks'
+  const companyName = 'Accounts team, Simple Order'
+
+  const htmlVersion = `
+  ${createParagraph(greeting)}
+  ${createParagraph(intro)}
+  ${link}
+  ${createParagraph(securityMessage)}
+  ${createParagraph(thankYou)}
+  ${createParagraph(companyName, 'semibold')}`
+
+  const textVersion = `${greeting}\n\n
+${intro}\n\n
+${confirmationURL}\n\n
+  ${createParagraph(securityMessage)}\n\n
+  ${createParagraph(thankYou)}\n\n
+  ${createParagraph(companyName, 'semibold')}`
+
+  return {
+    subject: 'Confirm your email',
+    htmlVersion,
+    textVersion,
+  }
+}
