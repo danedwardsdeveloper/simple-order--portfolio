cf82c5b9af82a7d6157b78e29779ec9b3f1ca8ce
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-04 20:24:33 +0000

    Add VAT toggle

--- src/providers/ui.tsx
+++ src/providers/ui.tsx
@@ -6,21 +6,32 @@ interface UiContextType {
   merchantMode: boolean
   setMerchantMode: (value: boolean) => void
   toggleMerchantMode: () => void
+  includeVat: boolean
+  setIncludeVat: (value: boolean) => void
+  toggleIncludeVat: () => void
 }
 
 const UiContext = createContext<UiContextType | undefined>(undefined)
 
 export function UiProvider({ children }: { children: React.ReactNode }) {
-  const [merchantMode, setMerchantMode] = useState(false)
+  const [merchantMode, setMerchantMode] = useState(true)
 
   const toggleMerchantMode = () => {
     setMerchantMode(current => !current)
+  }
+
+  const [includeVat, setIncludeVat] = useState(false)
+  const toggleIncludeVat = () => {
+    setIncludeVat(current => !current)
   }
 
   const value = {
     merchantMode,
     setMerchantMode,
     toggleMerchantMode,
+    includeVat,
+    setIncludeVat,
+    toggleIncludeVat,
   }
 
   return <UiContext.Provider value={value}>{children}</UiContext.Provider>
@@ -28,8 +39,6 @@ export function UiProvider({ children }: { children: React.ReactNode }) {
 
 export function useUi() {
   const context = useContext(UiContext)
-  if (context === undefined) {
-    throw new Error('useUi must be used within a UiProvider')
-  }
+  if (context === undefined) throw new Error('useUi must be used within a UiProvider')
   return context
 }
