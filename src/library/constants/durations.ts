005111b8f3fa1b208cd9754803c9702de1199547
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-08 15:58:39 +0000

    Refactor more expressively

--- src/library/constants/durations.ts
+++ src/library/constants/durations.ts
@@ -1,5 +1,21 @@
 export const durationOptions = {
-  oneYearInSeconds: 365 * 24 * 60 * 60, // Cookie when stay signed in
-  twentyFourHoursInMilliseconds: 24 * 60 * 60 * 1000, // Confirm email (new merchant account) expiry
-  oneWeekInMilliseconds: 7 * 24 * 60 * 60 * 1000, // Confirm invitation
+  oneYearInSeconds: 365 * 24 * 60 * 60,
+  twentyFourHoursInMilliseconds: 24 * 60 * 60 * 1000,
+  oneWeekInMilliseconds: 7 * 24 * 60 * 60 * 1000,
 } as const
+
+export const durationSettings = {
+  staySignedInCookie: durationOptions.oneYearInSeconds,
+  confirmEmailExpiry: durationOptions.twentyFourHoursInMilliseconds,
+  confirmInvitationExpiry: durationOptions.oneWeekInMilliseconds,
+} as const
+
+/*
+// Increasing email resend intervals
+- Send a maximum of 5 emails
+  - Send first instantly
+  - Second after 15 minutes
+  - third after another 15 minutes
+  - fourth after another hour
+  - final after another 24 hours
+*/
