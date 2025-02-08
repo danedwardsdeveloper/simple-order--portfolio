3f71167b5ed3f94350df4cd9ff92c480f0c0bbe4
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-08 15:56:26 +0000

    Formatting

--- src/library/logger.ts
+++ src/library/logger.ts
@@ -6,20 +6,12 @@ import { isProduction } from './environment/publicVariables'
 const voidCallback = (): void => {}
 
 const logger = {
-  debug: isProduction
-    ? voidCallback
-    : (...args: unknown[]): void => console.debug(chalk.magenta('[DEBUG]', ...args)),
-  info: isProduction
-    ? voidCallback
-    : (...args: unknown[]): void => console.info(chalk.blue('[INFO]', ...args)),
-  warn: isProduction
-    ? voidCallback
-    : (...args: unknown[]): void => console.warn(chalk.yellow('[WARN]', ...args)),
+  debug: isProduction ? voidCallback : (...args: unknown[]): void => console.debug(chalk.magenta('[DEBUG]', ...args)),
+  info: isProduction ? voidCallback : (...args: unknown[]): void => console.info(chalk.blue('[INFO]', ...args)),
+  warn: isProduction ? voidCallback : (...args: unknown[]): void => console.warn(chalk.yellow('[WARN]', ...args)),
   error: (...args: unknown[]): void => console.error(chalk.red('[ERROR]'), ...args),
   errorUnknown: (error: unknown, label: string = 'Unknown error: '): void =>
-    console.error(
-      chalk.red('[ERROR]', label, error instanceof Error ? error.message : JSON.stringify(error)),
-    ),
+    console.error(chalk.red('[ERROR]', label, error instanceof Error ? error.message : JSON.stringify(error))),
 } as const
 
 export default logger
