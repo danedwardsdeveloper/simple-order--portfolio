b944587f79f7e14e1a508a1a5eab87642b038076
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-08 15:56:49 +0000

    Create subscription.ts

---
+++ src/types/definitions/subscription.ts
@@ -0,0 +1,4 @@
+import { subscriptions } from '@/library/database/schema'
+
+export type Subscription = typeof subscriptions.$inferSelect
+export type NewSubscription = typeof subscriptions.$inferInsert
