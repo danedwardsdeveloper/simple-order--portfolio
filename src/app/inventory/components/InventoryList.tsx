ffd5970c1bcd4a8720928abb6e1e1ff105e006c8
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-08 15:59:34 +0000

    Remove temporaryProducts

--- src/app/inventory/components/InventoryList.tsx
+++ src/app/inventory/components/InventoryList.tsx
@@ -1,13 +1,16 @@
 'use client'
 
-import { temporaryProducts } from '@/library/database/schema'
-
 import InventoryCard from './InventoryCard'
+import { useAuthorisation } from '@/providers/authorisation'
 
 export default function InventoryList() {
+  const { clientSafeUser } = useAuthorisation()
+
+  if (!clientSafeUser?.inventory) return null
+
   return (
     <ul className="flex flex-col w-full gap-y-4 max-w-xl -mx-3">
-      {temporaryProducts.map((product, index) => (
+      {clientSafeUser.inventory.map((product, index) => (
         <InventoryCard key={product.id} product={product} zebraStripe={Boolean(index % 2)} />
       ))}
     </ul>
