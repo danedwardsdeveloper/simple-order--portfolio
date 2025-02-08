387d1c21ebf21590dc5807a3bfaeecd86624f705
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-08 15:55:00 +0000

    Delete createSafeUser.ts

--- src/library/utilities/definitions/createSafeUser.ts
+++
@@ -1,6 +0,0 @@
-import { SafeUser, User } from '@/types/definitions/users'
-
-export function createSafeUser(user: User): SafeUser {
-  const { hashedPassword, ...safeUser } = user
-  return safeUser
-}
