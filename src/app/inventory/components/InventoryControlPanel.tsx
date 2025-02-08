15ba9ecf532a99c31c153d8f1571cedb502cd71e
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-08 15:59:57 +0000

    Add second toggle label

--- src/app/inventory/components/InventoryControlPanel.tsx
+++ src/app/inventory/components/InventoryControlPanel.tsx
@@ -8,7 +8,7 @@ export default function InventoryControlPanel() {
   const { includeVat, toggleIncludeVat } = useUi()
   return (
     <div className="flex border-2 border-blue-200 p-3 rounded-xl max-w-xl -mx-3">
-      <ToggleWithLabel enabled={includeVat} setEnabled={toggleIncludeVat} label="Include VAT" />
+      <ToggleWithLabel enabled={includeVat} setEnabled={toggleIncludeVat} enabledLabel="Show VAT" disabledLabel="Hide VAT" />
     </div>
   )
 }
