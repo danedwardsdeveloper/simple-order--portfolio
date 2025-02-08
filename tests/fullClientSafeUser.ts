3b6e83e925b93cb9cbd9871d61cbcf386554d3fa
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-08 15:54:49 +0000

    Create temporary file

---
+++ tests/fullClientSafeUser.ts
@@ -0,0 +1,42 @@
+import { FullClientSafeUser } from '@/types'
+
+// Work in progress
+// I have no idea what data I actually need!
+
+export const fullClientSafeUser = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  businessName: '',
+  emailConfirmed: false,
+  merchantDetails: {
+    slug: '', // Not sure this is needed
+    freeTrialExpiry: new Date(), // Not sure I need this!
+    customersAsMerchant: [],
+  },
+  merchantsAsCustomer: [
+    {
+      id: 17, // Probably don't use the ID
+      // Slug maybe?
+      businessName: `Jane's Bakery`,
+      emailConfirmed: true,
+    },
+  ],
+  inventory: [
+    {
+      id: 0, // Use the id as it's the only guaranteed unique property (for the map key)
+      name: '',
+      description: null,
+      priceInMinorUnits: 0,
+      customVat: null,
+    },
+  ],
+}
+
+/*
+Data I actually need to display
+- Free trial end date/ time remaining
+- 
+
+
+*/
