ecc68edaee2478b85736834b9406c4106b70a3a8
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-08 16:01:14 +0000

    Remove early return statement

--- src/app/dashboard/components/InviteCustomerMessage.tsx
+++ src/app/dashboard/components/InviteCustomerMessage.tsx
@@ -3,7 +3,7 @@ import { useAuthorisation } from '@/providers/authorisation'
 export default function InviteCustomerMessage() {
   const { clientSafeUser } = useAuthorisation()
   if (!clientSafeUser) return null
-  if (clientSafeUser.merchantDetails?.customersAsMerchant) return null
+  // if (clientSafeUser.merchantDetails?.customersAsMerchant) return null
 
   return (
     <div className="max-w-prose p-3 my-4 border-2 rounded-xl border-blue-300 ">
