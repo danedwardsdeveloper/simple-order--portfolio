6213d66e8c372676dca1a8894ac3a139ab1af1c6
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-10 18:32:58 +0000

    Extend slow test threshold

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     environment: 'node',
     include: ['tests/**/*.test.ts'],
     exclude: ['**/node_modules/**', '**/.next/**', '**/.drizzle/**', '**/.misc/**', '**/.public/**'],
+    slowTestThreshold: 1000, // milliseconds
   },
   resolve: {
     alias: {
