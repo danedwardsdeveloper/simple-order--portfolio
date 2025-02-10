2253be45f6cf09948bf592da07c55f88702b2f25
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-10 18:33:41 +0000

    Create badCreateAccount.test.ts

---
+++ tests/badCreateAccount.test.ts
@@ -0,0 +1,57 @@
+import { Browser, launch, Page } from 'puppeteer'
+import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
+
+import { dataTestIdNames } from '@/library/constants/dataTestId'
+import { testPasswords, testUsers } from '@/library/constants/testUsers'
+import { developmentBaseURL } from '@/library/environment/publicVariables'
+
+import { deleteUserSequence } from './utilities/deleteUserSequence'
+import { getElements } from './utilities/getElements'
+
+const bothUser = testUsers.both
+
+describe('Create Account Form', () => {
+  let browser: Browser
+  let page: Page
+
+  beforeAll(async () => {
+    deleteUserSequence(bothUser.email)
+    browser = await launch()
+    page = await browser.newPage()
+    getElements.initialise(page)
+    await page.goto(`${developmentBaseURL}/free-trial`)
+  })
+
+  afterAll(async () => {
+    deleteUserSequence(bothUser.email)
+    if (page && !page.isClosed()) {
+      await page.close()
+    }
+    if (browser) {
+      await browser.close()
+    }
+  })
+
+  test('can fill out and submit the form', async () => {
+    const firstNameInput = await getElements.byTestId(dataTestIdNames.createAccountFirstNameInput)
+    const lastNameInput = await getElements.byTestId(dataTestIdNames.createAccountLastNameInput)
+    const businessNameInput = await getElements.byTestId(dataTestIdNames.createAccountBusinessNameInput)
+    const emailInput = await getElements.byTestId(dataTestIdNames.createAccountEmailInput)
+    const passwordInput = await getElements.byTestId(dataTestIdNames.createAccountPasswordInput)
+    const staySignedInCheckbox = await getElements.byTestId(dataTestIdNames.createAccountStaySignedInCheckbox)
+    const submitButton = await getElements.byTestId(dataTestIdNames.createAccountSubmitButton)
+
+    await firstNameInput?.type(bothUser.firstName)
+    await lastNameInput?.type(bothUser.lastName)
+    await businessNameInput?.type(bothUser.businessName)
+    await emailInput?.type(bothUser.email)
+    await staySignedInCheckbox?.click()
+    await passwordInput?.type(testPasswords.illegalCharacters)
+    await staySignedInCheckbox?.click()
+    await submitButton?.click()
+  })
+
+  it('should not redirect to /dashboard', async () => {
+    expect(page.url()).toBe(`${developmentBaseURL}/free-trial`)
+  })
+})
