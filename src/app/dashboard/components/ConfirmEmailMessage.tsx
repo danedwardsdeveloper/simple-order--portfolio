a7b62b4e2a8f21a67b30bf47e44e65cee72ac0b8
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-10 18:37:25 +0000

    Add data-test-id

--- src/app/dashboard/components/ConfirmEmailMessage.tsx
+++ src/app/dashboard/components/ConfirmEmailMessage.tsx
@@ -1,3 +1,5 @@
+import { dataTestIdNames } from '@/library/constants/dataTestId'
+
 import { useAuthorisation } from '@/providers/authorisation'
 
 export default function ConfirmEmailMessage() {
@@ -6,8 +8,8 @@ export default function ConfirmEmailMessage() {
   if (!clientSafeUser || clientSafeUser.emailConfirmed) return null
 
   return (
-    <div className="max-w-prose p-3 my-4 border-2 rounded-xl border-orange-300 ">
-      <p>{`Please confirm your email by clicking the link in the email sent to ${clientSafeUser.email}. Remember to check your junk folder.`}</p>
-    </div>
+    <p
+      data-test-id={dataTestIdNames.pleaseConfirmYourEmailMessage}
+      className="max-w-prose p-3 my-4 border-2 rounded-xl border-orange-300 ">{`Please confirm your email by clicking the link in the email sent to ${clientSafeUser.email}. Remember to check your junk folder.`}</p>
   )
 }
