4c79758a08b44c5440771e19f0176363d9a58546
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-10 18:36:26 +0000

    Create dataTestId.ts

---
+++ src/library/constants/dataTestId.ts
@@ -0,0 +1,11 @@
+export const dataTestIdNames = {
+  createAccountFirstNameInput: 'create-account-first-name-input',
+  createAccountLastNameInput: 'create-account-last-name-input',
+  createAccountBusinessNameInput: 'create-account-business-name-input',
+  createAccountEmailInput: 'create-account-email-input',
+  createAccountPasswordInput: 'create-account-password-input',
+  createAccountStaySignedInCheckbox: 'create-account-stay-signed-in-checkbox',
+  createAccountSubmitButton: 'create-account-submit-button',
+  pleaseConfirmYourEmailMessage: 'please-confirm-your-email-message',
+  emailConfirmationFeedback: 'email-confirmation-feedback',
+} as const
