962c230e7b7e2b473d40be1480b6e259cdbb66f4
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-10 18:33:37 +0000

    Create testEmailInbox.ts

---
+++ src/types/definitions/testEmailInbox.ts
@@ -0,0 +1,4 @@
+import { testEmailInbox } from '@/library/database/schema'
+
+export type TestEmail = typeof testEmailInbox.$inferSelect
+export type TestEmailInsert = typeof testEmailInbox.$inferInsert
