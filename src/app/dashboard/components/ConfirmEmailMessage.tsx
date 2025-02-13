b001ade978f63226fa93fa53f0df804c79493213
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-13 09:23:23 +0000

    Update ConfirmEmailMessage.tsx

--- src/app/dashboard/components/ConfirmEmailMessage.tsx
+++ src/app/dashboard/components/ConfirmEmailMessage.tsx
@@ -10,6 +10,6 @@ export default function ConfirmEmailMessage() {
   return (
     <p
       data-test-id={dataTestIdNames.pleaseConfirmYourEmailMessage}
-      className="max-w-prose p-3 my-4 border-2 rounded-xl border-orange-300 ">{`Please confirm your email by clicking the link in the email sent to ${clientSafeUser.email}. Remember to check your junk folder.`}</p>
+      className="max-w-prose p-3 my-4 border-2 rounded-xl border-orange-300">{`Please confirm your email by clicking the link in the email sent to ${clientSafeUser.email}. Remember to check your junk folder.`}</p>
   )
 }
