ed213bb8ecaa88bb10f1a63d53d378c3c4a370cc
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-13 09:22:49 +0000

    Update route.ts

--- src/app/api/invitations/accept/[token]/route.ts
+++ src/app/api/invitations/accept/[token]/route.ts
@@ -1,9 +1,32 @@
+import bcrypt from 'bcrypt'
+import { and, eq } from 'drizzle-orm'
 import { NextRequest, NextResponse } from 'next/server'
 import { validate } from 'uuid'
 
+import { checkIsTestEmail } from '@/library/constants/testUsers'
+import { database } from '@/library/database/connection'
+import { customerToMerchant, invitations, users } from '@/library/database/schema'
+import { sendEmail } from '@/library/email/sendEmail'
+import { isProduction } from '@/library/environment/publicVariables'
+import { myPersonalEmail } from '@/library/environment/serverVariables'
 import logger from '@/library/logger'
+import { createCookieWithToken, createSessionCookieWithToken } from '@/library/utilities/server'
 
-import { authenticationMessages, AuthenticationMessages, BasicMessages, basicMessages, httpStatus } from '@/types'
+import {
+  authenticationMessages,
+  AuthenticationMessages,
+  BaseUser,
+  BasicMessages,
+  basicMessages,
+  cookieDurations,
+  CustomerToMerchant,
+  httpStatus,
+  Invitation,
+  InvitedCustomerBrowserInput,
+  NewBaseUser,
+} from '@/types'
+
+export type InvitationsAcceptPOSTbody = InvitedCustomerBrowserInput
 
 export interface InvitationsAcceptPOSTresponse {
   message: BasicMessages | AuthenticationMessages | string // ToDo: make strict
@@ -14,6 +37,17 @@ export async function POST(
   { params }: { params: Promise<{ token: string }> },
 ): Promise<NextResponse<InvitationsAcceptPOSTresponse>> {
   try {
+    const { firstName, lastName, businessName, password, staySignedIn }: InvitationsAcceptPOSTbody = await request.json()
+    logger.debug(firstName, lastName, businessName, password, staySignedIn)
+
+    const allDetailsProvided = Boolean(firstName && lastName && businessName && password)
+    const partialDetailsProvided = Boolean(firstName || lastName || businessName || password) && !allDetailsProvided
+
+    // Reject requests that have some but not all fields
+    if (partialDetailsProvided) {
+      return NextResponse.json({ message: 'missing fields' }, { status: httpStatus.http400badRequest })
+    }
+
     const token = (await params).token
 
     if (!token) {
@@ -24,16 +58,123 @@ export async function POST(
       return NextResponse.json({ message: authenticationMessages.tokenInvalid }, { status: httpStatus.http400badRequest })
     }
 
-    return NextResponse.json({ message: basicMessages.success }, { status: httpStatus.http201created })
+    const [foundInvitation]: Invitation[] = await database.select().from(invitations).where(eq(invitations.token, token)).limit(1)
+
+    if (!foundInvitation) {
+      return NextResponse.json({ message: 'invitation not found' }, { status: 400 })
+    }
+
+    const foundEmail = foundInvitation.email
+
+    const [existingUser]: BaseUser[] = await database.select().from(users).where(eq(users.email, foundEmail))
+
+    if (existingUser) {
+      //   - Transaction: change user table emailConfirmed to true if not already
+      //   - Transaction: Create the relationship
+      //   - Transaction: Delete invitation
+      // - Return 201
+    }
+
+    if (!existingUser) {
+      // If no details provided, ask for details
+      if (!allDetailsProvided) {
+        return NextResponse.json({ message: 'please provide details' }, { status: httpStatus.http422unprocessableContent })
+      }
+
+      if (allDetailsProvided) {
+        let transactionErrorMessage: string | null = 'transaction error creating new user'
+        let transactionErrorCode: number | null = httpStatus.http503serviceUnavailable
+
+        const { createdUser, createdRelationship } = await database.transaction(async tx => {
+          try {
+            // Transaction: Create user with emailConfirmed=true
+            const saltRounds = 10
+            const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+            const newUserInsert: NewBaseUser = {
+              email: foundEmail,
+              firstName,
+              lastName,
+              businessName,
+              hashedPassword,
+              emailConfirmed: true,
+              cachedTrialExpired: false,
+            }
+            const [createdUser]: BaseUser[] = await tx.insert(users).values(newUserInsert).returning()
+
+            // Transaction: Create relationship
+            transactionErrorMessage = 'transaction error creating customerToMerchant'
+            const newRelationshipInsert: CustomerToMerchant = {
+              merchantUserId: foundInvitation.userId,
+              customerUserId: createdUser.id,
+            }
+            const [createdRelationship]: CustomerToMerchant[] = await tx
+              .insert(customerToMerchant)
+              .values(newRelationshipInsert)
+              .returning()
+
+            // Transaction: Delete invitation
+            transactionErrorMessage = 'transaction error deleting invitation'
+            await tx.delete(invitations).where(and(eq(invitations.userId, foundInvitation.userId), eq(invitations.token, token)))
+
+            // Transaction: Send welcome email
+            const emailContent = `Hello ${createdUser.firstName}, thank you for signing up to Simple Order.` // ToDo: write a much better email
+
+            transactionErrorMessage = 'transaction error sending welcome email'
+
+            // Don't send actual emails! Important of the testing framework will go crazy
+
+            const isTestEmail = checkIsTestEmail(foundEmail)
+            let handledEmailSuccessfully: boolean = false
+
+            if (isTestEmail) {
+              logger.info('Bypassing actual email sending as test email detected')
+              handledEmailSuccessfully = true
+            }
+
+            if (!isTestEmail) {
+              const emailSuccess = await sendEmail({
+                to: isProduction ? foundEmail : myPersonalEmail,
+                subject: 'Thank you for using Simple Order',
+                htmlVersion: emailContent,
+                textVersion: emailContent,
+              })
+
+              if (!emailSuccess) {
+                tx.rollback()
+              }
+              handledEmailSuccessfully = true
+            }
+
+            transactionErrorMessage = null
+            transactionErrorCode = null
+            return { createdUser, createdRelationship }
+          } catch (error) {
+            logger.errorUnknown(error, 'Transaction error')
+            return { createdUser: null, createdRelationship: null }
+          }
+        })
+
+        if (transactionErrorMessage || transactionErrorCode || !createdUser || !createdRelationship) {
+          return NextResponse.json(
+            { message: transactionErrorMessage || 'unknown transaction error' },
+            { status: transactionErrorCode || httpStatus.http503serviceUnavailable },
+          )
+        }
+        const response = NextResponse.json({ message: basicMessages.success, createdUser }, { status: httpStatus.http200ok })
+
+        if (staySignedIn) {
+          response.cookies.set(createCookieWithToken(createdUser.id, cookieDurations.oneYear))
+        } else {
+          response.cookies.set(createSessionCookieWithToken(createdUser.id))
+        }
+
+        return response
+      }
+    }
+    return NextResponse.json({ message: basicMessages.serverError }, { status: httpStatus.http500serverError })
   } catch (error) {
     logger.errorUnknown(error)
     return NextResponse.json({ message: basicMessages.serverError }, { status: httpStatus.http500serverError })
   }
 }
-
-/*
-export interface InvitationsAcceptPOSTbody extends Omit<NewBaseUser, 'emailConfirmed'> {
-staySignedIn: boolean
-
-const { firstName, lastName, email, businessName, hashedPassword, staySignedIn }: InvitationsAcceptPOSTbody = await request.json()
-*/
