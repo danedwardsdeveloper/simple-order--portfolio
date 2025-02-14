39a790f5f31bcec1d7a4379082d3cb3507fada77
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-14 15:57:23 +0000

    Prevent sending emails during tests

--- src/library/email/sendEmail.ts
+++ src/library/email/sendEmail.ts
@@ -3,30 +3,55 @@ import { MailgunMessageData } from 'mailgun.js'
 import { myPersonalEmail } from '@/library/environment/serverVariables'
 import logger from '@/library/logger'
 
+import { database } from '../database/connection'
+import { testEmailInbox } from '../database/schema'
 import emailClient from './client'
 import { SendEmailBody } from '@/types'
+import { TestEmail, TestEmailInsert } from '@/types/definitions/testEmailInbox'
 
-export const sendEmail = async ({ to = myPersonalEmail, subject, htmlVersion, textVersion }: SendEmailBody): Promise<boolean> => {
-  const messageData: MailgunMessageData = {
-    from: 'Simple Order <noreply@simpleorder.co.uk>',
-    to,
-    subject,
-    text: textVersion,
-    html: htmlVersion,
-  }
-  const response = await emailClient.messages.create('simpleorder.co.uk', messageData)
+export const sendEmail = async ({ recipientEmail, subject, htmlVersion, textVersion }: SendEmailBody): Promise<boolean> => {
+  try {
+    // 1. Add all emails to test_email_inbox
+    const newTestEmailInboxValues: TestEmailInsert = {
+      content: `${htmlVersion}\n\n\n${textVersion}`,
+    }
+    const [addedToTestEmailInbox]: TestEmail[] = await database.insert(testEmailInbox).values(newTestEmailInboxValues).returning()
 
-  if (response.status === 200) {
-    logger.info(`Sent email to ${to}`)
-    return true
-  } else {
-    logger.errorUnknown(response.message, 'Error sending email')
+    if (!addedToTestEmailInbox) {
+      return false
+    }
+
+    // Don't actually send emails except to myself
+    if (!(recipientEmail.trim().toLowerCase() === myPersonalEmail)) {
+      logger.info(`Intercepted email to ${recipientEmail}`)
+      return true
+    }
+
+    const messageData: MailgunMessageData = {
+      from: 'Simple Order <noreply@simpleorder.co.uk>',
+      to: myPersonalEmail, // Only send emails to myself at the moment, if at all
+      subject,
+      text: textVersion,
+      html: htmlVersion,
+    }
+    const response = await emailClient.messages.create('simpleorder.co.uk', messageData)
+
+    // Enhancement ToDo: Add retry logic later on
+    if (response.status === 200) {
+      logger.info(`Sent email to ${recipientEmail}`)
+      return true
+    } else {
+      logger.errorUnknown(response.message, 'Error sending email')
+      return false
+    }
+  } catch (error) {
+    logger.errorUnknown(error, 'Error sending email')
     return false
   }
 }
 
 // sendEmail({
-//   to: myPersonalEmail,
+//   recipientEmail: myPersonalEmail,
 //   ...createNewMerchantEmail({
 //     recipientName: 'Dan',
 //     confirmationURL: 'www.google.com',
