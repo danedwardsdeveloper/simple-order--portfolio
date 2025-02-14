75f5cbf00152f9c81f2f49a19bd2234ce659d1a4
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-14 17:00:16 +0000

    Add HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import HeroSection from '@/components/heroSection'
+
 export default function HomePage() {
-  return (
-    <div className="w-full min-h-screen bg-blue-100 p-4 rounded-xl">
-      <h1>Landing page</h1>
-    </div>
-  )
+  return <HeroSection />
 }
