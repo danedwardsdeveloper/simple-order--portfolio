89099bf17247d00e55af5f00b87759682b2edcfa
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-14 15:57:12 +0000

    Delete ConfirmationEmailQueryParameters

--- src/types/api/authentication/email/confirm/index.ts
+++ src/types/api/authentication/email/confirm/index.ts
@@ -1,13 +1,7 @@
 import { AuthenticationMessages, BasicMessages } from '@/types/definitions/responseMessages'
 
-export const ConfirmEmailQueryParameters = {
-  token: 't',
-} as const
-
 export interface ConfirmEmailPOSTresponse {
-  message:
-    | Extract<BasicMessages, 'success' | 'server error'>
-    | Extract<AuthenticationMessages, 'confirmation token missing'>
+  message: Extract<BasicMessages, 'success' | 'server error'> | Extract<AuthenticationMessages, 'confirmation token missing'>
 }
 
 export interface ConfirmEmailPOSTbody {
