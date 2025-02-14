d04659dcaf918705fcc6d025b86d9774f2fff8ba
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-14 17:01:13 +0000

    Add content

--- src/library/constants/websiteCopy.ts
+++ src/library/constants/websiteCopy.ts
@@ -1,4 +1,8 @@
 export const websiteCopy = {
+  heroSection: {
+    h1: `Order management made simple`,
+    intro: `Simple Order is an order management website for wholesalers and their customers so you can reduce costs, save time, and eliminate phone call confusion. Start a free 30-day trial now and get started in minutes.`,
+  },
   metadata: {
     titles: {
       home41: 'Simple Order | Wholesale order management',
@@ -14,5 +18,15 @@ export const websiteCopy = {
   },
   linkDescriptions: {
     howItWorks: `Learn more about our B2B order management website`,
+  },
+  CTAs: {
+    primary: {
+      displayText: `Start free trial`,
+      href: '/free-trial',
+    },
+    secondary: {
+      displayText: `How it works`,
+      href: '/articles/how-it-works',
+    },
   },
 } as const
