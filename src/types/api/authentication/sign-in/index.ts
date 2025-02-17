30688b056f82f32b972b30053945da0853987f6a
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-17 09:48:14 +0000

    Update interface names

--- src/types/api/authentication/sign-in/index.ts
+++ src/types/api/authentication/sign-in/index.ts
@@ -1,13 +1,16 @@
-import type { AuthenticationMessages, BasicMessages } from '@/types/definitions/responseMessages'
-import type { SafeUser } from '@/types/definitions/users'
+import type {
+	AuthenticationMessages,
+	BasicMessages,
+} from '@/types/definitions/responseMessages';
+import type { BaseBrowserSafeUser } from '@/types/definitions/users';
 
 export interface SignInPOSTbody {
-	password: string
-	email: string
-	staySignedIn: boolean
+	password: string;
+	email: string;
+	staySignedIn: boolean;
 }
 
 export interface SignInPOSTresponse {
-	message: BasicMessages | AuthenticationMessages
-	foundUser?: SafeUser
+	message: BasicMessages | AuthenticationMessages;
+	baseBrowserSafeUser?: BaseBrowserSafeUser;
 }
