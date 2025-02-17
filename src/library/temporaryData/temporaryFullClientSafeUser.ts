7fb39e0dbbfd2c71bba35ba2fd110ee8789db766
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-17 14:11:03 +0000

    Move to temporaryData

---
+++ src/library/temporaryData/temporaryFullClientSafeUser.ts
@@ -0,0 +1,39 @@
+// Work in progress
+
+export const temporaryFullClientSafeUser = {
+	firstName: '',
+	lastName: '',
+	email: '',
+	businessName: '',
+	emailConfirmed: false,
+	merchantDetails: {
+		slug: '', // Not sure this is needed
+		freeTrialExpiry: new Date(), // Not sure I need this!
+		customersAsMerchant: [],
+	},
+	merchantsAsCustomer: [
+		{
+			id: 17, // Probably don't use the ID
+			// Slug maybe?
+			businessName: `Jane's Bakery`,
+			emailConfirmed: true,
+		},
+	],
+	inventory: [
+		{
+			id: 0, // Use the id as it's the only guaranteed unique property (for the map key)
+			name: '',
+			description: null,
+			priceInMinorUnits: 0,
+			customVat: null,
+		},
+	],
+}
+
+/*
+Data I actually need to display
+- Free trial end date/ time remaining
+- 
+
+
+*/
