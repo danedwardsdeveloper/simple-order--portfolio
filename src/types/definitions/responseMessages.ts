aa0c6b8278517d1ed982a26ec65334a252522ab5
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-17 09:48:31 +0000

    Extract constants

--- src/types/definitions/responseMessages.ts
+++ src/types/definitions/responseMessages.ts
@@ -1,46 +1,13 @@
-export const basicMessages = {
-	databaseError: 'database error',
-	parametersMissing: 'parameters missing',
-	serverError: 'server error',
-	success: 'success',
-} as const
+import type {
+	authenticationMessages,
+	basicMessages,
+	illegalCharactersMessages,
+} from '@/library/constants/responseMessages';
 
-export type BasicMessages = (typeof basicMessages)[keyof typeof basicMessages]
+export type BasicMessages = (typeof basicMessages)[keyof typeof basicMessages];
 
-export const authenticationMessages = {
-	alreadyConfirmed: 'already confirmed',
-	authorisationError: 'authorisation error',
-	businessNameMissing: 'businessName missing',
-	businessNameTaken: 'businessName taken',
-	confirmationTokenMissing: 'confirmation token missing',
-	emailInvalid: 'email invalid',
-	emailMissing: 'email missing',
-	emailNotConfirmed: 'email not confirmed',
-	emailTaken: 'email taken',
-	errorSendingEmail: 'error sending email',
-	fistNameMissing: 'firstName missing',
-	invalidCredentials: 'invalid credentials',
-	lastNameMissing: 'lastName missing',
-	merchantMissing: 'merchant profile missing',
-	noActiveTrialSubscription: 'no active subscription or trial',
-	notSignedIn: 'not signed in',
-	passwordMissing: 'password missing',
-	slugTaken: 'slug taken',
-	tokenExpired: 'token expired',
-	tokenInvalid: 'token invalid',
-	tokenMissing: 'token missing',
-	tokenUsed: 'token used',
-	unauthorised: 'unauthorised',
-	userNotFound: 'user not found',
-} as const
+export type AuthenticationMessages =
+	(typeof authenticationMessages)[keyof typeof authenticationMessages];
 
-export type AuthenticationMessages = (typeof authenticationMessages)[keyof typeof authenticationMessages]
-
-export const illegalCharactersMessages = {
-	firstName: 'firstName contains illegal characters',
-	lastName: 'lastName contains illegal characters',
-	password: 'password contains illegal characters',
-	businessName: 'businessName contains illegal characters',
-} as const
-
-export type IllegalCharactersMessages = (typeof illegalCharactersMessages)[keyof typeof illegalCharactersMessages]
+export type IllegalCharactersMessages =
+	(typeof illegalCharactersMessages)[keyof typeof illegalCharactersMessages];
