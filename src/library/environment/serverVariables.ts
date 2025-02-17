8af1be6113d1997806843ff5017b37d0e012f214
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-17 09:48:07 +0000

    Formatting

--- src/library/environment/serverVariables.ts
+++ src/library/environment/serverVariables.ts
@@ -1,5 +1,5 @@
-import { requireVariable } from './requireVariable'
+import { requireVariable } from './requireVariable';
 
-export const jwtSecret = requireVariable('JWT_SECRET')
-export const mailgunKey = requireVariable('MAILGUN_KEY')
-export const myPersonalEmail = requireVariable('MY_PERSONAL_EMAIL')
+export const jwtSecret = requireVariable('JWT_SECRET');
+export const mailgunKey = requireVariable('MAILGUN_KEY');
+export const myPersonalEmail = requireVariable('MY_PERSONAL_EMAIL');
--- src/library/utilities/server/definitions/extractIdFromRequestCookie.ts
+++ src/library/utilities/server/definitions/extractIdFromRequestCookie.ts
@@ -1,65 +1,69 @@
-import jwt, { JsonWebTokenError, type JwtPayload, TokenExpiredError } from 'jsonwebtoken'
-import type { NextRequest } from 'next/server'
-
-import { jwtSecret } from '@/library/environment/serverVariables'
-import logger from '@/library/logger'
-
+import { httpStatus } from '@/library/constants/httpStatus';
 import {
-	type AuthenticationMessages,
-	type BasicMessages,
-	type CookieNames,
 	authenticationMessages,
 	basicMessages,
-	httpStatus,
-} from '@/types'
+} from '@/library/constants/responseMessages';
+import { jwtSecret } from '@/library/environment/serverVariables';
+import logger from '@/library/logger';
+import type {
+	AuthenticationMessages,
+	BasicMessages,
+	CookieNames,
+} from '@/types';
+import jwt, {
+	JsonWebTokenError,
+	type JwtPayload,
+	TokenExpiredError,
+} from 'jsonwebtoken';
+import type { NextRequest } from 'next/server';
 
 export function extractIdFromRequestCookie(request: NextRequest): {
-	extractedUserId?: number
-	message: BasicMessages | AuthenticationMessages
-	status: number
+	extractedUserId?: number;
+	message: BasicMessages | AuthenticationMessages;
+	status: number;
 } {
-	const accessToken = request.cookies.get('token' as CookieNames)?.value
+	const accessToken = request.cookies.get('token' as CookieNames)?.value;
 	if (!accessToken) {
 		return {
 			message: authenticationMessages.tokenMissing,
 			status: httpStatus.http200ok,
-		}
+		};
 	}
 
 	try {
-		const { sub } = jwt.verify(accessToken, jwtSecret) as JwtPayload
+		const { sub } = jwt.verify(accessToken, jwtSecret) as JwtPayload;
 		if (!sub) {
-			throw new JsonWebTokenError('Missing sub claim')
+			throw new JsonWebTokenError('Missing sub claim');
 		}
 
-		const extractedUserId = Number(sub)
+		const extractedUserId = Number(sub);
 		if (Number.isNaN(extractedUserId)) {
-			throw new JsonWebTokenError('Invalid sub claim')
+			throw new JsonWebTokenError('Invalid sub claim');
 		}
 
 		return {
 			message: basicMessages.success,
 			status: httpStatus.http200ok,
 			extractedUserId,
-		}
+		};
 	} catch (error) {
 		if (error instanceof TokenExpiredError) {
 			return {
 				message: authenticationMessages.tokenExpired,
 				status: httpStatus.http401unauthorised,
-			}
+			};
 		}
 		if (error instanceof JsonWebTokenError) {
-			logger.error('JSON web token error: ', error)
+			logger.error('JSON web token error: ', error);
 			return {
 				message: authenticationMessages.tokenInvalid,
 				status: httpStatus.http401unauthorised,
-			}
+			};
 		}
-		logger.error('Unexpected error while verifying token: ', error)
+		logger.error('Unexpected error while verifying token: ', error);
 		return {
 			message: authenticationMessages.tokenInvalid,
 			status: httpStatus.http401unauthorised,
-		}
+		};
 	}
 }
