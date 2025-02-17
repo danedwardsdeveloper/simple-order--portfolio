1ae2f2993d51bb3ca9cad9402d7b80a262b3cddb
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-17 09:46:41 +0000

    Create library/constants

--- src/library/constants/httpStatus.ts
+++ src/library/constants/httpStatus.ts
@@ -4,7 +4,6 @@ export const httpStatus = {
 	http202accepted: 202,
 	http204noContent: 204,
 	http206partialContent: 206,
-	http422unprocessableContent: 422,
 
 	http400badRequest: 400,
 	http401unauthorised: 401,
@@ -14,6 +13,7 @@ export const httpStatus = {
 	http409conflict: 409,
 	http410gone: 410,
 	http415unsupportedMediaType: 415,
+	http422unprocessableContent: 422,
 	http429tooManyRequests: 429,
 
 	http500serverError: 500,
@@ -21,4 +21,4 @@ export const httpStatus = {
 	http502badGateway: 502,
 	http503serviceUnavailable: 503,
 	http504gatewayTimeout: 504,
-} as const
+} as const;
---
+++ src/library/constants/responseMessages.ts
@@ -0,0 +1,40 @@
+export const basicMessages = {
+	databaseError: 'database error',
+	parametersMissing: 'parameters missing',
+	serverError: 'server error',
+	success: 'success',
+} as const;
+
+export const authenticationMessages = {
+	alreadyConfirmed: 'already confirmed',
+	authorisationError: 'authorisation error',
+	businessNameMissing: 'businessName missing',
+	businessNameTaken: 'businessName taken',
+	confirmationTokenMissing: 'confirmation token missing',
+	emailInvalid: 'email invalid',
+	emailMissing: 'email missing',
+	emailNotConfirmed: 'email not confirmed',
+	emailTaken: 'email taken',
+	errorSendingEmail: 'error sending email',
+	fistNameMissing: 'firstName missing',
+	invalidCredentials: 'invalid credentials',
+	lastNameMissing: 'lastName missing',
+	merchantMissing: 'merchant profile missing',
+	noActiveTrialSubscription: 'no active subscription or trial',
+	notSignedIn: 'not signed in',
+	passwordMissing: 'password missing',
+	slugTaken: 'slug taken',
+	tokenExpired: 'token expired',
+	tokenInvalid: 'token invalid',
+	tokenMissing: 'token missing',
+	tokenUsed: 'token used',
+	unauthorised: 'unauthorised',
+	userNotFound: 'user not found',
+} as const;
+
+export const illegalCharactersMessages = {
+	firstName: 'firstName contains illegal characters',
+	lastName: 'lastName contains illegal characters',
+	password: 'password contains illegal characters',
+	businessName: 'businessName contains illegal characters',
+} as const;
