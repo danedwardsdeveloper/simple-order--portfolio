c2e76bac0bcf2187b740f17da936c4c539a0fa68
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-22 17:37:43 +0000

    Add data fetching

--- src/app/merchants/page.tsx
+++ src/app/merchants/page.tsx
@@ -1,34 +1,81 @@
 'use client'
 
+import Spinner from '@/components/Spinner'
+import UnauthorisedLinks from '@/components/UnauthorisedLinks'
+import { apiPaths } from '@/library/constants'
+import logger from '@/library/logger'
 import { useAuthorisation } from '@/providers/authorisation'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import type { MerchantsGETresponse } from '../api/merchants/route'
 
 export default function Page() {
-	const { clientSafeUser } = useAuthorisation()
-	if (!clientSafeUser) return null
-	const merchants = clientSafeUser.merchantsAsCustomer?.map((id) => id) || []
+	const { browserSafeUser, confirmedMerchants, setConfirmedMerchants, pendingMerchants, setPendingMerchants } = useAuthorisation()
+	const [loading, setLoading] = useState(false)
 
-	function MerchantsList() {
-		if (!merchants || merchants.length === 0) {
-			return <span>No merchants found</span>
+	useEffect(() => {
+		async function getMerchants() {
+			try {
+				setLoading(true)
+				const response = await fetch(apiPaths.merchants.base, { credentials: 'include' })
+				const { confirmedMerchants, pendingMerchants, message }: MerchantsGETresponse = await response.json()
+
+				if (confirmedMerchants) {
+					setConfirmedMerchants(confirmedMerchants)
+				}
+
+				if (pendingMerchants) {
+					setPendingMerchants(pendingMerchants)
+				}
+
+				if (message !== 'success') {
+					logger.error('merchants/page.tsx error: ', message)
+				}
+			} catch (error) {
+				logger.error('merchants/page.tsx error: ', error)
+			} finally {
+				setLoading(false)
+			}
 		}
 
-		return (
-			<>
-				{merchants.map((merchant) => {
-					return (
-						<span key={merchant.id} className="block transition-colors duration-300 text-zinc-600 hover:text-blue-400 active:text-blue-500">
-							{merchant.businessName}
-						</span>
-					)
-				})}
-			</>
-		)
+		if (browserSafeUser) getMerchants()
+	}, [browserSafeUser, setConfirmedMerchants, setPendingMerchants])
+
+	if (!browserSafeUser) return <UnauthorisedLinks />
+
+	if (loading) {
+		return <Spinner />
+	}
+
+	if (!confirmedMerchants?.length && !pendingMerchants?.length) {
+		return <span>No merchants found</span>
 	}
 
 	return (
 		<>
 			<h1>Merchants</h1>
-			<div>{<MerchantsList />}</div>
+
+			{confirmedMerchants &&
+				confirmedMerchants?.length > 0 &&
+				confirmedMerchants.map((merchant) => (
+					<Link key={merchant.slug} href={`/merchants/${merchant.slug}`} className="link-primary">
+						{merchant.businessName}
+					</Link>
+				))}
+
+			{pendingMerchants && pendingMerchants?.length > 0 && (
+				<>
+					<h2>Invitations</h2>
+					{pendingMerchants.map((merchant) => (
+						<span
+							key={merchant.slug}
+							className="block transition-colors duration-300 text-zinc-600 hover:text-blue-400 active:text-blue-500"
+						>
+							{merchant.businessName}
+						</span>
+					))}
+				</>
+			)}
 		</>
 	)
 }
