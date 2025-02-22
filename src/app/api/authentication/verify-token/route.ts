f85aa7530fb78e4ae6e188cef5ccf5788b03dd6a
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-22 17:40:38 +0000

    Update interface names, add await

--- src/app/api/authentication/verify-token/route.ts
+++ src/app/api/authentication/verify-token/route.ts
@@ -1,27 +1,26 @@
 import { apiPaths, authenticationMessages, basicMessages, httpStatus } from '@/library/constants'
 import { database } from '@/library/database/connection'
-import { checkMerchantProfileExists, getInventory } from '@/library/database/operations'
-import { invitations, users } from '@/library/database/schema'
+import { users } from '@/library/database/schema'
 import logger from '@/library/logger'
 import { extractIdFromRequestCookie } from '@/library/utilities/server'
-import type { AuthenticationMessages, BaseBrowserSafeUser, BasicMessages, FullBrowserSafeUser, Invitation } from '@/types'
+import type { AuthenticationMessages, BaseBrowserSafeUser, BasicMessages, FullBrowserSafeUser } from '@/types'
 import { eq } from 'drizzle-orm'
 import { type NextRequest, NextResponse } from 'next/server'
 
 export interface VerifyTokenGETresponse {
 	message: BasicMessages | AuthenticationMessages
-	fullBrowserSafeUser?: FullBrowserSafeUser
+	browserSafeUser?: FullBrowserSafeUser
 }
 
 export async function GET(request: NextRequest): Promise<NextResponse<VerifyTokenGETresponse>> {
 	try {
-		const { extractedUserId, status, message } = extractIdFromRequestCookie(request)
+		const { extractedUserId, status, message } = await extractIdFromRequestCookie(request)
 
 		if (!extractedUserId) {
 			return NextResponse.json({ message }, { status })
 		}
 
-		const [foundBaseUser]: BaseBrowserSafeUser[] = await database
+		const [browserSafeUser]: BaseBrowserSafeUser[] = await database
 			.select({
 				firstName: users.firstName,
 				lastName: users.lastName,
@@ -33,46 +32,11 @@ export async function GET(request: NextRequest): Promise<NextResponse<VerifyToke
 			.from(users)
 			.where(eq(users.id, extractedUserId))
 
-		if (!foundBaseUser) {
+		if (!browserSafeUser) {
 			return NextResponse.json({ message: authenticationMessages.userNotFound }, { status: httpStatus.http401unauthorised })
 		}
 
-		let fullBrowserSafeUser: FullBrowserSafeUser = foundBaseUser
-
-		logger.debug('Full browser-safe user: ', fullBrowserSafeUser)
-
-		const { merchantProfileExists, slug } = await checkMerchantProfileExists(extractedUserId)
-
-		if (merchantProfileExists && slug) {
-			const inventory = await getInventory(extractedUserId)
-
-			const pendingInvitations: Invitation[] = await database
-				.select()
-				.from(invitations)
-				.where(eq(invitations.senderUserId, extractedUserId))
-
-			logger.debug('Pending invitations: ', pendingInvitations)
-
-			if (inventory) {
-				fullBrowserSafeUser = {
-					...foundBaseUser,
-					merchantDetails: {
-						slug,
-						freeTrial: {
-							endDate: new Date(), // ToDo: fix this!
-						},
-						customersAsMerchant: [],
-					},
-					inventory,
-				}
-			}
-		}
-
-		// Add merchant details, get orders etc. and transform user
-
-		logger.debug('Full browser-safe user: ', JSON.stringify(fullBrowserSafeUser))
-
-		return NextResponse.json({ message: basicMessages.success, fullBrowserSafeUser }, { status: httpStatus.http200ok })
+		return NextResponse.json({ message: basicMessages.success, browserSafeUser }, { status: httpStatus.http200ok })
 	} catch (error) {
 		logger.error(`${apiPaths.authentication.verifyToken} error: `, error)
 		return NextResponse.json({ message: basicMessages.serverError }, { status: httpStatus.http500serverError })
