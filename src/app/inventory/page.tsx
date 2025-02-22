162772245ccd92174f060273b240824043f392b3
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-22 17:38:15 +0000

    Update imports

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,8 +1,14 @@
+'use client'
+import UnauthorisedLinks from '@/components/UnauthorisedLinks'
+import { useAuthorisation } from '@/providers/authorisation'
 import AddInventoryForm from './components/AddInventoryForm'
 import InventoryControlPanel from './components/InventoryControlPanel'
 import InventoryList from './components/InventoryList'
 
 export default function InventoryPage() {
+	const { browserSafeUser } = useAuthorisation()
+	if (!browserSafeUser) return <UnauthorisedLinks />
+
 	return (
 		<div className="flex flex-col gap-y-4">
 			<h1>Inventory</h1>
