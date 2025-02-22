58f803c2de6eaba6339312e9a6cf5b26c1574465
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-22 17:32:40 +0000

    Create BrowserSafeMerchantProduct

--- src/types/definitions/products.ts
+++ src/types/definitions/products.ts
@@ -4,4 +4,6 @@ export type Product = typeof products.$inferSelect
 export type NewProduct = typeof products.$inferInsert
 
 // Keep the id here as it's useful for React mapping as it's guaranteed to be unique
-export type ClientProduct = Pick<Product, 'id' | 'name' | 'description' | 'priceInMinorUnits' | 'customVat'>
+export type BrowserSafeMerchantProduct = Pick<Product, 'id' | 'name' | 'description' | 'priceInMinorUnits' | 'customVat' | 'deletedAt'>
+
+export type BrowserSafeCustomerProduct = Omit<BrowserSafeMerchantProduct, 'deletedAt'>
