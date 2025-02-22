81cbdba9e533ee79b0dbf19b0bf02f8559dcaba7
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-22 17:39:39 +0000

    Add ConfirmEmailMessage

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -1,3 +1,4 @@
+import ConfirmEmailMessage from '@/components/ConfirmEmailMessage'
 import InviteCustomerForm from './components/InviteCustomerForm'
 
 // This is for merchants to handle their invited customers
@@ -5,6 +6,7 @@ export default function CustomersPage() {
 	return (
 		<>
 			<h2>Customers</h2>
+			<ConfirmEmailMessage />
 			<InviteCustomerForm />
 		</>
 	)
