a6eb0979fd6e41d43a979029bc0dd618c728306e
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-22 17:34:27 +0000

    Update temporaryFullClientSafeUser.ts

--- src/library/temporaryData/temporaryFullClientSafeUser.ts
+++ src/library/temporaryData/temporaryFullClientSafeUser.ts
@@ -1,39 +1,123 @@
-// Work in progress
+import type { BaseBrowserSafeUser, BrowserSafeMerchantProduct } from '@/types'
 
-export const temporaryFullClientSafeUser = {
+export interface WipConfirmedCustomer {
+	businessName: string
+	firstName: string
+}
+
+export interface WipPendingCustomer {
+	obfuscatedEmail: string
+	lastSendDate: Date
+}
+
+export interface WipExpiredCustomer {
+	obfuscatedEmail: string
+	lastSendDate: Date
+}
+
+export interface WipMerchant {
+	[merchantSlug: string]: {
+		businessName: string
+		orders: {
+			orderId: number
+			createdAt: Date
+			updatedAt: Date
+			status: 'draft' | 'submitted' | 'processed'
+			items: {
+				name: string
+				quantity: number
+			}[]
+		}[]
+	}
+}
+
+export interface WipUser extends BaseBrowserSafeUser {
+	merchantData?: {
+		accountType: 'trial' | 'paid' | 'expired'
+		freeTrialExpiry: Date
+		subscriptionData: {
+			lastPayment: Date
+			nextPayment: Date
+		}
+		vatSettings: {
+			defaultRate: number
+			rateOptions: number[]
+		}
+		customers: {
+			confirmed: WipConfirmedCustomer[]
+			pending: WipPendingCustomer[]
+			expired: WipExpiredCustomer[]
+		}
+	}
+	inventory: BrowserSafeMerchantProduct[]
+	merchants?: WipMerchant[]
+}
+
+export const temporaryFullClientSafeUser: WipUser = {
 	firstName: '',
 	lastName: '',
 	email: '',
 	businessName: '',
 	emailConfirmed: false,
-	merchantDetails: {
-		slug: '', // Not sure this is needed
-		freeTrialExpiry: new Date(), // Not sure I need this!
-		customersAsMerchant: [],
-	},
-	merchantsAsCustomer: [
-		{
-			id: 17, // Probably don't use the ID
-			// Slug maybe?
-			businessName: `Jane's Bakery`,
-			emailConfirmed: true,
+	cachedTrialExpired: false,
+	merchantData: {
+		accountType: 'trial',
+		freeTrialExpiry: new Date(),
+		subscriptionData: {
+			lastPayment: new Date(),
+			nextPayment: new Date(),
 		},
-	],
+		vatSettings: {
+			defaultRate: 20,
+			rateOptions: [20, 5, 0],
+		},
+		customers: {
+			confirmed: [],
+			pending: [],
+			expired: [],
+		},
+	},
 	inventory: [
 		{
-			id: 0, // Use the id as it's the only guaranteed unique property (for the map key)
-			name: '',
-			description: null,
-			priceInMinorUnits: 0,
+			id: 1,
+			name: 'Croissant, plain',
+			description: 'The classic French pastry.',
+			priceInMinorUnits: 84,
 			customVat: null,
+			deletedAt: null,
+		},
+		{
+			id: 23,
+			name: 'Sourdough loaf, white',
+			description: 'A flavorsome hand-crafted white loaf created with a 30-year-old starter.',
+			priceInMinorUnits: 289,
+			customVat: 0,
+			deletedAt: null,
+		},
+	],
+	merchants: [
+		{
+			'janes-bakery': {
+				businessName: "Jane's Bakery",
+				orders: [
+					{
+						orderId: 1,
+						createdAt: new Date(),
+						updatedAt: new Date(),
+						status: 'draft',
+						items: [
+							{
+								name: 'Croissant, plain',
+								quantity: 7,
+							},
+							{
+								name: 'Sourdough loaf, white',
+								quantity: 5,
+							},
+						],
+					},
+				],
+			},
 		},
 	],
 }
-
-/*
-Data I actually need to display
-- Free trial end date/ time remaining
-- 
-
-
-*/
