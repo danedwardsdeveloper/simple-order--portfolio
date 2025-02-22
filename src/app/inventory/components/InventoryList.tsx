95a8d2f17649f9fabbce502e8de36f89036faea3
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-22 17:24:04 +0000

    Update state handling

--- src/app/inventory/components/InventoryList.tsx
+++ src/app/inventory/components/InventoryList.tsx
@@ -1,16 +1,14 @@
 'use client'
-
 import { useAuthorisation } from '@/providers/authorisation'
 import InventoryCard from './InventoryCard'
 
 export default function InventoryList() {
-	const { clientSafeUser } = useAuthorisation()
-
-	if (!clientSafeUser?.inventory) return null
+	const { inventory } = useAuthorisation()
+	if (!inventory) return null
 
 	return (
 		<ul className="flex flex-col w-full gap-y-4 max-w-xl -mx-3">
-			{clientSafeUser.inventory.map((product, index) => (
+			{inventory.map((product, index) => (
 				<InventoryCard key={product.id} product={product} zebraStripe={Boolean(index % 2)} />
 			))}
 		</ul>
