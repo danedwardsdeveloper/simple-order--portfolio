ea9620c02d3299bf362e84caf5034a0442ac1893
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-22 17:19:41 +0000

    Update imports

--- misc/manifest.ts
+++ misc/manifest.ts
@@ -1,29 +1,28 @@
-import { MetadataRoute } from 'next'
-
-import { websiteCopy } from '@/library/misc/copy'
+import type { MetadataRoute } from 'next'
+import { websiteCopy } from '../src/library/constants'
 
 export default function manifest(): MetadataRoute.Manifest {
-  return {
-    name: websiteCopy.siteNameWithTag,
-    short_name: websiteCopy.siteName,
-    description: websiteCopy.metaDescription138,
-    start_url: '/',
-    display: 'browser',
-    background_color: 'hsl(0, 0%, 100%)',
-    theme_color: 'hsl(217, 91%, 60%)', // blue-500
-    icons: [
-      {
-        src: '/favicon/favicon-192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/favicon/favicon-512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
-    orientation: 'portrait',
-    scope: '/',
-  }
+	return {
+		name: websiteCopy.extras.siteNameWithTag,
+		short_name: 'Simple Order',
+		description: websiteCopy.metadata.descriptions.home138,
+		start_url: '/',
+		display: 'browser',
+		background_color: 'hsl(0, 0%, 100%)',
+		theme_color: 'hsl(217, 91%, 60%)', // blue-500
+		icons: [
+			{
+				src: '/favicon/favicon-192.png',
+				sizes: '192x192',
+				type: 'image/png',
+			},
+			{
+				src: '/favicon/favicon-512.png',
+				sizes: '512x512',
+				type: 'image/png',
+			},
+		],
+		orientation: 'portrait',
+		scope: '/',
+	}
 }
