7f079fa01cfc46a0f23fd7ccee0ca4b7a4e862f9
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-22 17:32:27 +0000

    Add TokenMessages

--- src/types/definitions/responseMessages.ts
+++ src/types/definitions/responseMessages.ts
@@ -1,7 +1,17 @@
-import type { authenticationMessages, basicMessages, illegalCharactersMessages } from '@/library/constants'
+import type {
+	authenticationMessages,
+	basicMessages,
+	illegalCharactersMessages,
+	relationshipMessages,
+	tokenMessages,
+} from '@/library/constants'
 
 export type BasicMessages = (typeof basicMessages)[keyof typeof basicMessages]
 
 export type AuthenticationMessages = (typeof authenticationMessages)[keyof typeof authenticationMessages]
 
 export type IllegalCharactersMessages = (typeof illegalCharactersMessages)[keyof typeof illegalCharactersMessages]
+
+export type TokenMessages = (typeof tokenMessages)[keyof typeof tokenMessages]
+
+export type RelationshipMessage = (typeof relationshipMessages)[keyof typeof relationshipMessages]
