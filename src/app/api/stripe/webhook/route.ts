9187ca1d0d4409acf6dbf9f73c5d20d2c6dd1f50
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-24 21:05:28 +0000

    Add webhookHandler

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -1,7 +1,8 @@
 import { apiPaths, basicMessages, httpStatus } from '@/library/constants'
 import { stripeWebhookSecret } from '@/library/environment/serverVariables'
 import logger from '@/library/logger'
-import stripeClient from '@/library/stripeClient'
+import stripeClient from '@/library/stripe/stripeClient'
+import { webhookHandler } from '@/library/stripe/webhookHandler'
 import { headers } from 'next/headers'
 import { type NextRequest, NextResponse } from 'next/server'
 
@@ -13,7 +14,10 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
 	if (stripeWebhookSecret && stripeSignature) {
 		try {
 			const event = stripeClient.webhooks.constructEvent(rawBody, stripeSignature, stripeWebhookSecret)
-			logger.info(`${apiPaths.stripe.webhook} event: `, event)
+
+			await webhookHandler(event)
+
+			logger.info('Received webhook type: ', event.type)
 			return NextResponse.json({ message: 'success' }, { status: httpStatus.http200ok })
 		} catch (error) {
 			logger.error(`${apiPaths.stripe.webhook} error: `, error)
