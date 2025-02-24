8694e1bec7749b9bebda9c0ab304be59afad06bb
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-24 21:03:47 +0000

    Update manualDatabaseCommands.ts

--- tests/utilities/manualDatabaseCommands.ts
+++ tests/utilities/manualDatabaseCommands.ts
@@ -1,11 +1,27 @@
-import { eq } from 'drizzle-orm'
-
 import { database } from '@/library/database/connection'
 import { merchantProfiles } from '@/library/database/schema'
+import { eq } from 'drizzle-orm'
 
 async function _deleteFromDatabase() {
 	const _success = await database.delete(merchantProfiles).where(eq(merchantProfiles.userId, 11)).returning()
 }
+
+// const session = {
+// 	metadata: {
+// 		email: 'myemail@gmail.com',
+// 		simpleOrderUserId: '148',
+// 	},
+// }
+
+// const values: SubscriptionInsertValues = {
+// 	userId: Number(session.metadata.simpleOrderUserId),
+// 	stripeCustomerId: String(session.customer),
+// 	subscriptionActive: true,
+// 	currentPeriodStart: new Date(session.current_period_start * 1000),
+// 	currentPeriodEnd: new Date(session.current_period_end * 1000),
+// }
+
+// await database.insert(subscriptions).values(values)
 
 /* 
 pnpm tsx tests/utilities/manualDatabaseCommands
