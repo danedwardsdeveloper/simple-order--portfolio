ca42318cd1c208bd1b765120b25507448d47b1ad
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-25 19:37:37 +0000

    Add metadata

--- src/app/sign-in/layout.tsx
+++ src/app/sign-in/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
 
 export const metadata: Metadata = {
-	title: 'Merchants',
+	title: 'Sign in',
 }
 
 export default function Layout({ children }: { children: ReactNode }) {
