317a02304efc3e35dfbbb2c5e1923e6e559bce65
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-25 19:40:32 +0000

    update bareDomain name

--- src/library/email/templates/invitations/newUser.ts
+++ src/library/email/templates/invitations/newUser.ts
@@ -1,5 +1,5 @@
 import { websiteCopy } from '@/library/constants'
-import { bareDomain, productionBaseURL } from '@/library/environment/publicVariables'
+import { bareLaunchedDomain, productionBaseURL } from '@/library/environment/publicVariables'
 import { formatTimeAndDate } from '@/library/utilities'
 import type { EmailTemplate } from '@/types'
 import { createParagraph } from '../../utilities'
@@ -23,7 +23,7 @@ export function createNewUserInvitation({
 	const securityMessage = `If you weren't expecting an invitation from this business or want to decline the invitation, you can safely ignore this email. The link will expire in 7 days, at  ${formatTimeAndDate(expiryDate)}.`
 	const thankYou = 'Many thanks'
 	const companyName = 'Accounts team, Simple Order'
-	const companyLink = `<a href="${productionBaseURL}">${bareDomain}</a>`
+	const companyLink = `<a href="${productionBaseURL}">${bareLaunchedDomain}</a>`
 
 	const htmlVersion = `
   ${createParagraph(greeting)}
