df86c92fdd0d4d2e0b2e831188322773804798db
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-25 19:39:02 +0000

    Extract invoiceHandler

--- src/library/stripe/webhookHandler.ts
+++ src/library/stripe/webhookHandler.ts
@@ -1,52 +1,12 @@
 import logger from '@/library/logger'
-import type { SubscriptionInsertValues } from '@/types'
-import { eq } from 'drizzle-orm'
 import type Stripe from 'stripe'
 import { apiPaths } from '../constants'
-import { database } from '../database/connection'
-import { subscriptions, users } from '../database/schema'
+import { handleInvoice } from './invoiceHandler'
 
 export async function webhookHandler(event: Stripe.Event) {
 	switch (event.type) {
-		// invoice.paid should be the single source of truth
-		// https://www.pedroalonso.net/blog/stripe-webhooks-solving-race-conditions/
 		case 'invoice.paid':
-			try {
-				const invoice = event.data.object as Stripe.Invoice
-
-				// ToDo: this is really important
-				if (!invoice.customer_email) {
-					logger.error(`No customer email for invoice ${invoice.id}, cannot match to user`)
-					break
-				}
-
-				let simpleOrderUserId = Number(invoice.subscription_details?.metadata.simpleOrderUserId) | null
-
-				if (!simpleOrderUserId) {
-					;[{ simpleOrderUserId }] = await database
-						.select({ simpleOrderUserId: users.id })
-						.from(users)
-						.where(eq(users.email, invoice.customer_email))
-						.limit(1)
-				}
-
-				if (!simpleOrderUserId) {
-					logger.error("Couldn't retrieve Simple Order customer ID from checkout.invoice.completed. Invoice: ", invoice)
-				}
-
-				const values: SubscriptionInsertValues = {
-					userId: Number(simpleOrderUserId),
-					stripeCustomerId: String(invoice.customer),
-					currentPeriodStart: new Date(invoice.lines.data[0].period.start * 1000),
-					currentPeriodEnd: new Date(invoice.lines.data[0].period.end * 1000),
-				}
-
-				logger.debug('Subscription insert values: ', values)
-
-				await database.insert(subscriptions).values(values)
-			} catch (error) {
-				logger.error('webhookHandler customer.subscription.created error: ', error)
-			}
+			await handleInvoice(event.data.object)
 			break
 
 		case 'customer.subscription.created':
@@ -85,12 +45,6 @@ export async function webhookHandler(event: Stripe.Event) {
 		case 'invoice.payment_succeeded':
 			break
 
-		// cspell:disable
-		// contains:
-		//   "customer_email": "myemail@gmail.com",
-		// "metadata": { "email": "myemail@gmail.com", "simpleOrderUserId": "148" },
-		//   "subscription": "sub_1Qw4bEKCyKQPfgmWIqA1HjlO",
-		// cspell:enable
 		case 'checkout.session.completed':
 			break
 
