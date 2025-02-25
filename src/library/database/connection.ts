9dcd160b6aa35acc2839ebe97e351d9c708c9e42
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-25 20:25:06 +0000

    Remove custom pool settings

--- src/library/database/connection.ts
+++ src/library/database/connection.ts
@@ -1,21 +1,15 @@
+import { sql } from 'drizzle-orm'
 import { drizzle } from 'drizzle-orm/node-postgres'
-import { Pool } from 'pg'
-
+import { databaseURL } from '../environment/serverVariables'
 import logger from '../logger'
 
-const pool = new Pool({
-	host: 'localhost',
-	port: 5432,
-	database: 'simple_order_dev',
-})
+export const database = drizzle(databaseURL)
 
 export async function testDatabaseConnection() {
 	try {
-		await pool.query('SELECT 1')
+		await database.execute(sql`SELECT 1`)
 		logger.info('Success testing database connection')
 	} catch (error) {
 		logger.error('Database connection test failed:', error)
 	}
 }
-
-export const database = drizzle(pool)
