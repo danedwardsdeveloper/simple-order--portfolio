ab7b3627777f2a0015bb8542a14fbf43bf19fed3
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-25 19:42:15 +0000

    Make same colour in either position

--- src/components/ToggleWithLabel.tsx
+++ src/components/ToggleWithLabel.tsx
@@ -27,7 +27,7 @@ export default function ToggleWithLabel({ enabled, setEnabled, enabledLabel, dis
 			<Switch
 				checked={enabled}
 				onChange={setEnabled}
-				className="group relative inline-flex h-6 w-11 shrink-0 cursor-pointer rounded-full border-2 border-transparent bg-gray-200 transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 data-[checked]:bg-blue-600"
+				className="group relative inline-flex h-6 w-11 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 bg-blue-600"
 			>
 				<span
 					aria-hidden="true"
