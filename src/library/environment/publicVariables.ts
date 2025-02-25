3a4fb58fbc18fc97b10fa9ba696cf2f1e8633475
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-25 19:40:19 +0000

    Rename bareDomain variables

--- src/library/environment/publicVariables.ts
+++ src/library/environment/publicVariables.ts
@@ -3,8 +3,12 @@ import type { LogLevel } from '@/types'
 export const isProduction = process.env.NODE_ENV === 'production'
 export const isDevelopment = process.env.NODE_ENV === 'development'
 
-export const bareDomain = 'simple-order.fly.dev'
-export const productionBaseURL = `https://${bareDomain}`
+export const siteIsLaunched = false
+
+export const bareProductionDomain = 'simple-order.fly.dev'
+export const bareLaunchedDomain = 'simpleorder.co.uk'
+
+export const productionBaseURL = `https://${bareProductionDomain}`
 export const developmentBaseURL = 'http://localhost:3000'
 export const dynamicBaseURL = isProduction ? productionBaseURL : developmentBaseURL
 
