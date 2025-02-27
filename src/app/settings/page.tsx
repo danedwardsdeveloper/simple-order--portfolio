ecf113c299a26cfb4a9cd7515c3ee5a9bf4ed041
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-27 17:05:55 +0000

    Add UserInformation

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -3,6 +3,7 @@ import UnauthorisedLinks from '@/components/UnauthorisedLinks'
 import RoleModeButton from '@/components/menubar/RoleModeButton'
 import { useUser } from '@/providers/user'
 import SignOutButton from './components/SignOutButton'
+import UserInformation from './components/UserInformation'
 
 export default function SettingsPage() {
 	const { user } = useUser()
@@ -12,6 +13,7 @@ export default function SettingsPage() {
 	return (
 		<>
 			<RoleModeButton />
+			<UserInformation />
 			<SignOutButton />
 		</>
 	)
