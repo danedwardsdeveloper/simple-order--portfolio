d4546f0f4416faffd8cb6184692c9c2ef725252a
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-27 17:05:27 +0000

    Add early return null

--- src/components/menubar/RoleModeButton.tsx
+++ src/components/menubar/RoleModeButton.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { useUi } from '@/providers/ui'
+import { useUser } from '@/providers/user'
 import ToggleWithLabel from '../ToggleWithLabel'
 
 export default function RoleModeButton() {
+	const { user } = useUser()
 	const { merchantMode, toggleMerchantMode } = useUi()
 
-	// ToDo: hide this if the user only has one role
+	if (user?.roles !== 'both') return null
 
 	return (
 		<div className="px-4 pt-2 pb-8 rounded-xl bg-blue-50 flex flex-col gap-y-4 text-center">
