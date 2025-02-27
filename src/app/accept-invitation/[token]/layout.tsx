3ca20e34b6db4bb40fcf79bfad8938358174649e
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-27 16:58:26 +0000

    Add metadata

--- src/app/accept-invitation/[token]/layout.tsx
+++ src/app/accept-invitation/[token]/layout.tsx
@@ -1,5 +1,10 @@
 import PageContainer from '@/components/PageContainer'
+import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+	title: 'Accept invitation',
+}
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return <PageContainer>{children}</PageContainer>
