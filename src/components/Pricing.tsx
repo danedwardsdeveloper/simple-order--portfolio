460bfa44ff5e2906c4fb8945d3e32f8a6dc98e4c
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-27 17:04:19 +0000

    Change accent colour

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -20,13 +20,13 @@ export default function Pricing() {
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 						</p>
 						<div className="mt-10 flex items-center gap-x-4">
-							<h4 className="flex-none text-sm/6 font-semibold text-indigo-600">What’s included</h4>
+							<h4 className="flex-none text-sm/6 font-semibold text-blue-600">What's included</h4>
 							<div className="h-px flex-auto bg-gray-100" />
 						</div>
 						<ul className="mt-8 grid grid-cols-1 gap-4 text-sm/6 text-gray-600 sm:grid-cols-2 sm:gap-6">
 							{includedFeatures.map((feature) => (
 								<li key={feature} className="flex gap-x-3">
-									<CheckIcon aria-hidden="true" className="h-6 w-5 flex-none text-indigo-600" />
+									<CheckIcon aria-hidden="true" className="h-6 w-5 flex-none text-blue-600" />
 									{feature}
 								</li>
 							))}
@@ -42,7 +42,7 @@ export default function Pricing() {
 								</p>
 								<Link
 									href="/free-trial"
-									className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+									className="mt-10 block w-full rounded-md bg-blue-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
 								>
 									Get access
 								</Link>
