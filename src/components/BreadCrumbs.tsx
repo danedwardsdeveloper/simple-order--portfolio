d44d76ce0d9d1cad6605b30f3dd4471d62e51c33
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-27 17:05:39 +0000

    Make generic and reusable

---
+++ src/components/BreadCrumbs.tsx
@@ -0,0 +1,65 @@
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import Link from 'next/link'
+
+interface BreadCrumbItem {
+	displayName: string
+	href: string
+}
+
+interface Props {
+	home: 'landingPage' | 'dashboard'
+	trail?: BreadCrumbItem[]
+	currentPageTitle: string
+}
+
+function DashboardItem() {
+	return (
+		<li>
+			<div className="flex items-center">
+				<Link href="/dashboard" className="link-primary">
+					Dashboard
+				</Link>
+			</div>
+		</li>
+	)
+}
+
+function LandingPageItem() {
+	return (
+		<li>
+			<div className="flex items-center">
+				<Link href="/" className="link-primary">
+					Home
+				</Link>
+			</div>
+		</li>
+	)
+}
+
+export default function BreadCrumbs({ home, trail, currentPageTitle }: Props) {
+	return (
+		<nav aria-label="Breadcrumb" className="flex mb-12">
+			<ol className="flex items-center space-x-4">
+				{home === 'landingPage' ? <LandingPageItem /> : <DashboardItem />}
+				{trail?.map((item) => (
+					<li key={item.href}>
+						<div className="flex items-center">
+							<ChevronRightIcon aria-hidden="true" className="size-5 shrink-0 text-zinc-400" />
+							<Link href={item.href} className="link-primary ml-4">
+								{item.displayName}
+							</Link>
+						</div>
+					</li>
+				))}
+				<li>
+					<div className="flex items-center">
+						<ChevronRightIcon aria-hidden="true" className="size-5 shrink-0 text-zinc-400" />
+						<span aria-current="page" className="ml-4 text-sm font-medium text-blue-400">
+							{currentPageTitle}
+						</span>
+					</div>
+				</li>
+			</ol>
+		</nav>
+	)
+}
