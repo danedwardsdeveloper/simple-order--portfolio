1ebae19d32c0567cb4e44b9cf35f025d3e378438
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-27 17:14:15 +0000

    Add comments

--- src/app/confirm/ConfirmEmailResponse.tsx
+++ src/app/confirm/ConfirmEmailResponse.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Spinner from '@/components/Spinner'
 import { apiPaths, dataTestIdNames } from '@/library/constants'
+import { useUser } from '@/providers/user'
 import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import type { ConfirmEmailPOSTbody, ConfirmEmailPOSTresponse } from '../api/authentication/email/confirm/route'
@@ -8,19 +9,30 @@ import type { ConfirmEmailPOSTbody, ConfirmEmailPOSTresponse } from '../api/auth
 export default function ConfirmEmailResponse() {
 	const searchParams = useSearchParams()
 	const token = searchParams.get('token')
+	const { setUser } = useUser()
 	const [isLoading, setIsLoading] = useState(true)
 	const [message, setMessage] = useState<string | null>(null)
 
+	// ToDo: This needs a lot of work.
+	// The message needs to be more welcoming!
+	// Plus it should handle the edge case where a user confirms the email with a different browser.
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies:
 	useEffect(() => {
 		const confirmEmail = async () => {
 			try {
 				const { message }: ConfirmEmailPOSTresponse = await (
 					await fetch(apiPaths.authentication.email.confirm, {
 						method: 'POST',
+						credentials: 'include',
 						headers: { 'Content-Type': 'application/json' },
 						body: JSON.stringify({ token } satisfies ConfirmEmailPOSTbody),
 					})
 				).json()
+
+				if (message === 'success') {
+					setUser((prev) => (prev ? { ...prev, emailConfirmed: true } : null))
+				}
 
 				setMessage(message)
 			} catch {
