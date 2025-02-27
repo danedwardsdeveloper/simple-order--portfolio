61e5be2e3b9c2a6dada00981cf1f87c0f5168dd8
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-27 17:08:24 +0000

    Navigate away if not a merchant

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,20 +1,36 @@
 'use client'
 import UnauthorisedLinks from '@/components/UnauthorisedLinks'
 import { useUser } from '@/providers/user'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 import AddInventoryForm from './components/AddInventoryForm'
 import InventoryControlPanel from './components/InventoryControlPanel'
 import InventoryList from './components/InventoryList'
 
 export default function InventoryPage() {
 	const { user } = useUser()
+	const router = useRouter()
+
+	useEffect(() => {
+		if (user && user.roles === 'customer') {
+			router.replace('/dashboard')
+		}
+	}, [user, router])
+
 	if (!user) return <UnauthorisedLinks />
+
+	// Only render the inventory UI if user is not a customer to prevent InventoryList from making bad requests
+	if (user.roles === 'customer') {
+		// Enhancement ToDo: Use this page to encourage customer-only users to start a free trial as a merchant
+		return null
+	}
 
 	return (
 		<div className="flex flex-col gap-y-4">
-			<h1>Inventory</h1>
+			<h1 className="">Inventory</h1>
 			<InventoryControlPanel />
-			<InventoryList />
 			<AddInventoryForm />
+			<InventoryList />
 		</div>
 	)
 }
