47d0b454daa83baa191bf976c81d619cd914f832
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-27 17:07:10 +0000

    Add comment

--- src/app/merchants/layout.tsx
+++ src/app/merchants/layout.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
 	title: 'Merchants',
 }
 
+// Don't put the title here as it applies to /merchants/[merchantSlug] too!
 export default function Layout({ children }: { children: ReactNode }) {
 	return <PageContainer>{children}</PageContainer>
 }
