441a5e7639d82c53b114da8ee39cdff1a4751d07
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-27 17:06:24 +0000

    Add DynamicTitle and Orders components

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,7 +1,16 @@
-export default function Orders() {
+'use client'
+import { useUi } from '@/providers/ui'
+import DynamicTitle from './components/DynamicTitle'
+import OrdersMade from './components/OrdersMade'
+import OrdersReceived from './components/OrdersReceived'
+
+export default function OrdersPage() {
+	const { merchantMode } = useUi()
+
 	return (
 		<>
-			<h1>Orders</h1>
+			<DynamicTitle />
+			{merchantMode ? <OrdersReceived /> : <OrdersMade />}
 		</>
 	)
 }
