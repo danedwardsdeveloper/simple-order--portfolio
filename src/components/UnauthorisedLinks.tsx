025a0583a57bd50617371cc8de636548f63ad11b
Dan Edwards <daniel.edwards96@yahoo.com>
2025-02-27 17:04:03 +0000

    Add comment, update styles

--- src/components/UnauthorisedLinks.tsx
+++ src/components/UnauthorisedLinks.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 
+// ToDo: this is a marketing opportunity. Use it!
+
 export default function UnauthorisedLinks() {
 	return (
 		<div className="flex flex-col gap-y-4">
-			<p className="text-red-600">You must be signed in to view this page.</p>
+			<p>You must be signed in to view this page.</p>
 			<div className="flex gap-x-4">
 				<Link href="/sign-in" className="button-secondary">
 					Sign in
