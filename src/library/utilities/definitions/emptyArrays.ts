5cf7efece45705cdeb7a75eaa9631cfdb89a6b30
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-03 10:07:22 +0000

    Delete emptyArrays.ts

--- src/library/utilities/definitions/emptyArrays.ts
+++
@@ -1,8 +0,0 @@
-export function nonEmptyArray<T>(arr: T[] | null | undefined): boolean {
-	return Array.isArray(arr) && arr.length > 0
-}
-
-// I may be able to delete this one in the future...
-export function isEmptyArray<T>(arr: T[] | null | undefined): boolean {
-	return Array.isArray(arr) && arr.length === 0
-}
