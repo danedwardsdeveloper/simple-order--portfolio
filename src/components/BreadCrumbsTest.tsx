573a81de18a39519926c8e61b670c6ac10134c09
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-03 20:33:28 +0000

    Update BreadCrumbsTest.tsx

--- src/components/BreadCrumbsTest.tsx
+++ src/components/BreadCrumbsTest.tsx
@@ -1,12 +1,17 @@
 import { useUi } from '@/providers/ui'
+import { useUser } from '@/providers/user'
 import { ChevronRightIcon } from '@heroicons/react/20/solid'
 
 export default function BreadCrumbsTest() {
+	const { user } = useUser()
 	const { breadCrumbTest } = useUi()
+
+	if (!user) return null
+
 	return (
 		<nav>
 			<ul className="flex gap-x-4 items-center mt-2 mb-8">
-				<li>Link 1</li>
+				<li>{user.businessName}</li>
 				<ChevronRightIcon className="size-5 text-zinc-500" />
 				<li className="font-medium text-blue-600">{breadCrumbTest}</li>
 			</ul>
