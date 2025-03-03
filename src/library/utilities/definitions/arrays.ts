c377a14304ef6d280d0f738300f2f3b56f65abc4
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-03 20:27:48 +0000

    Add isEmptyArray

--- src/library/utilities/definitions/arrays.ts
+++ src/library/utilities/definitions/arrays.ts
@@ -2,6 +2,10 @@ export function containsItems<T>(arr: T[] | null | undefined): boolean {
 	return Array.isArray(arr) && arr.length > 0
 }
 
+export function isEmptyArray<T>(arr: T[] | null | undefined): boolean {
+	return Array.isArray(arr) && arr.length === 0
+}
+
 export function convertEmptyToUndefined<T>(data: T[] | null | undefined): T[] | undefined {
 	if (!Array.isArray(data) || data.length === 0) {
 		return undefined
