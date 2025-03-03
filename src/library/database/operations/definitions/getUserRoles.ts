7c4bab11dcea5453ebeb34804de85c43d99adec1
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-03 20:28:42 +0000

    Add comments, simplify database query

--- src/library/database/operations/definitions/getUserRoles.ts
+++ src/library/database/operations/definitions/getUserRoles.ts
@@ -1,14 +1,21 @@
 import { database } from '@/library/database/connection'
-import { merchantProfiles, relationships } from '@/library/database/schema'
+import { relationships } from '@/library/database/schema'
 import logger from '@/library/logger'
 import type { Roles } from '@/types'
 import { eq } from 'drizzle-orm'
 
 export async function getUserRoles(userId: number): Promise<{ userRole: Roles }> {
 	try {
-		const [isMerchant] = await database.select().from(merchantProfiles).where(eq(merchantProfiles.userId, userId))
+		// Urgent ToDo: This needs a lot of work.
+
+		// A merchant is signified by:
+		// - an active free-trial or subscription, or confirmed customers
+
+		// A customer is signified by:
+		// - confirmed merchants
 
 		const [isCustomer] = await database.select().from(relationships).where(eq(relationships.customerId, userId))
+		const [isMerchant] = await database.select().from(relationships).where(eq(relationships.merchantId, userId))
 
 		const isBoth = isMerchant && isCustomer
 
