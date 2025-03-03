f633fdc5ea0645452993be06ca5cc11b5ce9f578
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-03 20:31:22 +0000

    Move file

---
+++ src/app/orders/[merchantSlug]/components/CustomerFacingProductCard.tsx
@@ -0,0 +1,41 @@
+import { dataTestIdNames } from '@/library/constants'
+import { formatPrice } from '@/library/utilities'
+import type { BrowserSafeCustomerProduct } from '@/types'
+import clsx from 'clsx'
+
+interface Props {
+	product: BrowserSafeCustomerProduct
+	quantity: number
+	onQuantityChange: (quantity: number) => void
+	zebraStripe: boolean
+}
+
+export default function CustomerFacingProductCard({ product, quantity, onQuantityChange, zebraStripe }: Props) {
+	return (
+		<li
+			data-test-id={dataTestIdNames.merchants.customerFacingProductCard}
+			className={clsx('flex flex-col gap-y-2 w-full p-3 rounded-xl', zebraStripe ? 'bg-blue-50' : 'bg-zinc-50')}
+		>
+			<h3 className="text-xl font-medium mb-1">{product.name}</h3>
+			<p className="text-zinc-700 max-w-prose">{product.description}</p>
+			<div className="flex justify-between items-center">
+				<div className="flex gap-x-1 items-center">
+					<span className="text-lg">{formatPrice(product.priceInMinorUnits)}</span>
+				</div>
+				<div>
+					<label htmlFor="quantity" className="text-zinc-600">
+						Quantity
+					</label>
+					<input
+						id="quantity"
+						type="number"
+						min="0"
+						max="1000"
+						value={quantity}
+						onChange={(event) => onQuantityChange(Number.parseInt(event.target.value) || 0)}
+					/>
+				</div>
+			</div>
+		</li>
+	)
+}
