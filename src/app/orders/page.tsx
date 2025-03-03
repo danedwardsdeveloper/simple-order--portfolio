09e8ba8d623c8d887f97717e34f4defc3fc64512
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-03 10:08:26 +0000

    Move dynamic rendering logic here

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,16 +1,33 @@
 'use client'
+import BreadCrumbsTest from '@/components/BreadCrumbsTest'
+import RoleModeButton from '@/components/menubar/RoleModeButton'
 import { useUi } from '@/providers/ui'
-import DynamicTitle from './components/DynamicTitle'
-import OrdersMade from './components/OrdersMade'
-import OrdersReceived from './components/OrdersReceived'
+import { useUser } from '@/providers/user'
+import CustomerFacingOrdersPage from './customerFacingOrdersPage'
+import MerchantFacingOrdersPage from './merchantFacingOrderPage'
+
+/* 
+/orders - Orders dashboard
+/orders/[order-id] - View specific order
+/orders/new/[merchant-slug] - Create new order
+*/
 
 export default function OrdersPage() {
 	const { merchantMode } = useUi()
+	const { user } = useUser()
+
+	if (!user) return null
+
+	const title = user?.roles === 'both' ? (merchantMode ? 'Orders received' : 'Orders made') : 'Orders'
 
 	return (
 		<>
-			<DynamicTitle />
-			{merchantMode ? <OrdersReceived /> : <OrdersMade />}
+			<BreadCrumbsTest />
+			<div className="flex flex-col gap-y-4 items-start">
+				<h1>{title}</h1>
+				<RoleModeButton />
+				{merchantMode ? <MerchantFacingOrdersPage /> : <CustomerFacingOrdersPage />}
+			</div>
 		</>
 	)
 }
