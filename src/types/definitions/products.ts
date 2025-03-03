4a1802c74614a90efa0f916232c336d7d9cb2d81
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-03 10:06:17 +0000

    Rename type

--- src/types/definitions/products.ts
+++ src/types/definitions/products.ts
@@ -1,7 +1,7 @@
 import type { products } from '@/library/database/schema'
 
 export type Product = typeof products.$inferSelect
-export type NewProduct = typeof products.$inferInsert
+export type ProductInsertValues = typeof products.$inferInsert
 
 // Keep the id here as it's useful for React mapping as it's guaranteed to be unique
 export type BrowserSafeMerchantProduct = Pick<Product, 'id' | 'name' | 'description' | 'priceInMinorUnits' | 'customVat' | 'deletedAt'>
