5dd8285714c33e9be89ce5cb0b6c0839014f555c
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-03 10:08:02 +0000

    Simplify menu items

--- src/components/menubar/SignedInMenu.tsx
+++ src/components/menubar/SignedInMenu.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { useUi } from '@/providers/ui'
 import HomePageLink from './HomePageLink'
 import MenuItem from './MenuItem'
@@ -7,33 +6,17 @@ import MenuItem from './MenuItem'
 export default function SignedInMenu() {
 	const { merchantMode } = useUi()
 
-	function DashboardLink() {
-		return <MenuItem href="/dashboard" text="Dashboard" />
-	}
-
-	function MerchantModeLinks() {
-		return (
-			<>
-				<MenuItem href="/inventory" text="Inventory" />
-				<MenuItem href="/customers" text="Customers" />
-			</>
-		)
-	}
-
-	function CustomerModeLinks() {
-		return <MenuItem href="/merchants" text="Merchants" />
-	}
-
 	return (
 		<nav className="fixed inset-x-0 top-0 flex h-14 bg-white/70 backdrop-blur border-b-2 border-neutral-100 z-menubar ">
 			<div className="w-full max-w-4xl mx-auto px-4 lg:px-0 flex items-center justify-between">
 				<div className="flex h-full items-center gap-x-3">
 					<HomePageLink />
-					<DashboardLink />
+					<MenuItem href="/dashboard" text="Dashboard" />
 				</div>
 				<div className="flex h-full items-center gap-x-6">
+					{merchantMode && <MenuItem href="/inventory" text="Inventory" />}
+					{merchantMode && <MenuItem href="/customers" text="Customers" />}
 					<MenuItem href="/orders" text="Orders" />
-					{merchantMode ? <MerchantModeLinks /> : <CustomerModeLinks />}
 					<MenuItem href="/settings" text="Settings" />
 				</div>
 			</div>
