990334ba2f3d5e2a57b45c74c5479a6557508968
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-04 18:00:52 +0000

    Update BreadCrumbs

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import BreadCrumbs from '@/components/BreadCrumbs'
+import { SignedOutBreadCrumbs } from '@/components/BreadCrumbs'
 import PageContainer from '@/components/PageContainer'
 import { dynamicBaseURL } from '@/library/environment/publicVariables'
 import type { Metadata } from 'next'
@@ -38,7 +38,7 @@ export default async function Page({
 
 	return (
 		<PageContainer>
-			<BreadCrumbs home="landingPage" trail={[{ href: '/articles', displayName: 'Articles' }]} currentPageTitle={article.displayTitle} />
+			<SignedOutBreadCrumbs trail={[{ href: '/articles', displayName: 'Articles' }]} currentPageTitle={article.displayTitle} />
 			<h1>{article.displayTitle}</h1>
 			<div className="flex flex-col max-w-prose gap-y-4">
 				{article.paragraphs.map((paragraph) => (
