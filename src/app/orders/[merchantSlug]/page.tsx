9af521dff850308714bbfcb7d9b486e4f52aced5
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-04 18:00:07 +0000

    Create page.tsx

---
+++ src/app/orders/[merchantSlug]/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { SignedInBreadCrumbs } from '@/components/BreadCrumbs'
+import { useUser } from '@/providers/user'
+import { use } from 'react'
+
+export default function Page({
+	params,
+}: {
+	params: Promise<{
+		merchantSlug: string
+	}>
+}) {
+	const resolvedParams = use(params)
+	const merchantSlug = resolvedParams.merchantSlug
+	const { user, confirmedMerchants } = useUser()
+	const merchantDetails = confirmedMerchants?.find((merchant) => merchant.slug === merchantSlug)
+
+	if (!user) return null
+
+	return (
+		<>
+			<SignedInBreadCrumbs
+				businessName={user.businessName}
+				trail={[{ displayName: 'Orders', href: '/orders' }]}
+				currentPageTitle={merchantDetails?.businessName || merchantSlug}
+			/>
+			<p>ToDo: Customer-facing page showing all orders from a specific merchant</p>
+		</>
+	)
+}
