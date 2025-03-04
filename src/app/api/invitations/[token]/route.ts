d4cf937efcfc00c9ff58401be91046cfcec601e8
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-04 18:01:35 +0000

    Fix transaction error handling

--- src/app/api/invitations/[token]/route.ts
+++ src/app/api/invitations/[token]/route.ts
@@ -3,6 +3,7 @@ import { database } from '@/library/database/connection'
 import { invitations, relationships, users } from '@/library/database/schema'
 import { sendEmail } from '@/library/email/sendEmail'
 import logger from '@/library/logger'
+import { createMerchantSlug } from '@/library/utilities'
 import { createCookieWithToken, createSessionCookieWithToken } from '@/library/utilities/server'
 import type {
 	BaseUserInsertValues,
@@ -19,13 +20,12 @@ import { cookies } from 'next/headers'
 import { type NextRequest, NextResponse } from 'next/server'
 import { validate } from 'uuid'
 
-export type InvitationsTokenPOSTbody = InvitedCustomerBrowserInputValues
+export type InvitationsTokenPATCHbody = InvitedCustomerBrowserInputValues
 
-// PATCH accept an invitation
+// PATCH accept an invitation (creates a relationship)
 // Ask for more details if user is new to the site
 // Sign the user in
-// Urgent ToDo: This doesn't work!
-export interface InvitationsTokenPOSTresponse {
+export interface InvitationsTokenPATCHresponse {
 	message:
 		| typeof basicMessages.success
 		| typeof basicMessages.serverError
@@ -37,52 +37,69 @@ export interface InvitationsTokenPOSTresponse {
 	senderBusinessName?: string
 }
 
-export async function POST(
+export async function PATCH(
 	request: NextRequest,
 	{ params }: { params: Promise<{ token: string }> },
-): Promise<NextResponse<InvitationsTokenPOSTresponse>> {
+): Promise<NextResponse<InvitationsTokenPATCHresponse>> {
 	const cookieStore = await cookies()
 
-	let transactionErrorMessage = null
-	let transactionErrorCode = null
+	let transactionErrorMessage = undefined
+	let transactionErrorCode = undefined
+
+	const partialDetailsProvided = undefined
+	const allDetailsProvided = undefined
+
+	let firstName = undefined
+	let lastName = undefined
+	let businessName = undefined
+	let password = undefined
+	let staySignedIn = undefined
+
+	// Parse the body conditionally
+	// Calling request.json() on an empty body throws an error
+	if (request.headers.get('content-length') !== '0' && request.headers.get('content-type')?.includes('application/json')) {
+		const body = await request.json()
+		firstName = body.firstName
+		lastName = body.lastName
+		businessName = body.businessName
+		password = body.password
+		staySignedIn = body.staySignedIn
+
+		logger.debug('Provided details: ', firstName, lastName, businessName, password, staySignedIn)
+	}
+
+	if (partialDetailsProvided) {
+		logger.info('Not enough details provided to create new account')
+		// ToDo: make this more specific
+		return NextResponse.json({ message: 'missing fields' }, { status: httpStatus.http400badRequest })
+	}
+
+	const token = (await params).token
+
+	if (!token) {
+		logger.warn(`${apiPaths.invitations.accept}: token missing`)
+		return NextResponse.json({ message: tokenMessages.tokenMissing }, { status: httpStatus.http400badRequest })
+	}
+
+	if (!validate(token)) {
+		return NextResponse.json({ message: tokenMessages.tokenInvalid }, { status: httpStatus.http400badRequest })
+	}
+
+	const [foundInvitation]: Invitation[] = await database.select().from(invitations).where(eq(invitations.token, token)).limit(1)
+
+	if (!foundInvitation) {
+		return NextResponse.json({ message: 'invitation not found' }, { status: httpStatus.http400badRequest })
+	}
+
+	const recipientEmail = foundInvitation.email
+
+	const [existingUser]: DangerousBaseUser[] = await database.select().from(users).where(eq(users.email, recipientEmail))
+
+	const [senderDetails]: DangerousBaseUser[] = await database.select().from(users).where(eq(users.id, foundInvitation.senderUserId))
+
+	const senderBusinessName = senderDetails.businessName
 
 	try {
-		const { firstName, lastName, businessName, password, staySignedIn }: InvitationsTokenPOSTbody = await request.json()
-		logger.debug(firstName, lastName, businessName, password, staySignedIn)
-
-		const allDetailsProvided = Boolean(firstName && lastName && businessName && password)
-		const partialDetailsProvided = Boolean(firstName || lastName || businessName || password) && !allDetailsProvided
-
-		if (partialDetailsProvided) {
-			logger.info('Not enough details provided to create new account')
-			// ToDo: make this more specific
-			return NextResponse.json({ message: 'missing fields' }, { status: httpStatus.http400badRequest })
-		}
-
-		const token = (await params).token
-
-		if (!token) {
-			return NextResponse.json({ message: tokenMessages.tokenMissing }, { status: httpStatus.http400badRequest })
-		}
-
-		if (!validate(token)) {
-			return NextResponse.json({ message: tokenMessages.tokenInvalid }, { status: httpStatus.http400badRequest })
-		}
-
-		const [foundInvitation]: Invitation[] = await database.select().from(invitations).where(eq(invitations.token, token)).limit(1)
-
-		if (!foundInvitation) {
-			return NextResponse.json({ message: 'invitation not found' }, { status: httpStatus.http400badRequest })
-		}
-
-		const recipientEmail = foundInvitation.email
-
-		const [existingUser]: DangerousBaseUser[] = await database.select().from(users).where(eq(users.email, recipientEmail))
-
-		const [senderDetails]: DangerousBaseUser[] = await database.select().from(users).where(eq(users.id, foundInvitation.senderUserId))
-
-		const senderBusinessName = senderDetails.businessName
-
 		if (existingUser) {
 			await database.transaction(async (tx) => {
 				// Transaction: Create the relationship
@@ -112,7 +129,7 @@ export async function POST(
 				cookieStore.set(createSessionCookieWithToken(existingUser.id))
 			}
 
-			// - Return 201 with merchant details
+			logger.info(`PATCH ${apiPaths.invitations.accept}: existing user found, relationship created`)
 			return NextResponse.json({ message: basicMessages.success, senderBusinessName }, { status: httpStatus.http201created })
 		}
 
@@ -132,6 +149,8 @@ export async function POST(
 					firstName,
 					lastName,
 					businessName,
+					// ToDo: add multiple attempts!
+					slug: createMerchantSlug(businessName),
 					hashedPassword,
 					emailConfirmed: true, // They've just clicked a link from an email!
 					cachedTrialExpired: false, // They haven't started a trial, and they're just a customer at this point
@@ -188,6 +207,7 @@ export async function POST(
 				accountActive: false, // This is a new customer-only, so they don't have a subscription but can still use the site to make orders
 			}
 
+			logger.info(`PATCH ${apiPaths.invitations.accept}: created new user: `, compositeUser)
 			return NextResponse.json(
 				{ message: basicMessages.success, createdUser: compositeUser, senderBusinessName },
 				{ status: httpStatus.http200ok },
