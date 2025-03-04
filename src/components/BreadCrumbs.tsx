5906bee6131cacb497a7773cf9d73ed6212f3298
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-04 17:54:16 +0000

    Update BreadCrumbs.tsx

--- src/components/BreadCrumbs.tsx
+++ src/components/BreadCrumbs.tsx
@@ -6,43 +6,24 @@ interface BreadCrumbItem {
 	href: string
 }
 
-interface Props {
-	home: 'landingPage' | 'dashboard'
+// ToDo: Add business name
+
+interface SignedOutProps {
 	trail?: BreadCrumbItem[]
 	currentPageTitle: string
 }
 
-// ToDo: Add business name
-
-function UserItem() {
-	return (
-		<li>
-			<div className="flex items-center">
-				<Link href="/dashboard" className="link-primary">
-					Dashboard
-				</Link>
-			</div>
-		</li>
-	)
-}
-
-function LandingPageItem() {
-	return (
-		<li>
-			<div className="flex items-center">
-				<Link href="/" className="link-primary">
-					Home
-				</Link>
-			</div>
-		</li>
-	)
-}
-
-export default function BreadCrumbs({ home, trail, currentPageTitle }: Props) {
+export function SignedOutBreadCrumbs({ trail, currentPageTitle }: SignedOutProps) {
 	return (
 		<nav aria-label="Breadcrumb" className="flex mb-12">
 			<ol className="flex items-center space-x-4">
-				{home === 'landingPage' ? <LandingPageItem /> : <UserItem />}
+				<li>
+					<div className="flex items-center">
+						<Link href="/" className="link-primary">
+							Home
+						</Link>
+					</div>
+				</li>
 				{trail?.map((item) => (
 					<li key={item.href}>
 						<div className="flex items-center">
@@ -61,6 +42,48 @@ export default function BreadCrumbs({ home, trail, currentPageTitle }: Props) {
 						</span>
 					</div>
 				</li>
+			</ol>
+		</nav>
+	)
+}
+
+interface SignedInProps {
+	businessName: string
+	trail?: BreadCrumbItem[]
+	currentPageTitle?: string
+}
+
+export function SignedInBreadCrumbs({ businessName, trail, currentPageTitle }: SignedInProps) {
+	return (
+		<nav aria-label="Breadcrumb" className="flex mb-12">
+			<ol className="flex items-center space-x-4">
+				<li>
+					<div className="flex items-center">
+						<Link href="/dashboard" className="link-primary">
+							{businessName}
+						</Link>
+					</div>
+				</li>
+				{trail?.map((item) => (
+					<li key={item.href}>
+						<div className="flex items-center">
+							<ChevronRightIcon aria-hidden="true" className="size-5 shrink-0 text-zinc-400" />
+							<Link href={item.href} className="link-primary ml-4">
+								{item.displayName}
+							</Link>
+						</div>
+					</li>
+				))}
+				{currentPageTitle && (
+					<li>
+						<div className="flex items-center">
+							<ChevronRightIcon aria-hidden="true" className="size-5 shrink-0 text-zinc-400" />
+							<span aria-current="page" className="ml-4 text-sm font-medium text-blue-400">
+								{currentPageTitle}
+							</span>
+						</div>
+					</li>
+				)}
 			</ol>
 		</nav>
 	)
