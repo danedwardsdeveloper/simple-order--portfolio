18d239439e68b56c253b629ee5beff344021a005
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-05 09:13:14 +0000

    Add BrowserSafeOrder

--- src/types/definitions/orders.ts
+++ src/types/definitions/orders.ts
@@ -8,3 +8,15 @@ export type OrderItemInsertValues = typeof orderItems.$inferInsert
 
 // This repeats the values from orderStatusEnum but I can't see any other way to get the values
 export type OrderStatus = 'pending' | 'completed' | 'cancelled'
+
+export interface BrowserSafeOrder {
+	id: number
+	customerBusinessName: string
+	requestedDeliveryDate: Date
+	adminOnlyNote?: string
+	customerNote?: string
+	status: OrderStatus
+	items: OrderItem[]
+	createdAt: Date
+	updatedAt: Date
+}
