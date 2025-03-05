65e06ce5090f121d5785cf3f16e016818708b217
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-05 16:12:10 +0000

    Fix route and improve error handling

--- src/app/api/authentication/email/confirm/route.ts
+++ src/app/api/authentication/email/confirm/route.ts
@@ -1,91 +1,102 @@
-import { authenticationMessages, basicMessages, httpStatus, tokenMessages } from '@/library/constants'
+import { apiPaths, authenticationMessages, basicMessages, cookieDurations, httpStatus, tokenMessages } from '@/library/constants'
 import { database } from '@/library/database/connection'
+import { checkActiveSubscriptionOrTrial, getUserRoles } from '@/library/database/operations'
 import { confirmationTokens, users } from '@/library/database/schema'
 import logger from '@/library/logger'
-import type { ConfirmationToken, DangerousBaseUser, EmailTokenMessages } from '@/types'
+import { sanitiseDangerousBaseUser } from '@/library/utilities'
+import { createCookieWithToken } from '@/library/utilities/server'
+import type { BrowserSafeCompositeUser, ConfirmationToken, DangerousBaseUser } from '@/types'
 import { eq } from 'drizzle-orm'
+import { cookies } from 'next/headers'
 import { type NextRequest, NextResponse } from 'next/server'
 
-export interface ConfirmEmailPOSTresponse {
-	message: typeof basicMessages.success | typeof basicMessages.serverError | EmailTokenMessages
+export interface AuthenticationEmailConfirmPOSTresponse {
+	message:
+		| typeof basicMessages.success
+		| typeof basicMessages.serverError
+		| typeof tokenMessages.tokenMissing
+		| typeof tokenMessages.tokenInvalid
+		| typeof tokenMessages.tokenExpired
+		| typeof tokenMessages.userNotFound
+		| typeof authenticationMessages.alreadyConfirmed
+		| 'transaction error updating user'
+		| 'transaction error expiring token'
+	confirmedUser?: BrowserSafeCompositeUser
 }
 
-export interface ConfirmEmailPOSTbody {
-	token: string | null
+export interface AuthenticationEmailConfirmPOSTbody {
+	token: string
 }
 
-export async function POST(request: NextRequest): Promise<NextResponse<ConfirmEmailPOSTresponse>> {
-	const { token }: ConfirmEmailPOSTbody = await request.json()
+export const routeDetail = `POST ${apiPaths.authentication.email.confirm}: `
 
-	if (!token) {
-		return NextResponse.json({ message: tokenMessages.tokenMissing }, { status: httpStatus.http401unauthorised })
-	}
-
-	// ToDo: this shouldn't be a transaction!
-	// ToDo: remove unnecessary stages from this transaction and catch errors properly
-	let transactionFailureMessage = null
-	let transactionFailureStatus = null
+export async function POST(request: NextRequest): Promise<NextResponse<AuthenticationEmailConfirmPOSTresponse>> {
+	let transactionFailureMessage = undefined
+	let transactionFailureStatus = undefined
 	try {
-		const result = await database.transaction(async (tx) => {
-			const [confirmationToken]: ConfirmationToken[] = await tx
-				.select()
-				.from(confirmationTokens)
-				.where(eq(confirmationTokens.token, token))
-				.limit(1)
+		const { token }: AuthenticationEmailConfirmPOSTbody = await request.json()
 
-			if (!confirmationToken) {
-				transactionFailureMessage = tokenMessages.tokenInvalid
-				transactionFailureStatus = httpStatus.http401unauthorised
-				throw new Error()
-			}
+		if (!token) {
+			logger.warn(routeDetail, 'Email confirmation token not found')
+			return NextResponse.json({ message: tokenMessages.tokenMissing }, { status: httpStatus.http401unauthorised })
+		}
 
-			if (confirmationToken.usedAt) {
-				const [user]: DangerousBaseUser[] = await tx.select().from(users).where(eq(users.id, confirmationToken.userId)).limit(1)
+		const [foundToken]: ConfirmationToken[] = await database.select().from(confirmationTokens).where(eq(confirmationTokens.token, token))
 
-				if (user?.emailConfirmed) {
-					transactionFailureMessage = authenticationMessages.alreadyConfirmed
-					transactionFailureStatus = httpStatus.http409conflict
-				} else {
-					transactionFailureMessage = 'token used'
-					transactionFailureStatus = httpStatus.http401unauthorised
-				}
-				return false
-			}
+		if (!foundToken) {
+			return NextResponse.json({ message: tokenMessages.tokenInvalid }, { status: httpStatus.http401unauthorised })
+		}
 
-			if (new Date() > new Date(confirmationToken.expiresAt)) {
-				transactionFailureMessage = tokenMessages.tokenExpired
-				transactionFailureStatus = httpStatus.http401unauthorised
-				throw new Error()
-			}
+		const [foundDangerousUser]: DangerousBaseUser[] = await database.select().from(users).where(eq(users.id, foundToken.userId))
 
-			const [user] = await tx.select().from(users).where(eq(users.id, confirmationToken.userId)).limit(1)
+		if (!foundDangerousUser) {
+			return NextResponse.json({ message: tokenMessages.userNotFound }, { status: httpStatus.http404notFound })
+		}
 
-			if (!user) {
-				transactionFailureMessage = tokenMessages.userNotFound
-				transactionFailureStatus = httpStatus.http404notFound
-				throw new Error()
-			}
+		if (foundDangerousUser.emailConfirmed) {
+			logger.info(routeDetail, 'Email already confirmed.', foundDangerousUser)
+			return NextResponse.json({ message: authenticationMessages.alreadyConfirmed }, { status: httpStatus.http202accepted })
+		}
 
-			await tx.update(users).set({ emailConfirmed: true }).where(eq(users.id, user.id))
+		if (new Date() > new Date(foundToken.expiresAt)) {
+			return NextResponse.json({ message: tokenMessages.tokenExpired }, { status: httpStatus.http401unauthorised })
+		}
 
-			await tx.update(confirmationTokens).set({ usedAt: new Date() }).where(eq(confirmationTokens.id, confirmationToken.id))
-			return true
+		await database.transaction(async (tx) => {
+			transactionFailureMessage = 'transaction error updating user'
+			transactionFailureStatus = httpStatus.http500serverError
+			await tx.update(users).set({ emailConfirmed: true }).where(eq(users.id, foundDangerousUser.id))
+
+			transactionFailureMessage = 'transaction error expiring token'
+			await tx.update(confirmationTokens).set({ usedAt: new Date() }).where(eq(confirmationTokens.id, foundToken.id))
+
+			transactionFailureMessage = undefined
+			transactionFailureStatus = undefined
 		})
 
-		if (!result) {
-			throw new Error()
+		const { userRole } = await getUserRoles(foundDangerousUser.id)
+		const { activeSubscriptionOrTrial } = await checkActiveSubscriptionOrTrial(foundDangerousUser.id)
+
+		const confirmedUser: BrowserSafeCompositeUser = {
+			...sanitiseDangerousBaseUser(foundDangerousUser),
+			roles: userRole,
+			activeSubscriptionOrTrial,
 		}
 
-		// Optimisation ToDo: Handle sign in if not already
+		const cookieStore = await cookies()
+		cookieStore.set(createCookieWithToken(foundDangerousUser.id, cookieDurations.oneYear))
 
-		return NextResponse.json({ message: basicMessages.success }, { status: httpStatus.http200ok })
+		return NextResponse.json({ message: basicMessages.success, confirmedUser }, { status: httpStatus.http200ok })
 	} catch (error) {
 		if (transactionFailureStatus && transactionFailureMessage) {
-			logger.error('Transaction failure: ', transactionFailureMessage)
-			return NextResponse.json({ message: transactionFailureMessage }, { status: transactionFailureStatus })
+			logger.error(routeDetail, 'Transaction failure: ', transactionFailureMessage)
+			return NextResponse.json(
+				{ message: transactionFailureMessage || 'unknown transaction error' },
+				{ status: transactionFailureStatus | httpStatus.http500serverError },
+			)
 		}
 
-		logger.error('Transaction failed: ', error)
+		logger.error(routeDetail, 'Uncaught error: ', error)
 		return NextResponse.json({ message: basicMessages.serverError }, { status: httpStatus.http500serverError })
 	}
 }
