e880655e681565df73d7d363f2a743dd312f3bbf
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-05 12:09:06 +0000

    Improve UX

--- src/app/accept-invitation/[token]/page.tsx
+++ src/app/accept-invitation/[token]/page.tsx
@@ -3,18 +3,30 @@ import type { InvitationsTokenPATCHresponse } from '@/app/api/invitations/[token
 import Spinner from '@/components/Spinner'
 import { apiPaths } from '@/library/constants'
 import { dynamicBaseURL } from '@/library/environment/publicVariables'
-import { useParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useNotifications } from '@/providers/notifications'
+import { useUi } from '@/providers/ui'
+import { useUser } from '@/providers/user'
+import { useParams, useRouter } from 'next/navigation'
+import { useEffect, useRef, useState } from 'react'
 import urlJoin from 'url-join'
 import CompleteRegistrationForm from '../components/CompleteRegistrationForm'
 
 export default function AcceptInvitationPage() {
 	const { token } = useParams<{ token: string }>()
-	const [status, setStatus] = useState<'checking' | 'please provide details' | 'success' | 'error'>('checking')
+	const hasCheckedInvitation = useRef(false) // Prevent double fetching in development
+	const [status, setStatus] = useState<'checking' | 'please provide details' | 'error' | 'relationship exists'>('checking')
 	const [errorMessage, setErrorMessage] = useState('')
+	const [senderBusinessName, setSenderBusinessName] = useState('')
+	const { setMerchantMode } = useUi()
+	const { setUser, setConfirmedMerchants } = useUser()
+	const { createNotification } = useNotifications()
+	const router = useRouter()
 
 	useEffect(() => {
-		checkInvitation()
+		if (!hasCheckedInvitation.current) {
+			checkInvitation()
+			hasCheckedInvitation.current = true
+		}
 	}, [])
 
 	async function checkInvitation() {
@@ -22,22 +34,36 @@ export default function AcceptInvitationPage() {
 
 		try {
 			setStatus('checking')
-			const { message }: InvitationsTokenPATCHresponse = await (
+			const { message, senderDetails, createdUser, existingUser }: InvitationsTokenPATCHresponse = await (
 				await fetch(urlJoin(dynamicBaseURL, apiPaths.invitations.base, token), {
 					method: 'PATCH',
 					headers: { 'Content-Type': 'application/json' },
-					body: JSON.stringify(token),
+					body: JSON.stringify({}),
 				})
 			).json()
 
+			setMerchantMode(false)
+			if (createdUser || existingUser) setUser(createdUser || existingUser || null)
+			if (senderDetails) {
+				setSenderBusinessName(senderDetails.businessName)
+				setConfirmedMerchants((prevMerchants) => [...(prevMerchants || []), senderDetails])
+			}
+
 			switch (message) {
 				case 'success':
-					setStatus('success')
+					createNotification({
+						level: 'success',
+						title: 'Success',
+						message: `You are now a confirmed customer of ${senderDetails?.businessName}`,
+					})
+					router.push('/orders')
 					break
 				case 'please provide details':
 					setStatus('please provide details')
 					break
-				// Enhancement ToDo: add edge case feedback
+				case 'relationship exists':
+					setStatus('relationship exists')
+					break
 				case 'invitation not found':
 				case 'missing fields':
 				case 'server error':
@@ -74,12 +100,13 @@ export default function AcceptInvitationPage() {
 		)
 	}
 
-	if (status === 'success') {
+	if (status === 'relationship exists') {
 		return (
-			<div className="max-w-md mx-auto mt-8 p-4 border rounded-lg bg-green-50 text-green-700">
-				{/* UX ToDo: This is awful */}
-				<p className="text-center">Successfully connected! You can now close this window.</p>
-			</div>
+			<p className="max-w-md mx-auto mt-8 p-4 border rounded-lg bg-green-50 text-green-700">
+				{senderBusinessName
+					? `This invitation from ${senderBusinessName} has already been accepted`
+					: 'You are already a confirmed customer of this merchant'}
+			</p>
 		)
 	}
 
