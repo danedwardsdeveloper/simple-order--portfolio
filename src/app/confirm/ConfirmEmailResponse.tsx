80783624aad9199898a4bf05cbd1f0850c811da4
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-05 16:09:41 +0000

    Improve UX

--- src/app/confirm/ConfirmEmailResponse.tsx
+++ src/app/confirm/ConfirmEmailResponse.tsx
@@ -1,40 +1,52 @@
 'use client'
 import Spinner from '@/components/Spinner'
 import { apiPaths, dataTestIdNames } from '@/library/constants'
+import { useNotifications } from '@/providers/notifications'
 import { useUser } from '@/providers/user'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
-import type { ConfirmEmailPOSTbody, ConfirmEmailPOSTresponse } from '../api/authentication/email/confirm/route'
+import type { AuthenticationEmailConfirmPOSTbody, AuthenticationEmailConfirmPOSTresponse } from '../api/authentication/email/confirm/route'
 
 export default function ConfirmEmailResponse() {
 	const searchParams = useSearchParams()
 	const token = searchParams.get('token')
 	const { setUser } = useUser()
+	const { createNotification } = useNotifications()
+	const router = useRouter()
 	const [isLoading, setIsLoading] = useState(true)
 	const [message, setMessage] = useState<string | null>(null)
 
-	// ToDo: This needs a lot of work.
-	// The message needs to be more welcoming!
-	// Plus it should handle the edge case where a user confirms the email with a different browser.
-
 	// biome-ignore lint/correctness/useExhaustiveDependencies:
 	useEffect(() => {
-		const confirmEmail = async () => {
+		async function confirmEmail() {
+			if (!token) return
 			try {
-				const { message }: ConfirmEmailPOSTresponse = await (
+				const { message, confirmedUser }: AuthenticationEmailConfirmPOSTresponse = await (
 					await fetch(apiPaths.authentication.email.confirm, {
 						method: 'POST',
-						credentials: 'include',
 						headers: { 'Content-Type': 'application/json' },
-						body: JSON.stringify({ token } satisfies ConfirmEmailPOSTbody),
+						body: JSON.stringify({ token } satisfies AuthenticationEmailConfirmPOSTbody),
 					})
 				).json()
 
+				if (confirmedUser) setUser(confirmedUser)
 				if (message === 'success') {
-					setUser((prev) => (prev ? { ...prev, emailConfirmed: true } : null))
+					createNotification({
+						level: 'success',
+						title: 'Success',
+						message: 'Thank you for confirming your email',
+					})
+					router.push('/dashboard')
+				} else if (message === 'already confirmed') {
+					createNotification({
+						level: 'info',
+						title: 'Already confirmed',
+						message: 'Your email address was already confirmed',
+					})
+					router.push('/dashboard')
+				} else {
+					setMessage(message)
 				}
-
-				setMessage(message)
 			} catch {
 				setMessage('An error occurred while confirming your email')
 			} finally {
@@ -47,6 +59,14 @@ export default function ConfirmEmailResponse() {
 		}
 	}, [token])
 
+	if (isLoading)
+		return (
+			<>
+				<h1>Confirming your email...</h1>
+				<Spinner />
+			</>
+		)
+
 	return (
 		<>
 			{isLoading && (
@@ -54,10 +74,6 @@ export default function ConfirmEmailResponse() {
 					<Spinner />
 					<span>Confirming your email...</span>
 				</div>
-			)}
-
-			{!isLoading && !message && (
-				<div data-test-id={dataTestIdNames.emailConfirmation.default}>Please wait while we confirm your email address</div>
 			)}
 
 			{!isLoading && message && <div data-test-id={dataTestIdNames.emailConfirmation.response}>{message}</div>}
