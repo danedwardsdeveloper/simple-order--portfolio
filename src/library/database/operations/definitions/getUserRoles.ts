bde4c936c61d56cb10c28c6a59a6f292e43f2470
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-10 12:20:26 +0000

    Make queries more efficient with limit(1)

--- src/library/database/operations/definitions/getUserRoles.ts
+++ src/library/database/operations/definitions/getUserRoles.ts
@@ -1,21 +1,20 @@
 import { database } from '@/library/database/connection'
-import { freeTrials, relationships, subscriptions } from '@/library/database/schema'
+import { freeTrials, invitations, relationships, subscriptions } from '@/library/database/schema'
 import logger from '@/library/logger'
-import type { Roles } from '@/types'
+import type { DangerousBaseUser, Roles } from '@/types'
 import { and, eq, gt } from 'drizzle-orm'
 
 // Optimisation ToDo: Merge this with checkActiveSubscriptionOrTrial as they check the same tables and are mostly used together
 
-export async function getUserRoles(userId: number): Promise<{ userRole: Roles }> {
+export async function getUserRoles(user: DangerousBaseUser): Promise<{ userRole: Roles }> {
 	try {
 		// A merchant is signified by:
-		// - an active free-trial
-		// OR
-		// - an active subscription
-		// - OR
+		// - an active free-trial OR
+		// - an active subscription OR
 		// - a relationship where the user's ID is in the merchant column
 
 		// A customer is signified by:
+		// An invitation where the user's email is the recipient email OR
 		// - a relationship where the user's ID is in the customer column
 
 		let isMerchant = false
@@ -23,7 +22,8 @@ export async function getUserRoles(userId: number): Promise<{ userRole: Roles }>
 		const [foundFreeTrial] = await database
 			.select()
 			.from(freeTrials)
-			.where(and(eq(freeTrials.userId, userId), gt(freeTrials.endDate, new Date())))
+			.where(and(eq(freeTrials.userId, user.id), gt(freeTrials.endDate, new Date())))
+			.limit(1)
 
 		if (foundFreeTrial) isMerchant = true
 
@@ -31,17 +31,27 @@ export async function getUserRoles(userId: number): Promise<{ userRole: Roles }>
 			const [validSubscription] = await database
 				.select()
 				.from(subscriptions)
-				.where(and(gt(subscriptions.currentPeriodEnd, new Date()), eq(subscriptions.userId, userId)))
+				.where(and(gt(subscriptions.currentPeriodEnd, new Date()), eq(subscriptions.userId, user.id)))
+				.limit(1)
 
 			if (validSubscription) isMerchant = true
 		}
 
 		if (!isMerchant) {
-			const [relationshipsAsMerchant] = await database.select().from(relationships).where(eq(relationships.merchantId, userId))
+			const [relationshipsAsMerchant] = await database.select().from(relationships).where(eq(relationships.merchantId, user.id)).limit(1)
 			if (relationshipsAsMerchant) isMerchant = true
 		}
 
-		const [isCustomer] = await database.select().from(relationships).where(eq(relationships.customerId, userId))
+		let isCustomer = false
+
+		const [foundRelationship] = await database.select().from(relationships).where(eq(relationships.customerId, user.id)).limit(1)
+
+		if (foundRelationship) isCustomer = true
+
+		if (!isCustomer) {
+			const [foundInvitation] = await database.select().from(invitations).where(eq(invitations.email, user.email)).limit(1)
+			if (foundInvitation) isCustomer = true
+		}
 
 		const isBoth = isMerchant && isCustomer
 
