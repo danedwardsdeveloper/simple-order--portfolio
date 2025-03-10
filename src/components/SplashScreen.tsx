68341325081daf9b7316f340aa5d062e6e597489
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-10 12:25:37 +0000

    make full height

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -5,7 +5,7 @@ export default function SplashScreen() {
 	return (
 		<div
 			data-component="SplashScreen"
-			className="fixed inset-0 z-splash-screen bg-white flex items-center justify-center transition-opacity duration-500 pointer-events-none"
+			className="inset-0 fixed h-full z-splash-screen bg-white flex items-center justify-center transition-opacity duration-500 pointer-events-none"
 		>
 			<div className="flex items-center gap-x-4 h-12 text-blue-600">
 				<div className="size-6 text-blue-600">
