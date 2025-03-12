0d9a655ad9d1fa961b596bf6359c37d199514b00
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-12 09:06:10 +0000

    Update styles

--- src/components/menubar/RoleModeButton.tsx
+++ src/components/menubar/RoleModeButton.tsx
@@ -10,7 +10,7 @@ export default function RoleModeButton() {
 	if (user?.roles !== 'both') return null
 
 	return (
-		<div className="px-4 pt-2 pb-8 rounded-xl bg-blue-50 flex flex-col gap-y-4 text-center">
+		<div className="px-4 pt-2 pb-8 rounded-xl bg-blue-50 flex flex-col gap-y-4 text-center max-w-xl items-center">
 			<h2>Mode</h2>
 			<ToggleWithLabel
 				enabled={merchantMode}
