58198dd0b96961f0cfa370dd13377aaaed3a5a2e
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-13 11:28:21 +0000

    Merge formatting utilities

---
+++ src/library/utilities/definitions/formatting.ts
@@ -0,0 +1,38 @@
+import { format } from 'date-fns'
+
+/**
+ * Example:
+ * 3:45 pm, Monday, 13 March 2025
+ */
+export function formatTimeAndDate(date: Date): string {
+	const time = format(date, 'h:mm').toLowerCase()
+	const meridiem = format(date, 'a').toLowerCase()
+
+	return `${time} ${meridiem}, ${format(date, 'EEEE, d MMMM yyyy')}`
+}
+
+/**
+ * Example
+ * 7 September
+ */
+export function formatDate(date: Date): string {
+	return format(date, 'd MMMM')
+}
+
+export function capitaliseFirstLetter(str: string): string {
+	if (!str) return str
+	return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+export function formatPrice(pence: number): string {
+	if (pence < 100) {
+		return `${pence}p`
+	}
+
+	const pounds = pence / 100
+	if (Number.isInteger(pounds)) {
+		return `£${pounds}`
+	}
+
+	return `£${pounds.toFixed(2)}`
+}
