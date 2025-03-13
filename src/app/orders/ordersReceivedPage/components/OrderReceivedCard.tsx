50da4ac5bb3d29dc2f153bdcef415a03b1948472
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-13 11:37:13 +0000

    Work on data display

--- src/app/orders/ordersReceivedPage/components/OrderReceivedCard.tsx
+++ src/app/orders/ordersReceivedPage/components/OrderReceivedCard.tsx
@@ -1,5 +1,5 @@
 import ZebraContainer from '@/components/ZebraContainer'
-import { formatDate, formatPrice } from '@/library/utilities'
+import { capitaliseFirstLetter, formatDate, formatPrice } from '@/library/utilities'
 import type { BrowserSafeOrderReceived } from '@/types'
 
 interface Props {
@@ -10,24 +10,35 @@ interface Props {
 export default function OrderReceivedCard({ orderDetails, index }: Props) {
 	return (
 		<li>
-			<ZebraContainer index={index} oddStyles="bg-blue-50" evenStyles="bg-zinc-50" baseStyles="flex flex-col gap-y-2 w-full p-3 rounded-xl">
+			<ZebraContainer index={index} oddStyles="bg-blue-50" evenStyles="bg-zinc-50" baseStyles="flex flex-col gap-y-6 w-full p-3 rounded-xl">
 				<div className="flex justify-between">
 					<h3>{orderDetails.customerBusinessName}</h3>
-					<time
-					// ToDo: add datetime={}
-					>
-						{formatDate(orderDetails.requestedDeliveryDate)}
-					</time>
+					<div className="flex gap-x-2">
+						<span className="text-orange-600">{capitaliseFirstLetter(orderDetails.status)}</span>
+						<time
+						// ToDo: add datetime={}
+						>
+							{formatDate(orderDetails.requestedDeliveryDate)}
+						</time>
+					</div>
 				</div>
 				<ul className="flex-col gap-y-4">
 					{orderDetails.products.map((item) => (
-						<li key={item.productId} className="flex gap-x-4">
-							<span>{item.name}</span>
-							<span>x {item.vat}</span>
-							<span>{formatPrice(item.priceInMinorUnitsWithoutVat)}</span>
+						<li key={item.id} className="flex gap-x-4">
+							<span>
+								{item.name}
+								<span className="text-zinc-600"> x </span>
+								{item.vat},
+							</span>
+							<span>{formatPrice(item.priceInMinorUnitsWithoutVat)} each,</span>
+							<span>{formatPrice(item.priceInMinorUnitsWithoutVat * item.quantity)} subtotal</span>
 						</li>
 					))}
 				</ul>
+				<div className="w-full flex gap-x-2 items-end justify-end">
+					<span className="text-xl font-medium">Total</span>
+					<span>£666</span>
+				</div>
 			</ZebraContainer>
 		</li>
 	)
