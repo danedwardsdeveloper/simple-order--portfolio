e30fbfe8eef6258976f5d0fa2601650d14c31bff
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-20 08:57:18 +0000

    Create workInProgressOperation.ts

---
+++ src/library/database/operations/definitions/workInProgressOperation.ts
@@ -0,0 +1,61 @@
+import { logAndSanitiseApiResponse } from '@/library/utilities/public'
+import { extractIdFromRequestCookie } from '@/library/utilities/server'
+import type { DangerousBaseUser } from '@/types'
+import { and, eq, gt } from 'drizzle-orm'
+import type { NextRequest } from 'next/server'
+import { database } from '../../connection'
+import { freeTrials, users } from '../../schema'
+
+interface Input {
+	request: NextRequest
+	routeDetail: string
+	requireConfirmed?: boolean
+	requireSubscriptionOrTrial?: boolean
+}
+
+type Output = Promise<{
+	success: boolean
+	developerMessage?: string
+	foundDangerousUser?: DangerousBaseUser
+}>
+
+export async function checkAuthentication({
+	request,
+	routeDetail,
+	requireConfirmed = true,
+	requireSubscriptionOrTrial = true,
+}: Input): Output {
+	const { extractedUserId } = await extractIdFromRequestCookie(request)
+
+	let developerMessage = undefined
+
+	if (!extractedUserId) {
+		developerMessage = logAndSanitiseApiResponse({ routeDetail, message: '' })
+		return { success: false, developerMessage }
+	}
+
+	const [foundDangerousUser] = await database.select().from(users).where(eq(users.id, extractedUserId))
+
+	if (!foundDangerousUser) {
+		developerMessage = logAndSanitiseApiResponse({ routeDetail, message: '' })
+		return { success: false }
+	}
+
+	if (requireConfirmed && !foundDangerousUser.emailConfirmed) {
+		developerMessage = logAndSanitiseApiResponse({ routeDetail, message: 'email not confirmed' })
+		return { success: false }
+	}
+
+	if (requireSubscriptionOrTrial) {
+		const _validSubscriptionOrTrial = false
+		if (!foundDangerousUser.cachedTrialExpired) {
+		} else {
+			const [_validFreeTrial] = await database
+				.select()
+				.from(freeTrials)
+				.where(and(gt(freeTrials.endDate, new Date()), eq(freeTrials.userId, foundDangerousUser.id)))
+		}
+	}
+
+	return { success: true, foundDangerousUser }
+}
