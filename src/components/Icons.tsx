f07a79866376b40cca52fbc8c7fa14a469ddd73f
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-23 21:13:32 +0000

    Update CompanyLogo

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,24 +1,21 @@
-export default function CompanyLogo() {
+import { mergeClasses } from '@/library/utilities/public'
+
+export default function CompanyLogo({
+	size = 'size-7',
+	colour,
+}: { size?: 'size-6' | 'size-7' | 'size-20'; colour?: 'text-zinc-600' | 'text-blue-600' }) {
 	return (
-		<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className="w-full h-full" aria-hidden="true">
-			<g>
-				<path
-					fill="none"
-					stroke="currentColor"
-					strokeWidth="47"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					d="M 46 419c0 26 21 47 47 47h326c26 0 47-21 47-47V93c0-26-21-47-47-47H93c-26 0-47 21-47 47z"
-				/>
-				<path
-					fill="none"
-					stroke="currentColor"
-					strokeWidth="59"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					d="M 121 270l108 108 162-244"
-				/>
-			</g>
+		<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className={mergeClasses(size, colour)} aria-hidden="true">
+			<title>Simple Order company logo</title>
+			<path
+				fill="none"
+				stroke="currentColor"
+				strokeWidth="47"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M 46 419c0 26 21 47 47 47h326c26 0 47-21 47-47V93c0-26-21-47-47-47H93c-26 0-47 21-47 47z"
+			/>
+			<path fill="none" stroke="currentColor" strokeWidth="59" strokeLinecap="round" strokeLinejoin="round" d="M 121 270l108 108 162-244" />
 		</svg>
 	)
 }
