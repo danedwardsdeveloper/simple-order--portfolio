420bc5d0dfd2c3fa3fe38567ff1b647bd7b6bc11
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-24 15:52:02 +0000

    Update margin

--- src/app/inventory/components/VatToggleButton.tsx
+++ src/app/inventory/components/VatToggleButton.tsx
@@ -7,7 +7,7 @@ import { useUi } from '@/providers/ui'
 export default function VatToggleButton() {
 	const { includeVat, toggleIncludeVat } = useUi()
 	return (
-		<div className="w-full max-w-xl flex border-2 border-blue-200 p-3 rounded-xl">
+		<div className="max-w-xl flex border-2 border-blue-200 p-3 lg:-mx-3 rounded-xl">
 			<ToggleWithLabel enabled={includeVat} setEnabled={toggleIncludeVat} enabledLabel="Show VAT" disabledLabel="Hide VAT" />
 		</div>
 	)
