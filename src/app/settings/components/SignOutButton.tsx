8e23049364f7ee7f5d66fb0c09ed4cad7cdc4b97
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-27 13:14:59 +0000

    Add notifications on error

--- src/app/settings/components/SignOutButton.tsx
+++ src/app/settings/components/SignOutButton.tsx
@@ -1,11 +1,12 @@
 'use client'
 import type { SignOutPOSTresponse } from '@/app/api/authentication/sign-out/route'
-import { apiPaths, dataTestIdNames } from '@/library/constants'
-import logger from '@/library/logger'
+import { apiPaths, dataTestIdNames, userMessages } from '@/library/constants'
+import { useNotifications } from '@/providers/notifications'
 import { useUser } from '@/providers/user'
 
 export default function SignOutButton() {
 	const { setUser, setInventory, setConfirmedCustomers, setConfirmedMerchants, setInvitationsReceived, setInvitationsSent } = useUser()
+	const { createNotification } = useNotifications()
 
 	async function handleSignOut() {
 		const response = await fetch(apiPaths.authentication.signOut, {
@@ -15,17 +16,29 @@ export default function SignOutButton() {
 			},
 		})
 
-		const { message }: SignOutPOSTresponse = await response.json()
-
-		if (message === 'success') {
+		if (response.ok) {
 			setUser(null)
 			setConfirmedCustomers(null)
 			setInventory(null)
 			setConfirmedMerchants(null)
 			setInvitationsReceived(null)
 			setInvitationsSent(null)
-		} else {
-			logger.error('Error signing out: ', message)
+			createNotification({
+				level: 'success',
+				title: 'Success',
+				message: userMessages.signedOutSuccessfully,
+			})
+			return
+		}
+
+		const { userMessage }: SignOutPOSTresponse = await response.json()
+
+		if (userMessage) {
+			createNotification({
+				level: 'error',
+				title: 'Error',
+				message: userMessage,
+			})
 		}
 	}
 
