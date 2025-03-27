c02fc8989d05b5421ebcb770d880f070108ba406
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-27 13:07:16 +0000

    Add TrialExpiryInformation and subscription search params handling

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,26 +1,64 @@
 'use client'
 import { SignedInBreadCrumbs } from '@/components/BreadCrumbs'
+import Spinner from '@/components/Spinner'
 import UnauthorisedLinks from '@/components/UnauthorisedLinks'
-import RoleModeButton from '@/components/menubar/RoleModeButton'
+import { checkoutSearchParam, checkoutSearchParamValues, userMessages } from '@/library/constants'
+import { useNotifications } from '@/providers/notifications'
 import { useUser } from '@/providers/user'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { Suspense, useEffect, useRef } from 'react'
 import SignOutButton from './components/SignOutButton'
 import UserInformation from './components/UserInformation'
+import TrialExpiryInformation from './components/trialExpiryInformation'
 
 export default function SettingsPage() {
+	const searchParams = useSearchParams()
+	const subscriptionQuery = searchParams.get(checkoutSearchParam)
 	const { user } = useUser()
+	const { createNotification } = useNotifications()
+	const router = useRouter()
+	const notificationShown = useRef(false)
+
+	function clearParams() {
+		router.replace(window.location.pathname)
+	}
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <run once on mount>
+	useEffect(() => {
+		if (subscriptionQuery && !notificationShown.current) {
+			notificationShown.current = true
+
+			if (subscriptionQuery === checkoutSearchParamValues.success) {
+				createNotification({
+					level: 'success',
+					title: 'Success',
+					message: userMessages.stripeCheckoutSuccess,
+				})
+				clearParams()
+			}
+			if (subscriptionQuery === checkoutSearchParamValues.incomplete) {
+				createNotification({
+					level: 'info',
+					title: 'Subscription not started',
+					message: userMessages.stripeCheckoutIncomplete,
+				})
+				clearParams()
+			}
+		}
+	}, [subscriptionQuery, createNotification])
 
 	if (!user) return <UnauthorisedLinks />
 
 	return (
-		<>
+		<Suspense fallback={<Spinner />}>
 			<SignedInBreadCrumbs businessName={user.businessName} currentPageTitle="Settings" />
 			<div className="flex flex-col gap-y-4 items-start">
 				<h1>Settings</h1>
 
-				<RoleModeButton />
 				<UserInformation />
+				<TrialExpiryInformation expiry={user.trialExpiry} />
 				<SignOutButton />
 			</div>
-		</>
+		</Suspense>
 	)
 }
