b4e4b0f43fc8997b18ae824049824eb135828272
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-27 09:12:25 +0000

    Update include list

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
-		include: ['**/test.ts'],
+		include: ['**/test.ts', '**/*.test.ts'],
 		exclude: ['**/node_modules/**'],
 		env: {
 			NODE_ENV: 'development',
