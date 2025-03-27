59885349a083cbc57c0f0d311a97f57224628947
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-27 13:13:51 +0000

    Add comment

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
 
 export const metadata: Metadata = {
-	title: 'Settings',
+	title: 'Settings', // ...template
 }
 
 export default function Layout({ children }: { children: ReactNode }) {
