82dbe1d756f1d19e0874084d39ff35beaf92c1aa
Dan Edwards <daniel.edwards96@yahoo.com>
2025-03-27 13:02:46 +0000

    Create userMessages.ts

---
+++ src/library/constants/definitions/userMessages.ts
@@ -0,0 +1,13 @@
+/**
+ * Grammarly-check user-friendly messages that can be rendered on the browser
+ */
+export const userMessages = {
+	emailTaken: 'This email address is already in use. Please try a different one or sign in.',
+	businessNameTaken: 'This business name is already taken. Please choose another.',
+	databaseError: 'Sorry, something went wrong. Please try again later.',
+	serverError: 'Sorry, something went wrong. Please try again later.',
+	signedOutSuccessfully: 'You have been signed out.',
+	stripeCreateCheckoutError: "Sorry, we weren't able to redirect you to our payment provider, Stripe. Please try again later.",
+	stripeCheckoutSuccess: 'Thank you for subscribing to SimpleOrder.co.uk!',
+	stripeCheckoutIncomplete: "Your subscription wasn't started. No payment has been taken",
+} as const
