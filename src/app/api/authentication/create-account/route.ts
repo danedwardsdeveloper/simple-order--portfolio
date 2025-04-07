32401395a770ae3986fb36b253fefd884cb10b9f
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-07 08:33:04 +0100

    Refactor with Zod

--- src/app/api/authentication/create-account/route.ts
+++ src/app/api/authentication/create-account/route.ts
@@ -5,15 +5,14 @@ import { sendEmail } from '@/library/email/sendEmail'
 import { createNewMerchantEmail } from '@/library/email/templates'
 import { dynamicBaseURL } from '@/library/environment/publicVariables'
 import {
-	containsIllegalCharacters,
 	createFreeTrialEndTime,
 	createMerchantSlug,
-	emailRegex,
 	generateUuid,
 	initialiseDevelopmentLogger,
 	sanitiseDangerousBaseUser,
 } from '@/library/utilities/public'
 import { createCookieWithToken, equals, or } from '@/library/utilities/server'
+import { NewUserSchema } from '@/library/validations'
 import type { BaseUserInsertValues, BrowserSafeCompositeUser, DangerousBaseUser, NewFreeTrial, UserMessages } from '@/types'
 import bcrypt from 'bcryptjs'
 import { cookies } from 'next/headers'
@@ -32,59 +31,25 @@ export interface CreateAccountPOSTresponse {
 export async function POST(request: NextRequest): Promise<NextResponse<CreateAccountPOSTresponse>> {
 	const developmentLogger = initialiseDevelopmentLogger(`POST ${apiPaths.authentication.createAccount}:`)
 
-	const { firstName, lastName, email, password, businessName }: CreateAccountPOSTbody = await request.json()
-
-	let missingFieldMessage = null
-
-	if (!firstName) {
-		missingFieldMessage = 'firstName missing'
-	} else if (!lastName) {
-		missingFieldMessage = 'lastName missing'
-	} else if (!email) {
-		missingFieldMessage = 'email missing'
-	} else if (!password) {
-		missingFieldMessage = 'password missing'
-	} else if (!businessName) {
-		missingFieldMessage = 'businessName missing'
-	}
-
-	if (missingFieldMessage) {
-		const developmentMessage = developmentLogger(missingFieldMessage)
+	const result = NewUserSchema.safeParse(await request.json())
+	if (!result.success) {
+		const firstError = result.error.errors[0]
+		const message = [firstError.path, firstError.message].join(': ')
+		const developmentMessage = developmentLogger(message)
 		return NextResponse.json({ developmentMessage }, { status: 400 })
 	}
 
-	let illegalCharactersMessage = null
-	if (containsIllegalCharacters(firstName)) {
-		illegalCharactersMessage = 'firstName contains illegal characters'
-	} else if (containsIllegalCharacters(lastName)) {
-		illegalCharactersMessage = 'lastName contains illegal characters'
-	} else if (containsIllegalCharacters(password)) {
-		illegalCharactersMessage = 'password contains illegal characters'
-	} else if (containsIllegalCharacters(businessName)) {
-		illegalCharactersMessage = 'businessName contains illegal characters'
-	}
-
-	if (illegalCharactersMessage) {
-		const developmentMessage = developmentLogger(illegalCharactersMessage)
-		return NextResponse.json({ developmentMessage }, { status: 400 })
-	}
-
-	const normalisedEmail = email.toLowerCase().trim()
-
-	if (!emailRegex.test(normalisedEmail)) {
-		const developmentMessage = developmentLogger('Invalid email format')
-		return NextResponse.json({ developmentMessage }, { status: 400 })
-	}
+	const { firstName, lastName, businessName, email, password } = result.data
 
 	try {
 		const [existingUser] = await database
 			.select()
 			.from(users)
-			.where(or(equals(users.email, normalisedEmail), equals(users.businessName, businessName)))
+			.where(or(equals(users.email, email), equals(users.businessName, businessName)))
 			.limit(1)
 
 		if (existingUser) {
-			if (existingUser.email === normalisedEmail) {
+			if (existingUser.email === email) {
 				return NextResponse.json({ userMessage: userMessages.emailTaken }, { status: 409 })
 			}
 
@@ -112,11 +77,11 @@ export async function POST(request: NextRequest): Promise<NextResponse<CreateAcc
 			}
 
 			const newUserInsertValues: BaseUserInsertValues = {
-				firstName: firstName.trim(),
-				lastName: lastName.trim(),
-				email: normalisedEmail,
-				hashedPassword: hashedPassword.trim(),
-				businessName: businessName.trim(),
+				firstName,
+				lastName,
+				email,
+				hashedPassword,
+				businessName,
 				slug,
 				emailConfirmed: false,
 				cachedTrialExpired: false,
@@ -175,7 +140,7 @@ export async function POST(request: NextRequest): Promise<NextResponse<CreateAcc
 		const cookieStore = await cookies()
 		cookieStore.set(createCookieWithToken(dangerousNewUser.id, cookieDurations.oneYear))
 
-		developmentLogger(`Account created for ${compositeUser.firstName}.  Confirmation URL: ${confirmationURL}`, 'level3success')
+		developmentLogger(`Account created for ${compositeUser.firstName}.  Confirmation URL: ${confirmationURL}`, undefined, 'level3success')
 
 		return NextResponse.json({ user: compositeUser }, { status: 201 })
 	} catch (error) {
