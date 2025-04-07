83423807afdbab72307793fd31d08e2b423916b7
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-07 08:33:19 +0100

    Create route.test.ts

---
+++ src/app/api/authentication/verify-token/route.test.ts
@@ -0,0 +1,51 @@
+import { cookieDurations } from '@/library/constants'
+import { database } from '@/library/database/connection'
+import { deleteUserSequence } from '@/library/database/operations/definitions/deleteUserSequence/deleteUserSequence'
+import { users } from '@/library/database/schema'
+import { createCookieWithToken } from '@/library/utilities/server'
+import { initialiseGETRequestMaker } from '@tests/utilities'
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
+
+const requestMaker = initialiseGETRequestMaker({
+	url: 'http://localhost:3000/api/authentication/verify-token',
+})
+
+const badCookie = '123456789'
+let validCookie: undefined | string = undefined
+
+describe('Verify token', () => {
+	beforeAll(async () => {
+		const [stanSmith] = await database
+			.insert(users)
+			.values({
+				firstName: 'Stan',
+				lastName: 'Smith',
+				businessName: 'CIA',
+				slug: 'cia',
+				email: 'stansmith@gmail.com',
+				hashedPassword: '123456789',
+			})
+			.returning()
+
+		const cookieOptions = createCookieWithToken(stanSmith.id, cookieDurations.oneYear)
+		validCookie = `${cookieOptions.name}=${cookieOptions.value}`
+	})
+
+	afterAll(async () => {
+		await deleteUserSequence('stansmith@gmail.com')
+	})
+
+	test('Invalid token', async () => {
+		const { response } = await requestMaker(badCookie)
+		expect(response.status).toBe(400)
+	})
+
+	test('Valid token', async () => {
+		const { response } = await requestMaker(validCookie)
+		expect(response.status).toBe(200)
+	})
+})
+
+/*
+pnpm vitest src/app/api/authentication/verify-token
+*/
