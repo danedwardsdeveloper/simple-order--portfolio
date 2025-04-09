14483e2cd6684ce6323e9bf2eaedfb047d2796cc
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-09 09:11:01 +0100

    Create createMerchantSlug.test.ts

---
+++ src/library/utilities/public/definitions/createMerchantSlug.test.ts
@@ -0,0 +1,43 @@
+import { describe, expect, test } from 'vitest'
+import { createMerchantSlug } from './createMerchantSlug'
+
+const testCases: { description: string; input: string; expected: string }[] = [
+	{
+		description: 'with apostrophes',
+		input: `Julia's Joke Shop`,
+
+		// cspell:disable-next-line
+		expected: 'julias-joke-shop',
+	},
+	{
+		description: 'removes @',
+		input: 'business n@me',
+		expected: 'business-nme',
+	},
+	{
+		description: 'removes underscores',
+		input: 'name_with_underscores',
+
+		// cspell:disable-next-line
+		expected: 'namewithunderscores',
+	},
+	{
+		description: 'makes lowercase',
+		input: 'SNAKE_SHOP',
+
+		// cspell:disable-next-line
+		expected: 'snakeshop',
+	},
+]
+
+describe('Creates valid slugs', () => {
+	for (const testCase of testCases) {
+		test(testCase.description, () => {
+			expect(createMerchantSlug(testCase.input)).toEqual(testCase.expected)
+		})
+	}
+})
+
+/* 
+pnpm vitest src/library/utilities/public/definitions/createMerchantSlug
+*/
