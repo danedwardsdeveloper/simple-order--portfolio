f21b47ccc57729ad8b96a21c3e26f89caa1e6f20
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-09 09:10:36 +0100

    Remove milliseconds, add JSDoc comment

--- src/library/utilities/public/definitions/createFreeTrialEndTime.ts
+++ src/library/utilities/public/definitions/createFreeTrialEndTime.ts
@@ -1,14 +1,17 @@
-import { addDays, setHours, setMilliseconds, setMinutes, setSeconds } from 'date-fns'
+import { addDays, setHours, setMinutes, setSeconds } from 'date-fns'
 import { toZonedTime } from 'date-fns-tz'
 
+/**
+ * @returns a Date at least 30 days in the future, with the time set to 11.59pm (and 59 seconds) in UK time
+ */
 export function createFreeTrialEndTime(): Date {
 	const now = new Date()
-	const thirtyOneDays = addDays(now, 31)
+	const thirtyDays = addDays(now, 30)
 
-	const ukTime = toZonedTime(thirtyOneDays, 'Europe/London')
-	const endOfDay = setMilliseconds(setSeconds(setMinutes(setHours(ukTime, 23), 59), 59), 999)
+	const ukTime = toZonedTime(thirtyDays, 'Europe/London')
+	const endOfDay = setSeconds(setMinutes(setHours(ukTime, 23), 59), 59)
 
-	if (endOfDay.getTime() - now.getTime() < 31 * 24 * 60 * 60 * 1000) {
+	if (endOfDay.getTime() - now.getTime() < 30 * 24 * 60 * 60 * 1000) {
 		return addDays(endOfDay, 1)
 	}
 
