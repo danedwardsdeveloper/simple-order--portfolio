925518b18644d79c2093ba0f71b53ee717b73bf6
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-10 22:53:22 +0100

    Add React testing plugins

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,16 @@
 import path from 'node:path'
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+// import '@testing-library/jest-dom'
 
 export default defineConfig({
+	plugins: [tsconfigPaths(), react()],
 	test: {
-		include: ['**/test.ts', '**/*.test.ts'],
+		environment: 'jsdom',
+		include: ['**/test.ts', '**/*.test.ts', '**/test.tsx', '**/*.test.tsx'],
 		exclude: ['**/node_modules/**'],
-		slowTestThreshold: 2000,
+		slowTestThreshold: 5000,
 	},
 	resolve: {
 		alias: {
