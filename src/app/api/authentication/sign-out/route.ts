b20bf8992a818f1bc920192f7242b755acba596c
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-21 11:07:59 +0100

    Update logging behaviour, handle missing bodies

--- src/app/api/authentication/sign-out/route.ts
+++ src/app/api/authentication/sign-out/route.ts
@@ -1,36 +1,32 @@
-import { apiPaths, cookieNames, httpStatus, userMessages } from '@/library/constants'
+import { apiPaths, cookieNames, userMessages } from '@/library/constants'
 import { initialiseDevelopmentLogger } from '@/library/utilities/public'
 import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 
 export interface SignOutPOSTresponse {
 	userMessage?: typeof userMessages.serverError
 	developmentMessage?: string
 }
 
-export async function POST(): Promise<NextResponse<SignOutPOSTresponse>> {
+export async function POST(request: NextRequest): Promise<NextResponse<SignOutPOSTresponse>> {
 	const developmentLogger = initialiseDevelopmentLogger(`POST ${apiPaths.authentication.signOut}: `)
 
 	try {
+		const _body = await request.json().catch(() => ({}))
 		const cookieStore = await cookies()
 		const tokenCookie = cookieStore.get(cookieNames.token)
 
 		if (!tokenCookie) {
 			const developmentMessage = developmentLogger('token not found')
-			return NextResponse.json({ developmentMessage }, { status: httpStatus.http400badRequest })
+			return NextResponse.json({ developmentMessage }, { status: 400 })
 		}
-
-		/* ToDo 
-		- Don't use cookies() as it doesn't work in testing
-		- Delete Stripe cookies too
-		*/
 
 		cookieStore.delete(cookieNames.token)
 
-		const developmentMessage = developmentLogger('Signed out successfully', undefined, 'level3success')
+		const developmentMessage = developmentLogger('Signed out successfully', { level: 'level3success' })
 		return NextResponse.json({ developmentMessage }, { status: 200 })
 	} catch (error) {
-		const developmentMessage = developmentLogger('Caught error', error)
+		const developmentMessage = developmentLogger('Caught error', { error })
 		return NextResponse.json({ userMessage: userMessages.serverError, developmentMessage }, { status: 500 })
 	}
 }
