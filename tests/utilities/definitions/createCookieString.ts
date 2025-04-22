bd6af46d74847d336af5bc3beaf83e9bccf18695
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-22 09:00:11 +0100

    Update createCookieString.ts

--- tests/utilities/definitions/createCookieString.ts
+++ tests/utilities/definitions/createCookieString.ts
@@ -1,18 +1,18 @@
 import { cookieDurations } from '@/library/constants'
 import { createCookieWithToken } from '@/library/utilities/server'
+import { serialize } from 'cookie'
 
 /**
- * 
  * @example 
-const requestCookie = createCookieString({userId: createdUser.id})
+const requestCookie = createCookieString({ userId: 1, expired: true })
  */
-export function createCookieString({
-	userId,
-}: {
-	userId: number
-}) {
+export function createCookieString({ userId, expired = false }: { userId: number; expired?: boolean }) {
 	const cookieOptions = createCookieWithToken(userId, cookieDurations.oneYear)
-	const validCookie = `${cookieOptions.name}=${cookieOptions.value}`
-
-	return validCookie
+	return serialize(cookieOptions.name, cookieOptions.value, {
+		httpOnly: cookieOptions.httpOnly,
+		secure: cookieOptions.secure,
+		sameSite: cookieOptions.sameSite,
+		path: cookieOptions.path,
+		maxAge: expired ? -1 : cookieOptions.maxAge,
+	})
 }
