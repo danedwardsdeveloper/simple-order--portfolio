a8a9e3f6290e55f83f3d28bcafb394496667346d
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-23 09:13:39 +0100

    Create test constants barrel

---
+++ tests/constants/testUsers.ts
@@ -0,0 +1,236 @@
+import type { TestUserInputValues } from '@/types'
+// Cut and paste these into tests so each test uses a different person and there aren't any conflicts
+
+const emilyGilmoreInputValues: TestUserInputValues = {
+	firstName: 'Emily',
+	lastName: 'Gilmore',
+	businessName: 'Emily Gilmore Enterprises',
+	email: 'email@gmail.com',
+	emailConfirmed: true,
+	password: 'D@rT0wn!123',
+}
+
+const lukeDanesInputValues: TestUserInputValues = {
+	firstName: 'Luke',
+	lastName: 'Danes',
+	businessName: "Luke's Diner",
+	email: 'luke@lukesdiner.com',
+	emailConfirmed: true,
+	password: 'C0ffee&P@ncakes',
+}
+
+const melissaJoanHartInputValues: TestUserInputValues = {
+	firstName: 'Melissa',
+	lastName: 'Joan Hart',
+	businessName: 'Melissa Joan Enterprises',
+	email: 'melissa@joanhart.com',
+	emailConfirmed: true,
+	password: 'Sabr1n@!Witch',
+}
+
+const biancaDelRioInputValues: TestUserInputValues = {
+	firstName: 'Bianca',
+	lastName: 'Del Rio',
+	businessName: 'Bianca Del Rio Fashion',
+	email: 'bianca@delrio.com',
+	emailConfirmed: true,
+	password: 'N0tT0day$atan',
+}
+
+const betteDavisInputValues: TestUserInputValues = {
+	firstName: 'Bette',
+	lastName: 'Davis',
+	businessName: 'Bette Davis Pictures',
+	email: 'bette@davis.com',
+	emailConfirmed: true,
+	password: 'AllAb0ut3ve!',
+}
+
+const juliaDavisInputValues: TestUserInputValues = {
+	firstName: 'Julia',
+	lastName: 'Davis',
+	businessName: 'Julia Davis Productions',
+	email: 'julia@davis.com',
+	emailConfirmed: true,
+	password: 'Nighty!N1ght',
+}
+
+const countDraculaInputValues: TestUserInputValues = {
+	firstName: 'Count',
+	lastName: 'Dracula',
+	businessName: 'Transylvanian Estates',
+	email: 'count@dracula.com',
+	emailConfirmed: true,
+	password: 'Bl00dType0!',
+}
+
+const janeEyreInputValues: TestUserInputValues = {
+	firstName: 'Jane',
+	lastName: 'Eyre',
+	businessName: 'Thornfield Governesses',
+	email: 'jane@thornfield.com',
+	emailConfirmed: true,
+	password: 'R0chester!Fire',
+}
+
+const anneShirleyInputValues: TestUserInputValues = {
+	firstName: 'Anne',
+	lastName: 'Shirley',
+	businessName: 'Green Gables Farm',
+	email: 'anne@greengables.com',
+	emailConfirmed: true,
+	password: 'C@rr0ts&Sl@te',
+}
+
+const elizabethBennetInputValues: TestUserInputValues = {
+	firstName: 'Elizabeth',
+	lastName: 'Bennet',
+	businessName: 'Longbourn Estate',
+	email: 'lizzie@longbourn.com',
+	emailConfirmed: true,
+	password: 'PrideN0tPr3judice',
+}
+
+const joMarchInputValues: TestUserInputValues = {
+	firstName: 'Jo',
+	lastName: 'March',
+	businessName: 'March Writers',
+	email: 'jo@march.com',
+	emailConfirmed: true,
+	password: 'L1ttleW0men!',
+}
+
+const esthePrynneInputValues: TestUserInputValues = {
+	firstName: 'Esther',
+	lastName: 'Prynne',
+	businessName: 'Scarlet Embroidery',
+	email: 'hester@prynne.com',
+	emailConfirmed: true,
+	password: 'Sc@rletLett3r',
+}
+
+const cinderellaInputValues: TestUserInputValues = {
+	firstName: 'Cinderella',
+	lastName: 'Charming',
+	businessName: 'Glass Slipper Cleaners',
+	email: 'cinderella@castle.com',
+	emailConfirmed: true,
+	password: 'M1dnight!Pumpkin',
+}
+
+const arielInputValues: TestUserInputValues = {
+	firstName: 'Ariel',
+	lastName: 'Triton',
+	businessName: 'Under The Sea Collectibles',
+	email: 'ariel@atlantica.com',
+	emailConfirmed: true,
+	password: 'Dingleh0pper!',
+}
+
+const belleInputValues: TestUserInputValues = {
+	firstName: 'Belle',
+	lastName: 'Beauty',
+	businessName: 'Provincial Library',
+	email: 'belle@castle.com',
+	emailConfirmed: true,
+	password: 'B00kw0rm!Rose',
+}
+
+const mulanInputValues: TestUserInputValues = {
+	firstName: 'Fa',
+	lastName: 'Mulan',
+	businessName: 'Dynasty Warriors',
+	email: 'mulan@china.com',
+	emailConfirmed: true,
+	password: 'D1sguise!Honor',
+}
+
+const jasmineInputValues: TestUserInputValues = {
+	firstName: 'Jasmine',
+	lastName: 'Sultan',
+	businessName: 'Agrabah Bazaar',
+	email: 'jasmine@palace.com',
+	emailConfirmed: true,
+	password: 'Carpet!R1de',
+}
+
+const kimWoodburneInputValues: TestUserInputValues = {
+	firstName: 'Kim',
+	lastName: 'Woodburne',
+	businessName: 'How Clean Is Your House',
+	email: 'kim@cleaning.com',
+	emailConfirmed: true,
+	password: 'Cl3@nAndTidy!',
+}
+
+const aggieInputValues: TestUserInputValues = {
+	firstName: 'Aggie',
+	lastName: 'MacKenzie',
+	businessName: 'Spotless Solutions',
+	email: 'aggie@spotless.com',
+	emailConfirmed: true,
+	password: 'D1rtBust3r!',
+}
+
+const kylieInputValues: TestUserInputValues = {
+	firstName: 'Kylie',
+	lastName: 'Minogue',
+	businessName: 'Spinning Around Inc',
+	email: 'kylie@minogue.com',
+	emailConfirmed: true,
+	password: 'C@ntGetY0uOutOfMyH3ad',
+}
+
+const britneyInputValues: TestUserInputValues = {
+	firstName: 'Britney',
+	lastName: 'Spears',
+	businessName: 'Toxic Enterprises',
+	email: 'britney@spears.com',
+	emailConfirmed: true,
+	password: 'OopsID1dItAg@in!',
+}
+
+const cherInputValues: TestUserInputValues = {
+	firstName: 'Cher',
+	lastName: '',
+	businessName: 'Believe Productions',
+	email: 'cher@believe.com',
+	emailConfirmed: true,
+	password: 'LifeAfterL0ve!',
+}
+
+const ruPaulInputValues: TestUserInputValues = {
+	firstName: 'RuPaul',
+	lastName: 'Charles',
+	businessName: 'Drag Race Enterprises',
+	email: 'rupaul@dragrace.com',
+	emailConfirmed: true,
+	password: 'S@shayAw@y!',
+}
+
+const eltonJohnInputValues: TestUserInputValues = {
+	firstName: 'Elton',
+	lastName: 'John',
+	businessName: 'Rocket Man Productions',
+	email: 'elton@rocketman.com',
+	emailConfirmed: true,
+	password: 'Y3ll0wBrickR0ad!',
+}
+
+const freddieMercuryInputValues: TestUserInputValues = {
+	firstName: 'Freddie',
+	lastName: 'Mercury',
+	businessName: 'Queen Enterprises',
+	email: 'freddie@queen.com',
+	emailConfirmed: true,
+	password: 'B0hemianRh@psody!',
+}
+
+const ladyGagaInputValues: TestUserInputValues = {
+	firstName: 'Lady',
+	lastName: 'Gaga',
+	businessName: 'Haus of Gaga',
+	email: 'lady@gaga.com',
+	emailConfirmed: true,
+	password: 'B@dR0mance!',
+}
