cf8e0751ff5c05fb17b59c0d840c668bb66156c5
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-23 09:15:57 +0100

    Rename to developmentUsers, add comment

--- src/library/constants/definitions/developmentUsers.ts
+++ src/library/constants/definitions/developmentUsers.ts
@@ -1,57 +1,47 @@
-export const testPasswords = {
-	good: 'securePassword123',
-	illegalCharacters: 'illegalP`password',
-}
-
-export const testUsers = {
+/**
+ * Users for manual testing. They are always present in the development database
+ */
+export const developmentUsers = {
 	permanentBoth: {
 		firstName: 'Samantha',
 		lastName: 'Jones',
 		email: 'permanentboth@gmail.com',
 		businessName: "Samantha's Sauces",
+		password: 'securePassword123',
 	},
 	permanentCustomer: {
 		firstName: 'Charlotte',
 		lastName: 'York',
 		email: 'permanentcustomer@gmail.com',
 		businessName: "Charlotte's Harlots",
+		password: 'securePassword123',
 	},
 	permanentMerchant: {
 		firstName: 'Miranda',
 		lastName: 'Hobbes',
 		email: 'permanentmerchant@gmail.com',
 		businessName: "Miranda's Pandas",
+		password: 'securePassword123',
 	},
 	customerOnly: {
 		firstName: 'Jane',
 		lastName: 'Pompermonterson',
 		email: 'janepompermonterson@gmail.com',
 		businessName: `Jane's Bakery`,
+		password: 'securePassword123',
 	},
 	merchantOnly: {
 		firstName: 'David',
 		lastName: 'Agromberis',
 		email: 'davidagromberis@gmail.com',
 		businessName: `David's Stinky Cheeses`,
+		password: 'securePassword123',
 	},
 	both: {
 		firstName: 'Susan',
 		lastName: 'Poodle',
 		email: 'susanpoodle@gmail.com',
 		businessName: `Susan's Spicy Sausages`,
+		password: 'securePassword123',
 	},
-}
-
-export function checkIsTestEmail(email: string): boolean {
-	const normalizedEmail = email.trim().toLowerCase()
-	return [
-		testUsers.customerOnly.email,
-		testUsers.merchantOnly.email,
-		testUsers.both.email,
-		testUsers.permanentBoth.email,
-		testUsers.permanentCustomer.email,
-		testUsers.permanentMerchant.email,
-	]
-		.map((email) => email.toLowerCase())
-		.includes(normalizedEmail)
 }
