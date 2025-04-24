015b6a54279fe102264815fe4d6129eefcfd25d7
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-24 09:02:35 +0100

    Add WIP test snippets

---
+++ tests/work-in-progress-snippets/apiTestSnippet.ts
@@ -0,0 +1,50 @@
+import { initialiseTestRequestMaker } from '@tests/utilities'
+import { describe, expect, test } from 'vitest'
+
+interface Suite {
+	description: string
+	expectedStatus: number
+	cases: {
+		description: string
+		value: string
+	}[]
+}
+
+const suites: Suite[] = [
+	{
+		description: 'Bad requests',
+		expectedStatus: 400,
+		cases: [
+			{ description: '', value: '' },
+			{ description: '', value: '' },
+			{ description: '', value: '' },
+		],
+	},
+	{
+		description: 'Success',
+		expectedStatus: 200,
+		cases: [{ description: '', value: '' }],
+	},
+]
+
+const makeRequest = initialiseTestRequestMaker({
+	basePath: '',
+	method: 'POST',
+})
+
+describe('Description', () => {
+	for (const { description, expectedStatus, cases } of suites) {
+		describe(description, () => {
+			for (const { description, value } of cases) {
+				test(description, async () => {
+					const { response } = await makeRequest({ body: value })
+					expect(response.status).toBe(expectedStatus)
+				})
+			}
+		})
+	}
+})
+
+/* 
+pnpm vitest src/app/api/
+*/
