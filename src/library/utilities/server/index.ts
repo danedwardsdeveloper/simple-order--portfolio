b50bf474f0b443e7142206514030b76cdd01a064
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-26 12:20:00 +0100

    Create validateToken

--- src/library/utilities/server/index.ts
+++ src/library/utilities/server/index.ts
@@ -2,8 +2,10 @@ import { and, eq as equals, gt as greaterThan, inArray, isNull, or } from 'drizz
 export { and, equals, greaterThan, isNull, inArray, or }
 
 export * from './definitions/createCookies'
-export * from './definitions/extractIdFromRequestCookie'
+export * from './definitions/validateToken'
 export * from './definitions/createHtmlParagraph'
 export * from './definitions/hashPassword'
 export * from './definitions/createInvitation'
 export * from './definitions/createSubscription'
+export * from './definitions/initialiseResponder'
+export * from './definitions/formatFirstError'
