c331207c6871893ae975e4033cf243d801b7e7dd
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-28 12:45:28 +0100

    Update dates.ts

--- src/types/definitions/dates.ts
+++ src/types/definitions/dates.ts
@@ -1,5 +1,2 @@
-import type { months } from '@/library/constants'
-
 export type Year = 2025 | 2026 | 2027
-
-export type Month = (typeof months)[keyof typeof months]
+export type Month = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
