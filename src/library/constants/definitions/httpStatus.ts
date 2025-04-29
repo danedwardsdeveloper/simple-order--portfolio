98222e0128e0cb9238af19b99ee4f6527889cb59
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-29 11:46:55 +0100

    Export http statuses separately

--- src/library/constants/definitions/httpStatus.ts
+++ src/library/constants/definitions/httpStatus.ts
@@ -1,29 +1,24 @@
-export const http409conflict = 409
+import type { HttpStatus } from '@/types'
 
-/**
- * @deprecated use individual exports instead
- */
-export const httpStatus = {
-	http200ok: 200,
-	http201created: 201,
-	http202accepted: 202,
-	http204noContent: 204,
-	http206partialContent: 206,
+export const http200ok: HttpStatus = 200
+export const http201created: HttpStatus = 201
+export const http202accepted: HttpStatus = 202
+export const http204noContent: HttpStatus = 204
+export const http206partialContent: HttpStatus = 206
 
-	http400badRequest: 400,
-	http401unauthorised: 401,
-	http403forbidden: 403,
-	http404notFound: 404,
-	http405methodNotAllowed: 405,
-	http409conflict: 409,
-	http410gone: 410,
-	http415unsupportedMediaType: 415,
-	http422unprocessableContent: 422,
-	http429tooManyRequests: 429,
+export const http400badRequest: HttpStatus = 400
+export const http401unauthorised: HttpStatus = 401
+export const http403forbidden: HttpStatus = 403
+export const http404notFound: HttpStatus = 404
+export const http405methodNotAllowed: HttpStatus = 405
+export const http409conflict: HttpStatus = 409
+export const http410gone: HttpStatus = 410
+export const http415unsupportedMediaType: HttpStatus = 415
+export const http422unprocessableContent: HttpStatus = 422
+export const http429tooManyRequests: HttpStatus = 429
 
-	http500serverError: 500,
-	http501notImplemented: 501,
-	http502badGateway: 502,
-	http503serviceUnavailable: 503,
-	http504gatewayTimeout: 504,
-} as const
+export const http500serverError: HttpStatus = 500
+export const http501notImplemented: HttpStatus = 501
+export const http502badGateway: HttpStatus = 502
+export const http503serviceUnavailable: HttpStatus = 503
+export const http504gatewayTimeout: HttpStatus = 504
