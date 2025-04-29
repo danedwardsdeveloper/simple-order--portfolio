2e8ec7eec500dcf9b20bf3b22e9dca58b6abe19d
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-29 11:45:20 +0100

    Create images barrel

---
+++ src/library/images/index.ts
@@ -0,0 +1,19 @@
+import autoParts from '../../../public/images/auto-parts.png'
+import cheeseMaker from '../../../public/images/cheese-maker.png'
+import chocolatiers from '../../../public/images/chocolatiers.png'
+import florist from '../../../public/images/florist.png'
+import fruitSeller from '../../../public/images/fruit-seller.png'
+import furnitureManufacturer from '../../../public/images/furniture-manufacturer.png'
+import defaultSocialImage from '../../../public/images/simple-order-wholesale-order-management-software-website.png'
+import threeBakers from '../../../public/images/three-bakers.png'
+
+export const validatedImages = {
+	autoParts,
+	cheeseMaker,
+	chocolatiers,
+	defaultSocialImage,
+	florist,
+	fruitSeller,
+	furnitureManufacturer,
+	threeBakers,
+}
