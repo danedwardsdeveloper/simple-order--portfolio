8a66e51715c05e34b2d2e91c5d7370897334129f
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-29 11:47:58 +0100

    Make smallest font size 18px

--- src/app/inventory/components/DeleteProductModal.tsx
+++ src/app/inventory/components/DeleteProductModal.tsx
@@ -43,7 +43,7 @@ export default function DeleteProductModal({
 									Delete product
 								</DialogTitle>
 								<div className="mt-2">
-									<p className="text-sm text-gray-500">{`Are you sure you want to delete ${product.name}?`}</p>
+									<p className="text-gray-500">{`Are you sure you want to delete ${product.name}?`}</p>
 								</div>
 							</div>
 						</div>
@@ -51,7 +51,7 @@ export default function DeleteProductModal({
 							<button
 								type="button"
 								onClick={handleConfirm}
-								className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+								className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
 							>
 								Delete product
 							</button>
@@ -59,7 +59,7 @@ export default function DeleteProductModal({
 								type="button"
 								data-autofocus
 								onClick={onClose}
-								className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+								className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
 							>
 								Cancel
 							</button>
