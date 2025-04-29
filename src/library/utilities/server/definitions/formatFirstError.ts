2806da53f23135a4617504a7d78cb2288ac478be
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-29 11:48:30 +0100

    Add comment

--- src/library/utilities/server/definitions/formatFirstError.ts
+++ src/library/utilities/server/definitions/formatFirstError.ts
@@ -1,5 +1,6 @@
 import type { ZodError } from 'zod'
 
+// This needs work - doesn't handle general errors like missing body
 export function formatFirstError(error: ZodError): string {
 	const firstError = error.errors[0]
 	const fieldPath = firstError.path.join('.')
