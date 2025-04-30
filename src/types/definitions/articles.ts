327c0266424f6759ee61760f4372bb655c150c4c
Dan Edwards <daniel.edwards96@yahoo.com>
2025-04-30 09:35:32 +0100

    Update articles copy

--- src/types/definitions/articles.ts
+++ src/types/definitions/articles.ts
@@ -1,19 +1,40 @@
-import type { StaticImageData } from 'next/image'
 import type { ReactNode } from 'react'
+import type { ImageWithAlt } from './images'
+
+export type ArticleSlug = 'how-it-works' | 'privacy-policy' | 'terms-of-service' | 'cookie-policy' | 'about' | 'gdpr-compliance'
 
 export interface ArticleDetails {
 	displayTitle: string
+	slug: ArticleSlug
 	metaTitle: string
 	displayDescription: string
 	metaDescription: string
 	publishedAt: Date
 	updatedAt?: Date
 	utilityPage?: boolean
-	featuredImage: { src: StaticImageData; alt: string }
-	categorySlug: 'Help' | 'Legal'
+	featuredImage: ImageWithAlt
+	category: ArticleCategory
 	content: (string | ReactNode)[]
 }
 
-export type ArticleSlug = 'how-it-works' | 'privacy-policy' | 'terms-of-service' | 'cookie-policy' | 'about' | 'gdpr-compliance'
+interface ArticleCategory {
+	slug: ArticleCategorySlug
+	display: string
+}
+
+export type ArticleCategorySlug = 'legal' | 'getting-started'
+
+export const articleCategories = {
+	legal: {
+		slug: 'legal',
+		display: 'Legal',
+	},
+	gettingStarted: {
+		slug: 'getting-started',
+		display: 'Getting Started',
+	},
+} as const
+
+export type ArticleCategories = typeof articleCategories
 
 export type ArticlesCollection = Record<ArticleSlug, ArticleDetails>
