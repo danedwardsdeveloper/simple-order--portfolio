93d06f9206a801250d4ad2f4026ba95b77164b06
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-04 18:41:50 +0100

    Add deleteFreeTrial, rename file

--- src/library/database/operations/definitions/freeTrial.ts
+++ src/library/database/operations/definitions/freeTrial.ts
@@ -2,6 +2,7 @@ import { database } from '@/library/database/connection'
 import { freeTrials } from '@/library/database/schema'
 import logger from '@/library/logger'
 import { createFreeTrialEndTime } from '@/library/utilities/public'
+import { equals } from '@/library/utilities/server'
 import type { FreeTrial, Transaction } from '@/types'
 
 type Input = {
@@ -20,6 +21,15 @@ export async function createFreeTrial({ userId, tx }: Input): Promise<FreeTrial>
 		return freeTrial
 	} catch (error) {
 		logger.error(`createFreeTrial: failed to create free trial for user with ID ${userId}`, error)
+		throw error
+	}
+}
+
+export async function deleteFreeTrial(userId: number) {
+	try {
+		await database.delete(freeTrials).where(equals(freeTrials.userId, userId))
+	} catch (error) {
+		logger.error(`deleteFreeTrial: failed to delete free trial for user with ID ${userId}`, error)
 		throw error
 	}
 }
