5174a9022acc49d780cd51c9fa0d8d7359c02c89
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-04 18:31:21 +0100

    Create tests.ts

---
+++ src/types/definitions/tests.ts
@@ -0,0 +1,12 @@
+import type { RequestInit, Response } from 'node-fetch'
+import type { ParsedSetCookie } from './cookies'
+
+export type NodeFetchResponse = Response
+export type NodeRequestInit = RequestInit
+
+export interface TestRequestResponse {
+	response: NodeFetchResponse
+	setCookie: ParsedSetCookie | null
+}
+
+export type JsonData = { [key: string]: unknown }
