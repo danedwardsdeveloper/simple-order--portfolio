06550a560eadafb4ee19b6920157ad92537629bd
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-04 18:35:33 +0100

    Create time utilities

---
+++ src/library/utilities/public/definitions/time.ts
@@ -0,0 +1,35 @@
+import { january } from '@/library/constants'
+import { HoursSchema, MinutesSchema } from '@/library/validations/definitions/time'
+import type { Hours, Minutes } from '@/types'
+import { z } from 'zod'
+
+export function epochDateToTimeInput(epochDate: Date) {
+	const date = new Date(epochDate)
+	const hours = date.getUTCHours().toString().padStart(2, '0')
+	const minutes = date.getUTCMinutes().toString().padStart(2, '0')
+	return `${hours}:${minutes}`
+}
+
+const TimeInputSchema = z.string().regex(
+	/^\d{1,2}:\d{2}$/, //
+	"Time must be in format 'HH:MM'",
+)
+
+export function timeInputToEpochDate(timeInputValue: string): Date {
+	TimeInputSchema.parse(timeInputValue)
+	const [hours, minutes] = timeInputValue.split(':').map(Number)
+
+	HoursSchema.parse(hours)
+	MinutesSchema.parse(minutes)
+
+	return createCutOffTime({ minutes, hours })
+}
+
+export function createCutOffTime({ hours, minutes }: { hours: Hours; minutes: Minutes }): Date {
+	HoursSchema.parse(hours)
+	MinutesSchema.parse(minutes)
+
+	const epochDay = new Date(Date.UTC(1970, january, 1))
+	epochDay.setUTCHours(hours, minutes)
+	return epochDay
+}
