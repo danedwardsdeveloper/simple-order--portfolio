1d1261083e4c4a2c2481960189eb6973ed54657e
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-04 18:35:18 +0100

    Delete message type checking utilities

--- src/library/utilities/public/definitions/typeChecking.ts
+++ src/library/utilities/public/definitions/typeChecking.ts
@@ -1,22 +1,4 @@
-import { orderStatusArray, systemMessages, unauthorisedMessages, userMessages } from '@/library/constants'
-import type { OrderStatus, SelectedProduct, SystemMessages, UnauthorisedMessages, UserMessages } from '@/types'
-
-export function isSystemMessage(message: string): message is SystemMessages {
-	return Object.values(systemMessages).includes(message as SystemMessages)
-}
-
-export function isUserMessage(message: string): message is UserMessages {
-	return Object.values(userMessages).includes(message as UserMessages)
-}
-
-export function isUnauthorisedMessage(message: string): message is UnauthorisedMessages {
-	return Object.values(unauthorisedMessages).includes(message as UnauthorisedMessages)
-}
-
-export function isOrderStatus(value: unknown): value is OrderStatus {
-	if (typeof value !== 'string') return false
-	return orderStatusArray.includes(value as OrderStatus)
-}
+import type { SelectedProduct } from '@/types'
 
 export function isSelectedProductArray(input: Array<SelectedProduct>): input is SelectedProduct[] {
 	if (!Array.isArray(input) || input.length === 0) return false
