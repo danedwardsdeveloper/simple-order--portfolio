ed30281b730c51c2ba6378a56c54744e746af3ae
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-04 18:44:32 +0100

    Add red option

--- src/components/MessageContainer.tsx
+++ src/components/MessageContainer.tsx
@@ -1,7 +1,7 @@
 import { mergeClasses } from '@/library/utilities/public'
 import type { ReactNode } from 'react'
 
-type BorderClasses = 'border-emerald-300' | 'border-blue-300' | 'border-orange-300'
+type BorderClasses = 'border-emerald-300' | 'border-blue-300' | 'border-orange-300' | 'border-red-300'
 
 export default function MessageContainer({ children, borderColour }: { children: ReactNode; borderColour: BorderClasses }) {
 	return <div className={mergeClasses('rounded-xl border-2 p-3 my-4 max-w-md', borderColour)}>{children}</div>
