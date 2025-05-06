64f6674395e79c7398d2b4aad63ce53e0a7c3fd5
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-06 23:56:26 +0100

    Delete temporary data display

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,37 +2,23 @@
 import { SignedInBreadCrumbs } from '@/components/BreadCrumbs'
 import ContentSplash from '@/components/ContentSplash'
 import UnauthorisedLinks from '@/components/UnauthorisedLinks'
-import { useUser } from '@/providers/user'
+import { useUser } from '@/components/providers/user'
 import WelcomeMessages from './components/WelcomeMessages'
 
 export default function DashboardPage() {
-	const { user, isLoading, ordersMade, ordersReceived } = useUser()
-
-	if (!user) return <UnauthorisedLinks />
+	const { user, isLoading } = useUser()
 
 	if (isLoading) return <ContentSplash />
 
-	return (
-		<>
-			<SignedInBreadCrumbs businessName={user.businessName} />
-			<h1>Dashboard</h1>
-			<WelcomeMessages />
+	if (!isLoading && !user) return <UnauthorisedLinks />
 
-			{/* Temporary data display */}
-			{ordersMade && (
-				<div className="whitespace-pre-wrap break-all">
-					<h2 className="mt-8 mb-2">Orders made</h2>
-					<p>{JSON.stringify(ordersMade)}</p>
-				</div>
-			)}
-
-			{/* Temporary data display */}
-			{ordersReceived && (
-				<div className="whitespace-pre-wrap break-all">
-					<h2 className="mt-8 mb-2">Orders received</h2>
-					<p>{JSON.stringify(ordersReceived)}</p>
-				</div>
-			)}
-		</>
-	)
+	if (user) {
+		return (
+			<>
+				<SignedInBreadCrumbs businessName={user.businessName} />
+				<h1>Dashboard</h1>
+				<WelcomeMessages />
+			</>
+		)
+	}
 }
