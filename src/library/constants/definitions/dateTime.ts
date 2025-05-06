496d0075d50cb6ae9db003b875ae0891079ff18c
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-06 23:45:22 +0100

    Create dateTime.ts

---
+++ src/library/constants/definitions/dateTime.ts
@@ -0,0 +1,53 @@
+import type { DayOfTheWeek, Month } from '@/types'
+
+export const january: Month = 0
+export const february: Month = 1
+export const march: Month = 2
+export const april: Month = 3
+export const may: Month = 4
+export const june: Month = 5
+export const july: Month = 6
+export const august: Month = 7
+export const september: Month = 8
+export const october: Month = 9
+export const november: Month = 10
+export const december: Month = 11
+
+export const monday = 1
+export const tuesday = 2
+export const wednesday = 3
+export const thursday = 4
+export const friday = 5
+export const saturday = 6
+export const sunday = 7
+
+export const daysOfTheWeek: DayOfTheWeek[] = [
+	{
+		name: 'Monday',
+		sortOrder: 1,
+	},
+	{
+		name: 'Tuesday',
+		sortOrder: 2,
+	},
+	{
+		name: 'Wednesday',
+		sortOrder: 3,
+	},
+	{
+		name: 'Thursday',
+		sortOrder: 4,
+	},
+	{
+		name: 'Friday',
+		sortOrder: 5,
+	},
+	{
+		name: 'Saturday',
+		sortOrder: 6,
+	},
+	{
+		name: 'Sunday',
+		sortOrder: 7,
+	},
+]
