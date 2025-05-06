6ac53d45d25fe5fd00dbb30bba7a0e139c7acd18
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-06 23:48:35 +0100

    Add sanitiseMerchant

--- src/library/utilities/public/definitions/sanitiseUser.ts
+++ src/library/utilities/public/definitions/sanitiseUser.ts
@@ -1,6 +1,11 @@
-import type { BaseBrowserSafeUser, DangerousBaseUser } from '@/types'
+import type { BaseBrowserSafeUser, BrowserSafeMerchantProfile, DangerousBaseUser } from '@/types'
 
 export function sanitiseDangerousBaseUser(user: DangerousBaseUser): BaseBrowserSafeUser {
 	const { id, hashedPassword, ...safeUser } = user
+	return safeUser
+}
+
+export function sanitiseMerchant(merchant: DangerousBaseUser): BrowserSafeMerchantProfile {
+	const { id, hashedPassword, email, firstName, lastName, emailConfirmed, ...safeUser } = merchant
 	return safeUser
 }
