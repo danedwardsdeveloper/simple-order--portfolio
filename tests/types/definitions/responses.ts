3f45538d5f2b467f54a2d7056c755f4ea9d2e337
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-06 23:40:37 +0100

    Move and rename file

---
+++ tests/types/definitions/responses.ts
@@ -0,0 +1,12 @@
+import type { ParsedSetCookie } from '@/types'
+import type { RequestInit, Response } from 'node-fetch'
+
+export type NodeFetchResponse = Response
+export type NodeRequestInit = RequestInit
+
+export interface TestRequestResponse {
+	response: NodeFetchResponse
+	setCookie: ParsedSetCookie | null
+}
+
+export type JsonData = { [key: string]: unknown }
