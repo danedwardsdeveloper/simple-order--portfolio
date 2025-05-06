0e0cb3deb9c937a33ab4eddb3f3b83816cfe3297
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-06 23:49:38 +0100

    Use UTC time

--- src/library/utilities/public/definitions/createFreeTrialEndTime/index.ts
+++ src/library/utilities/public/definitions/createFreeTrialEndTime/index.ts
@@ -5,13 +5,13 @@ import { toZonedTime } from 'date-fns-tz'
  * @returns a Date at least 30 days in the future, with the time set to 11.59pm (and 59 seconds) in UK time
  */
 export function createFreeTrialEndTime(): Date {
-	const now = new Date()
-	const thirtyDays = addDays(now, 30)
+	const nowUTC = new Date().setUTCHours(0, 0, 0, 0)
+	const thirtyDays = addDays(nowUTC, 30)
 
 	const ukTime = toZonedTime(thirtyDays, 'Europe/London')
 	const endOfDay = setSeconds(setMinutes(setHours(ukTime, 23), 59), 59)
 
-	if (endOfDay.getTime() - now.getTime() < 30 * 24 * 60 * 60 * 1000) {
+	if (endOfDay.getTime() - nowUTC < 30 * 24 * 60 * 60 * 1000) {
 		return addDays(endOfDay, 1)
 	}
 
