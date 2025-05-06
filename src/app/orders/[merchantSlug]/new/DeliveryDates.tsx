808021a5a5a982fa3a5b9b071c839b9c24863486
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-06 23:53:45 +0100

    Create DeliveryDates.tsx

---
+++ src/app/orders/[merchantSlug]/new/DeliveryDates.tsx
@@ -0,0 +1,37 @@
+import { format, toZonedTime } from 'date-fns-tz'
+
+export default function DeliveryDates({ availableDeliveryDays }: { availableDeliveryDays: Date[] | null }) {
+	if (!availableDeliveryDays) return null
+
+	return (
+		<fieldset aria-label="Pricing plans" className="relative -space-y-px rounded-md bg-white">
+			{availableDeliveryDays.slice(0, 5).map((date) => {
+				const stringDate = String(date)
+				return (
+					<label
+						key={stringDate}
+						className="group flex cursor-pointer flex-col border border-zinc-200 p-4 first:rounded-tl-md first:rounded-tr-md last:rounded-bl-md last:rounded-br-md focus:outline-none has-[:checked]:relative has-[:checked]:border-blue-200 has-[:checked]:bg-blue-50"
+					>
+						<span className="flex items-center gap-3 text-sm">
+							<input
+								defaultValue={stringDate}
+								defaultChecked={true}
+								name="pricing-plan"
+								type="radio"
+								className="relative size-4 appearance-none rounded-full border border-gray-300 bg-white before:absolute before:inset-1 before:rounded-full before:bg-white checked:border-blue-600 checked:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden [&:not(:checked)]:before:hidden shrink-0"
+							/>
+							<span className="font-medium text-gray-900 group-has-[:checked]:text-blue-900 w-full">
+								{(() => {
+									const timeZone = 'Europe/London'
+									const zonedDate = toZonedTime(date, timeZone)
+
+									return <>{format(zonedDate, 'EEEE, d MMMM', { timeZone })}</>
+								})()}
+							</span>
+						</span>
+					</label>
+				)
+			})}
+		</fieldset>
+	)
+}
