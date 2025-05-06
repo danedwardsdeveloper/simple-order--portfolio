30a59bda80e9f29ec02128f76878752aad00fc83
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-06 23:41:19 +0100

    Create anonymousProducts.ts

---
+++ tests/constants/anonymousProducts.ts
@@ -0,0 +1,8 @@
+import type { AnonymousProduct } from '@/types'
+
+export const strawberryJam: AnonymousProduct = {
+	name: 'Strawberry jam',
+	priceInMinorUnits: 213,
+	description: 'A delicious homemade conserve made',
+	customVat: 15,
+}
