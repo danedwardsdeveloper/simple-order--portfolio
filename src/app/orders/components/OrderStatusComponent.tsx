e88d743f193b1a9c055ee97afa35093926a18bc4
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-06 23:52:22 +0100

    Refactor with orderStatusId

--- src/app/orders/components/OrderStatusComponent.tsx
+++ src/app/orders/components/OrderStatusComponent.tsx
@@ -1,7 +1,16 @@
-import { capitaliseFirstLetter } from '@/library/utilities/public'
-import type { OrderStatus } from '@/types'
+import type { OrderStatusName } from '@/types'
 
-export default function OrderStatusComponent({ orderStatus }: { orderStatus: OrderStatus }) {
-	const colourClass = orderStatus === 'completed' ? 'text-green-600' : orderStatus === 'pending' ? 'text-orange-600' : 'text-red-600'
-	return <span className={colourClass}>{capitaliseFirstLetter(orderStatus)}</span>
+export default function OrderStatusComponent({ orderStatusName }: { orderStatusName: OrderStatusName }) {
+	const colourClass = (() => {
+		switch (orderStatusName) {
+			case 'Cancelled':
+				return 'text-red-600'
+			case 'Completed':
+				return 'text-green-600'
+			default: // 'Pending'
+				return 'text-orange-600'
+		}
+	})()
+
+	return <span className={colourClass}>{orderStatusName}</span>
 }
