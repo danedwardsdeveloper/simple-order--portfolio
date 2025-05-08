97184a1417a0e16c5ea773f0f8f5a9eae0fd7b08
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-08 10:11:14 +0100

    Update WelcomeMessages.tsx

--- src/app/dashboard/components/WelcomeMessages.tsx
+++ src/app/dashboard/components/WelcomeMessages.tsx
@@ -1,40 +1,35 @@
-"use client";
-import MessageContainer from "@/components/MessageContainer";
-import PleaseConfirmYourEmailMessage from "@/components/PleaseConfirmYourEmailMessage";
-import { useUser } from "@/components/providers/user";
-import { capitaliseFirstLetter } from "@/library/utilities/public";
-import Link from "next/link";
+'use client'
+import MessageContainer from '@/components/MessageContainer'
+import PleaseConfirmYourEmailMessage from '@/components/PleaseConfirmYourEmailMessage'
+import { useUser } from '@/components/providers/user'
+import { capitaliseFirstLetter } from '@/library/utilities/public'
+import Link from 'next/link'
 
 export default function WelcomeMessages() {
-	const { user, inventory, confirmedCustomers, invitationsSent } = useUser();
-	if (!user) return null;
+	const { user, inventory, confirmedCustomers, invitationsSent } = useUser()
+	if (!user) return null
 
 	const messageTypes = {
-		roleNotice: "role notice",
-		pleaseConfirmYourEmail: "please confirm your email",
-		emptyInventory: "empty inventory",
-		sendFirstInvitation: "send first invitation",
-		confirmBeforeSendingFirstInvitation:
-			"confirm before sending first invitation",
-		trialSubscriptionExpired: "trial or subscription expired",
-	};
+		roleNotice: 'role notice',
+		pleaseConfirmYourEmail: 'please confirm your email',
+		emptyInventory: 'empty inventory',
+		sendFirstInvitation: 'send first invitation',
+		confirmBeforeSendingFirstInvitation: 'confirm before sending first invitation',
+		trialSubscriptionExpired: 'trial or subscription expired',
+	}
 
-	const notJustACustomer = user.roles !== "customer";
-	const emailConfirmed = user.emailConfirmed;
-	const noInvitedOrConfirmedCustomers = !confirmedCustomers && !invitationsSent;
+	const notJustACustomer = user.roles !== 'customer'
+	const emailConfirmed = user.emailConfirmed
+	const noInvitedOrConfirmedCustomers = !confirmedCustomers && !invitationsSent
 
 	const showMessage = {
 		[messageTypes.roleNotice]: true,
 		[messageTypes.pleaseConfirmYourEmail]: !emailConfirmed,
 		[messageTypes.emptyInventory]: notJustACustomer && !inventory,
-		[messageTypes.sendFirstInvitation]:
-			notJustACustomer && emailConfirmed && noInvitedOrConfirmedCustomers,
-		[messageTypes.confirmBeforeSendingFirstInvitation]:
-			notJustACustomer && !emailConfirmed && noInvitedOrConfirmedCustomers,
-		[messageTypes.trialSubscriptionExpired]:
-			(notJustACustomer && !user.trialEnd) ||
-			(notJustACustomer && !user.subscriptionEnd),
-	};
+		[messageTypes.sendFirstInvitation]: notJustACustomer && emailConfirmed && noInvitedOrConfirmedCustomers,
+		[messageTypes.confirmBeforeSendingFirstInvitation]: notJustACustomer && !emailConfirmed && noInvitedOrConfirmedCustomers,
+		[messageTypes.trialSubscriptionExpired]: (notJustACustomer && !user.trialEnd) || (notJustACustomer && !user.subscriptionEnd),
+	}
 
 	function TrialExpired() {
 		return (
@@ -42,7 +37,7 @@ export default function WelcomeMessages() {
 				<h2 className="mb-2">Trial / subscription expired</h2>
 				<p>Please subscribe to continue using Simple Order</p>
 			</MessageContainer>
-		);
+		)
 	}
 
 	function EmptyInventoryMessage() {
@@ -52,7 +47,7 @@ export default function WelcomeMessages() {
 					Add your first product
 				</Link>
 			</MessageContainer>
-		);
+		)
 	}
 
 	function SendFirstInvitation() {
@@ -62,17 +57,15 @@ export default function WelcomeMessages() {
 					Invite your first customer
 				</Link>
 			</MessageContainer>
-		);
+		)
 	}
 
 	function ConfirmBeforeInviting() {
 		return (
 			<MessageContainer borderColour="border-orange-300">
-				<p>
-					{"You must confirm your email before inviting your first customer"}
-				</p>
+				<p>{'You must confirm your email before inviting your first customer'}</p>
 			</MessageContainer>
-		);
+		)
 	}
 
 	return (
@@ -88,17 +81,13 @@ export default function WelcomeMessages() {
 
 			{showMessage[messageTypes.trialSubscriptionExpired] && <TrialExpired />}
 
-			{showMessage[messageTypes.pleaseConfirmYourEmail] && (
-				<PleaseConfirmYourEmailMessage email={user.email} />
-			)}
+			{showMessage[messageTypes.pleaseConfirmYourEmail] && <PleaseConfirmYourEmailMessage email={user.email} />}
 
-			{showMessage[messageTypes.confirmBeforeSendingFirstInvitation] && (
-				<ConfirmBeforeInviting />
-			)}
+			{showMessage[messageTypes.confirmBeforeSendingFirstInvitation] && <ConfirmBeforeInviting />}
 
 			{showMessage[messageTypes.sendFirstInvitation] && <SendFirstInvitation />}
 
 			{showMessage[messageTypes.emptyInventory] && <EmptyInventoryMessage />}
 		</>
-	);
+	)
 }
