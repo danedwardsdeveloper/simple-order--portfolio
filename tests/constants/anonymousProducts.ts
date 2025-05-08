d51601c9325f6c275f95cdd510ae79227d839f9e
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-08 14:32:37 +0100

    Add gingerBeer

--- tests/constants/anonymousProducts.ts
+++ tests/constants/anonymousProducts.ts
@@ -2,7 +2,16 @@ import type { AnonymousProduct } from '@/types'
 
 export const strawberryJam: AnonymousProduct = {
 	name: 'Strawberry jam',
-	priceInMinorUnits: 213,
 	description: 'A delicious homemade conserve made',
+	priceInMinorUnits: 213,
 	customVat: 15,
+}
+
+export const gingerBeer: AnonymousProduct = {
+	id: 1,
+	name: 'Ginger beer',
+	description: 'A spicy fizzy drink',
+	priceInMinorUnits: 86,
+	customVat: 20,
+	deletedAt: null,
 }
