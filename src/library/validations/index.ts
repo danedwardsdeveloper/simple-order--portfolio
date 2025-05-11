208d28b96be22c305b4fa09ac5674e0bd542ab1c
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-11 10:20:03 +0100

    Update index.ts

--- src/library/validations/index.ts
+++ src/library/validations/index.ts
@@ -1,4 +1,6 @@
 export * from './definitions/user'
 export * from './definitions/settings'
+export * from './definitions/holidays'
+export * from './definitions/deliveryDays'
 
 // Do not export /time or will cause a treeshaking issue that breaks Drizzle studio
