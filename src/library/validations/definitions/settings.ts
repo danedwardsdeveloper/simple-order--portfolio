d2f32ba0057003ad131affdd3e2c981b00ba9770
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-11 10:20:14 +0100

    Create settings schemas

--- src/library/validations/definitions/settings.ts
+++ src/library/validations/definitions/settings.ts
@@ -1,12 +1,18 @@
 import { z } from 'zod'
+import { acceptedDeliveryDaysFormValidator } from './deliveryDays'
+import { holidaysFormValidator } from './holidays'
+
+const wholeNumberValidator = z.number().int('Must be a whole number').nonnegative('Cannot be negative')
+
+export const cutOffTimeValidator = z.string().transform((str) => new Date(str))
+export const leadTimeDaysValidator = wholeNumberValidator.describe('Lead time must be a whole number and cannot be negative')
+export const minimumSpendValidator = wholeNumberValidator.describe('Minimum spend must be a whole number and cannot be negative')
 
 export const settingsSchema = z
 	.object({
-		cutOffTime: z
-			.string()
-			.transform((str) => new Date(str))
-			.optional(),
-		leadTimeDays: z.number().int('Lead time must be a whole number').nonnegative('Lead time cannot be negative').default(0).optional(),
+		cutOffTime: cutOffTimeValidator.optional(),
+		leadTimeDays: leadTimeDaysValidator.optional(),
+		minimumSpendPence: minimumSpendValidator.optional(),
 	})
 	.refine(
 		(data) => {
@@ -17,14 +23,32 @@ export const settingsSchema = z
 		},
 	)
 
-export const cutoffSettingsFormSchema = z.object({
-	cutOffTime: z
-		.string()
-		.transform((str) => (str ? new Date(`1970-01-01T${str}:00`) : null))
-		.optional()
-		.nullable(),
-	leadTimeDays: z.coerce.number().int('Must be a whole number').nonnegative('Cannot be negative').default(0).optional().nullable(),
+export const cutOffTimeFormValidator = z
+	.string()
+	.transform((str) => (str ? new Date(`1970-01-01T${str}:00`) : null))
+	.optional()
+	.nullable()
+
+export const leadTimeDaysFormValidator = z.coerce
+	.number()
+	.int('Must be a whole number')
+	.nonnegative('Cannot be negative')
+	.optional()
+	.nullable()
+
+export const minimumSpendFormValidator = z.coerce
+	.number()
+	.int('Must be a whole number')
+	.nonnegative('Cannot be negative')
+	.optional()
+	.nullable()
+
+export const settingsFormSchema = z.object({
+	cutOffTime: cutOffTimeFormValidator,
+	leadTimeDays: leadTimeDaysFormValidator,
+	minimumSpendPence: minimumSpendFormValidator,
+	acceptedDeliveryDays: acceptedDeliveryDaysFormValidator,
+	holidays: holidaysFormValidator,
 })
 
-export type Settings = z.infer<typeof settingsSchema>
-export type SettingsForm = z.infer<typeof cutoffSettingsFormSchema>
+export type SettingsForm = z.infer<typeof settingsFormSchema>
