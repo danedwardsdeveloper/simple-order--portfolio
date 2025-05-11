6818ad38bc6a2ab344dd2c13a8d61828286d336e
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-11 10:30:32 +0100

    Make wider

--- src/app/settings/components/SignOutButton.tsx
+++ src/app/settings/components/SignOutButton.tsx
@@ -44,7 +44,7 @@ export default function SignOutButton() {
 			type="button"
 			data-test-id={dataTestIdNames.account.signOutButton}
 			onClick={handleSignOut}
-			className="button-secondary max-w-xs"
+			className="button-secondary w-full max-w-md"
 		>
 			Sign out
 		</button>
