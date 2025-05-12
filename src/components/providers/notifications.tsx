e2186dcc53f4700b63a638e70475a8a3a5dcaf44
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-12 15:45:54 +0100

    Add function exports

--- src/components/providers/notifications.tsx
+++ src/components/providers/notifications.tsx
@@ -1,16 +1,84 @@
 'use client'
+import { userMessages } from '@/library/constants'
 import type { NewNotification, NotificationInterface } from '@/types'
 import { type ReactNode, createContext, useCallback, useContext, useState } from 'react'
 
 interface NotificationsContextType {
 	createNotification: ({ title, message }: NewNotification) => void
+	successNotification: (message: string) => void
+	errorNotification: (message: string) => void
+	serverErrorNotification: () => void
 	removeNotification: (id: number) => void
 	notifications: NotificationInterface[] | null
 }
 
 const NotificationsContext = createContext<NotificationsContextType | undefined>(undefined)
 
-const _exampleNotifications: NotificationInterface[] = [
+export function NotificationsProvider({ children }: { children: ReactNode }) {
+	const [notifications, setNotifications] = useState<NotificationInterface[] | null>(null)
+
+	function createNotification({ title, message, level }: NewNotification): void {
+		const id = Date.now()
+		setNotifications((prev) => {
+			if (!prev) return [{ id, title, message, level }]
+			return [...prev, { id, title, message, level }]
+		})
+	}
+
+	const removeNotification = useCallback((id: number) => {
+		setNotifications((prev) => (prev ? prev.filter((notification) => notification.id !== id) : null))
+	}, [])
+
+	function successNotification(message: string) {
+		createNotification({
+			title: 'Success',
+			level: 'success',
+			message,
+		})
+	}
+
+	function errorNotification(message: string) {
+		createNotification({
+			title: 'Error',
+			level: 'error',
+			message,
+		})
+	}
+
+	function serverErrorNotification() {
+		createNotification({
+			title: 'Error',
+			level: 'error',
+			message: userMessages.serverError,
+		})
+	}
+
+	return (
+		<NotificationsContext.Provider
+			value={{
+				notifications,
+				createNotification,
+				successNotification,
+				errorNotification,
+				serverErrorNotification,
+				removeNotification,
+			}}
+		>
+			{children}
+		</NotificationsContext.Provider>
+	)
+}
+
+export function useNotifications() {
+	const context = useContext(NotificationsContext)
+	if (context === undefined) {
+		throw new Error('useNotifications must be used within a NotificationsProvider')
+	}
+	return context
+}
+
+/*
+const exampleNotifications: NotificationInterface[] = [
 	{
 		id: 0,
 		title: 'Congratulations!',
@@ -36,33 +104,4 @@ const _exampleNotifications: NotificationInterface[] = [
 		level: 'error',
 	},
 ]
-
-export function NotificationsProvider({ children }: { children: ReactNode }) {
-	const [notifications, setNotifications] = useState<NotificationInterface[] | null>(null)
-
-	function createNotification({ title, message, level }: NewNotification): void {
-		const id = Date.now()
-		setNotifications((prev) => {
-			if (!prev) return [{ id, title, message, level }]
-			return [...prev, { id, title, message, level }]
-		})
-	}
-
-	const removeNotification = useCallback((id: number) => {
-		setNotifications((prev) => (prev ? prev.filter((notification) => notification.id !== id) : null))
-	}, [])
-
-	return (
-		<NotificationsContext.Provider value={{ notifications, createNotification, removeNotification }}>
-			{children}
-		</NotificationsContext.Provider>
-	)
-}
-
-export function useNotifications() {
-	const context = useContext(NotificationsContext)
-	if (context === undefined) {
-		throw new Error('useNotifications must be used within a NotificationsProvider')
-	}
-	return context
-}
+*/
