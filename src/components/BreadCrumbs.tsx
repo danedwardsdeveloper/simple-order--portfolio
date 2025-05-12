d2769992ef9f6bbfa62d61a91d395bebb82078f1
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-12 15:46:03 +0100

    Responsive style updates

--- src/components/BreadCrumbs.tsx
+++ src/components/BreadCrumbs.tsx
@@ -17,28 +17,28 @@ interface SignedOutProps {
 export function SignedOutBreadCrumbs({ trail, currentPageTitle }: SignedOutProps) {
 	return (
 		<nav aria-label="Breadcrumb" className="flex mb-12">
-			<ol className="flex items-center space-x-4">
+			<ol className="flex flex-wrap items-center gap-4">
 				<li>
 					<div className="flex items-center">
 						<Link href="/" className="link-primary">
 							Home
 						</Link>
+						<ChevronRightIcon aria-hidden="true" className="size-5 shrink-0 text-zinc-400" />
 					</div>
 				</li>
 				{trail?.map((item) => (
 					<li key={item.href}>
 						<div className="flex items-center">
-							<ChevronRightIcon aria-hidden="true" className="size-5 shrink-0 text-zinc-400" />
-							<Link href={item.href} className="link-primary ml-4">
+							<Link href={item.href} className="link-primary">
 								{item.displayName}
 							</Link>
+							<ChevronRightIcon aria-hidden="true" className="size-5 shrink-0 text-zinc-400" />
 						</div>
 					</li>
 				))}
 				<li>
 					<div className="flex items-center">
-						<ChevronRightIcon aria-hidden="true" className="size-5 shrink-0 text-zinc-400" />
-						<span aria-current="page" className="ml-4 font-medium text-blue-600">
+						<span aria-current="page" className="font-medium text-blue-600">
 							{currentPageTitle}
 						</span>
 					</div>
@@ -57,7 +57,7 @@ interface SignedInProps {
 export function SignedInBreadCrumbs({ businessName, trail, currentPageTitle }: SignedInProps) {
 	return (
 		<nav aria-label="Breadcrumb" className="flex mb-12">
-			<ol className="flex items-center space-x-4">
+			<ol className="flex flex-wrap items-center gap-4">
 				<li>
 					<div className="flex items-center">
 						{/* ToDo: This should not be a link if it's the only item */}
