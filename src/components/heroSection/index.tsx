bc8e89c71a6cffc17f9f265e78b357de21f066cd
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-15 09:19:28 +0100

    Update import path

--- src/components/heroSection/index.tsx
+++ src/components/heroSection/index.tsx
@@ -1,5 +1,5 @@
 import { websiteCopy } from '@/library/constants'
-import { imagesCollection } from '@/library/images'
+import { imagesCollection } from '@/library/imagesCollection'
 import Image from 'next/image'
 import Link from 'next/link'
 
