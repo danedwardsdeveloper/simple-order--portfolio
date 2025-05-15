ddc3d5e707fd54d1db039e92053f65d2e3b4bed0
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-15 21:24:21 +0100

    Add params

--- src/library/utilities/public/definitions/delay.ts
+++ src/library/utilities/public/definitions/delay.ts
@@ -1,6 +1,4 @@
-export function subtleDelay(): Promise<void> {
-	const minDelay = 500
-	const maxDelay = 1500
+export function subtleDelay(minDelay = 500, maxDelay = 1500): Promise<void> {
 	const randomDelay = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay
 
 	return new Promise((resolve) => {
