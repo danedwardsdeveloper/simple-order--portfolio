dfaa4a7a8d9711ad260a121a0b1714af8f6ab73f
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-15 21:27:35 +0100

    Remove loading state

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,24 +1,18 @@
 'use client'
 import { SignedInBreadCrumbs } from '@/components/BreadCrumbs'
-import ContentSplash from '@/components/ContentSplash'
 import UnauthorisedLinks from '@/components/UnauthorisedLinks'
 import { useUser } from '@/components/providers/user'
 import WelcomeMessages from './components/WelcomeMessages'
 
 export default function DashboardPage() {
-	const { user, isLoading } = useUser()
+	const { user } = useUser()
+	if (!user) return <UnauthorisedLinks />
 
-	if (isLoading) return <ContentSplash />
-
-	if (!isLoading && !user) return <UnauthorisedLinks />
-
-	if (user) {
-		return (
-			<>
-				<SignedInBreadCrumbs businessName={user.businessName} />
-				<h1>Dashboard</h1>
-				<WelcomeMessages />
-			</>
-		)
-	}
+	return (
+		<>
+			<SignedInBreadCrumbs businessName={user.businessName} />
+			<h1>Dashboard</h1>
+			<WelcomeMessages />
+		</>
+	)
 }
