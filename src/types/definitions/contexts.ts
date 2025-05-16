999d3e53df68b228390cd662feca964107ea7e6d
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-16 13:11:16 +0100

    Create contexts.ts

---
+++ src/types/definitions/contexts.ts
@@ -0,0 +1,20 @@
+import type { Holiday } from './deliveryDays'
+import type { WeekDayIndex } from './timeDate'
+
+export type SaveSetting<T1, T2 = void> = (value: T1, valueTwo?: T2) => Promise<boolean>
+
+/**
+ * For demo and application providers
+ */
+export type SettingsContextType = {
+	// Settings data that isn't on the user object and has to be retrieved separately
+	holidays: Holiday[] | null
+	acceptedWeekDayIndices: WeekDayIndex[] | null
+
+	// Data saving functions
+	saveCutOffTime: SaveSetting<Date>
+	saveLeadTime: SaveSetting<number>
+	saveMinimumSpendPence: SaveSetting<number>
+	addHoliday: SaveSetting<Date, Date>
+	saveDeliveryDays: SaveSetting<number[]>
+}
