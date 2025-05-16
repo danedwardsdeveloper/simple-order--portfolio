6c9aa9109c02cadc5cf0d989ed144b2bd1e7fed1
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-16 12:30:14 +0100

    Remove barrel, add merchant prop

--- src/app/settings/components/MerchantSettings.tsx
+++ src/app/settings/components/MerchantSettings.tsx
@@ -1,25 +1,22 @@
 'use client'
-import UnauthorisedLinks from '@/components/UnauthorisedLinks'
 import { useMerchantSettings } from '@/components/providers/settings'
-import { useUser } from '@/components/providers/user'
 import CutOffTime from '@/components/settings/CutOffTime'
 import DeliveryDaysSetting from '@/components/settings/DeliveryDaysSetting'
 import LeadTime from '@/components/settings/LeadTime'
 import MinimumSpend from '@/components/settings/MinimumSpend'
+import type { BrowserSafeCompositeUser } from '@/types'
 
-export default function MerchantSettings() {
-	const { user } = useUser()
+export default function MerchantSettings({ merchant }: { merchant: BrowserSafeCompositeUser }) {
 	const { saveMinimumSpendPence, acceptedWeekDayIndices, updateDeliveryDays, saveCutOffTime, saveLeadTime } = useMerchantSettings()
-
-	if (!user) return <UnauthorisedLinks />
+	const { leadTimeDays, cutOffTime, minimumSpendPence } = merchant
 
 	return (
 		<div className="w-full max-w-md border-2 border-slate-100 rounded-xl p-3 flex flex-col gap-y-6">
-			<MinimumSpend minimumSpendPence={user.minimumSpendPence} saveMinimumSpendPence={saveMinimumSpendPence} />
+			<MinimumSpend minimumSpendPence={minimumSpendPence} saveMinimumSpendPence={saveMinimumSpendPence} />
 
-			<LeadTime leadTimeDays={user.leadTimeDays} saveLeadTime={saveLeadTime} />
+			<LeadTime leadTimeDays={leadTimeDays} saveLeadTime={saveLeadTime} />
 
-			<CutOffTime cutOffTime={user.cutOffTime} saveCutOffTime={saveCutOffTime} />
+			<CutOffTime cutOffTime={cutOffTime} saveCutOffTime={saveCutOffTime} />
 
 			<DeliveryDaysSetting acceptedWeekDayIndices={acceptedWeekDayIndices} updateDeliveryDays={updateDeliveryDays} />
 
