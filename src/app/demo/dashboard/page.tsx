eb15e7d431f49dcc83271ab940c9a11ddacbff9f
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-16 11:11:13 +0100

    Add welcome message

--- src/app/demo/dashboard/page.tsx
+++ src/app/demo/dashboard/page.tsx
@@ -1,23 +1,26 @@
 'use client'
 import { SignedOutBreadCrumbs } from '@/components/BreadCrumbs'
-import UnderConstruction from '@/components/UnderConstruction'
+import MessageContainer from '@/components/MessageContainer'
 import { useUi } from '@/components/providers/ui'
 import { useEffect } from 'react'
 
 export default function DemoDashboard() {
-	const { demoMode, setDemoMode } = useUi()
+	const { demoMode, setDemoMode, merchantMode } = useUi()
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies:
 	useEffect(() => {
 		if (!demoMode) setDemoMode(true)
 	}, [])
+
 	return (
 		<>
-			<SignedOutBreadCrumbs trail={[{ displayName: 'Demo', href: '/demo' }]} currentPageTitle="Dashboard" />
-			<div className="">
-				<h1>Dashboard</h1>
-				<UnderConstruction />
-			</div>
+			<SignedOutBreadCrumbs currentPageTitle="Dashboard" />
+			<h1>Dashboard</h1>
+			<MessageContainer borderColour={'border-blue-300'}>
+				Welcome to the Simple Order demo!
+				<br />
+				{`This is the dashboard for ${merchantMode ? 'merchants' : 'customers'}.`}
+			</MessageContainer>
 		</>
 	)
 }
