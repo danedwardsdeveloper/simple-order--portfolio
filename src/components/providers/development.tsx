7548b12a2c8e580aed933461247f34eade16f1fa
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-17 20:16:33 +0100

    Create development.tsx

---
+++ src/components/providers/development.tsx
@@ -0,0 +1,27 @@
+import { type ReactNode, createContext, useContext, useEffect, useState } from 'react'
+
+const DevelopmentProviderContext = createContext<boolean | undefined>(undefined)
+
+export function DevelopmentProvider({ children }: { children: ReactNode }) {
+	const [value, setValue] = useState(true)
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setValue((prev) => !prev)
+		}, 1000)
+
+		return () => clearInterval(timer)
+	}, [])
+
+	return <DevelopmentProviderContext.Provider value={value}>{children}</DevelopmentProviderContext.Provider>
+}
+
+export function alternatingBoolean() {
+	const context = useContext(DevelopmentProviderContext)
+
+	if (context === undefined) {
+		throw new Error('alternatingBoolean must be used within a DevelopmentProvider')
+	}
+
+	return context
+}
