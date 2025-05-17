d6faad33f39be7a3a273d195f870e91697358f29
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-17 20:16:20 +0100

    Create HomePageContent for not-found

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import Pricing from '@/components/Pricing'
 import HeroSection from '@/components/heroSection'
 
-export default function HomePage() {
+/**
+ * Export the entire homepage to use on the 404 page
+ * Never miss an opportunity to explain your brand!
+ */
+export function HomePageContent() {
 	return (
 		<>
 			<HeroSection />
 			<Pricing />
 		</>
 	)
+}
+
+export default function HomePage() {
+	return <HomePageContent />
 }
