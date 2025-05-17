bba07cd45e3a5fe7c9a0d6727f89dc1326f19eea
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-17 12:44:01 +0100

    Move file and make presentational

--- src/components/UserInformation.tsx
+++ src/components/UserInformation.tsx
@@ -1,10 +1,6 @@
-import { useUser } from '@/components/providers/user'
+import type { BrowserSafeCompositeUser } from '@/types'
 
-export default function UserInformation() {
-	const { user } = useUser()
-
-	if (!user) return null
-
+export default function UserInformation({ user }: { user: BrowserSafeCompositeUser }) {
 	return (
 		<div className="flex flex-col gap-y-2 w-full max-w-md border-2 border-slate-100 rounded-xl p-3">
 			<p className="font-medium">{user.businessName}</p>
