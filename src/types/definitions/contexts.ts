628100c88e1d198e34aff7aba139ff3e8deb6559
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-17 12:48:54 +0100

    Add UserContextType & DemoUserContextType

--- src/types/definitions/contexts.ts
+++ src/types/definitions/contexts.ts
@@ -1,5 +1,48 @@
+import type { Dispatch, SetStateAction } from 'react'
 import type { Holiday } from './deliveryDays'
+import type { OrderMade, OrderReceived } from './orders'
+import type { BrowserSafeMerchantProduct } from './products'
 import type { WeekDayIndex } from './timeDate'
+import type {
+	BrowserSafeCompositeUser,
+	BrowserSafeCustomerProfile,
+	BrowserSafeInvitationReceived,
+	BrowserSafeInvitationSent,
+	BrowserSafeMerchantProfile,
+} from './users'
+
+export interface UserContextType {
+	user: BrowserSafeCompositeUser | null
+	setUser: Dispatch<SetStateAction<BrowserSafeCompositeUser | null>>
+
+	inventory: BrowserSafeMerchantProduct[] | null
+	setInventory: Dispatch<SetStateAction<BrowserSafeMerchantProduct[] | null>>
+
+	confirmedMerchants: BrowserSafeMerchantProfile[] | null
+	setConfirmedMerchants: Dispatch<SetStateAction<BrowserSafeMerchantProfile[] | null>>
+
+	confirmedCustomers: BrowserSafeCustomerProfile[] | null
+	setConfirmedCustomers: Dispatch<SetStateAction<BrowserSafeCustomerProfile[] | null>>
+
+	invitationsReceived: BrowserSafeInvitationReceived[] | null
+	setInvitationsReceived: Dispatch<SetStateAction<BrowserSafeInvitationReceived[] | null>>
+
+	invitationsSent: BrowserSafeInvitationSent[] | null
+	setInvitationsSent: Dispatch<SetStateAction<BrowserSafeInvitationSent[] | null>>
+
+	ordersMade: OrderMade[] | null
+	setOrdersMade: Dispatch<SetStateAction<OrderMade[] | null>>
+
+	ordersReceived: OrderReceived[] | null
+	setOrdersReceived: Dispatch<SetStateAction<OrderReceived[] | null>>
+
+	vat: number
+}
+
+export type DemoUserContextType = Omit<UserContextType, 'user' | 'setUser'> & {
+	demoUser: BrowserSafeCompositeUser
+	setDemoUser: Dispatch<SetStateAction<BrowserSafeCompositeUser>>
+}
 
 export type SaveSetting<T1, T2 = void> = (value: T1, valueTwo?: T2) => Promise<boolean>
 
