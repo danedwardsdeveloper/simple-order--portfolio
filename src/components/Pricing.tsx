ea13c4a25c6cc510ce265e5a9a476834869ad653
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-17 12:49:53 +0100

    Update CTA

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -47,7 +47,7 @@ export default function Pricing() {
 								<span className="leading-6 font-semibold tracking-wide text-gray-600">GBP/month</span>
 							</p>
 							<Link href="/free-trial" className="mt-10 block w-full button-primary py-2 text-lg shadow-md">
-								{websiteCopy.CTAs.primary.displayText}
+								{websiteCopy.CTAs.trial.displayText}
 							</Link>
 							<p className="mt-6 text-gray-600">No credit card required</p>
 						</div>
