3f072f8a69c0840ab56c968a20d2e19018138cd7
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-17 12:52:05 +0100

    Delete metadata

--- src/app/demo/layout.tsx
+++ src/app/demo/layout.tsx
@@ -1,13 +1,5 @@
 import PageContainer from '@/components/PageContainer'
-import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
-
-export const metadata: Metadata = {
-	title: 'Demo', // ...template
-	alternates: {
-		canonical: '/demo',
-	},
-}
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return <PageContainer>{children}</PageContainer>
