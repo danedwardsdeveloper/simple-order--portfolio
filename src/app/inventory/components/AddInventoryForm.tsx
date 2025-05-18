c7f250466e6ef2fbec2df599d7556b321e6dbe87
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-18 11:06:10 +0100

    Refactor with Zod and react-hook-form

--- src/app/inventory/components/AddInventoryForm.tsx
+++ src/app/inventory/components/AddInventoryForm.tsx
@@ -1,17 +1,11 @@
 'use client'
-import Spinner from '@/components/Spinner'
+import SubmitButton from '@/components/SubmitButton'
 import { serviceConstraints } from '@/library/constants'
-import { containsIllegalCharacters } from '@/library/utilities/public'
+import { type InventoryAddFormData, inventoryAddFormInputSchema } from '@/library/validations'
 import type { BrowserSafeMerchantProduct } from '@/types'
-import { type FormEvent, useEffect, useState } from 'react'
-
-// ToDo: Use Zod here
-export type InventoryAddFormData = {
-	name: string
-	description: string
-	priceInMinorUnits: string
-	customVat: string
-}
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useState } from 'react'
+import { type FieldError, useForm } from 'react-hook-form'
 
 interface AddInventoryFormProps {
 	inventory: BrowserSafeMerchantProduct[] | null
@@ -21,38 +15,34 @@ interface AddInventoryFormProps {
 }
 
 export default function AddInventoryForm({ inventory, vat, addProduct, isSubmitting }: AddInventoryFormProps) {
-	const initialFormState: InventoryAddFormData = {
-		name: '',
-		description: '',
-		priceInMinorUnits: '',
-		customVat: String(vat),
-	}
-
-	const [formData, setFormData] = useState<InventoryAddFormData>(initialFormState)
 	const [errorMessage, setErrorMessage] = useState('')
-	const [illegalCharacterWarnings, setIllegalCharacterWarnings] = useState<{
-		name: boolean
-		description: boolean
-	}>({ name: false, description: false })
 
-	useEffect(() => {
-		setIllegalCharacterWarnings({
-			name: containsIllegalCharacters(formData.name || ''),
-			description: containsIllegalCharacters(formData.description || ''),
-		})
-	}, [formData.name, formData.description])
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+		reset,
+	} = useForm<InventoryAddFormData>({
+		resolver: zodResolver(inventoryAddFormInputSchema),
+		mode: 'onChange',
+		defaultValues: {
+			name: '',
+			description: '',
+			priceInMinorUnits: '',
+			customVat: String(vat),
+		},
+	})
 
 	if (inventory && inventory?.length > serviceConstraints.maximumProducts) {
 		return null
 	}
 
-	async function handleSubmit(event: FormEvent) {
-		event.preventDefault()
+	async function onSubmit(data: InventoryAddFormData) {
 		setErrorMessage('')
 
-		const success = await addProduct(formData)
+		const success = await addProduct(data)
 		if (success) {
-			setFormData(initialFormState)
+			reset()
 			return
 		}
 
@@ -60,24 +50,27 @@ export default function AddInventoryForm({ inventory, vat, addProduct, isSubmitt
 		setErrorMessage('Failed to add item')
 	}
 
-	const handleInputChange = (field: keyof InventoryAddFormData, value: string) => {
-		setFormData((prev) => ({ ...prev, [field]: value }))
+	function ErrorMessage(props: { error: FieldError | string | undefined }) {
+		const { error } = props
+		if (!error) return null
+
+		return <p className="text-red-600 mt-2">{typeof error === 'string' ? error : error.message}</p>
 	}
 
 	return (
-		<form onSubmit={handleSubmit} className="p-4 border-2 border-zinc-200 rounded-xl flex flex-col gap-y-4 max-w-xl lg:-mx-3">
+		<form onSubmit={handleSubmit(onSubmit)} className="p-4 border-2 border-zinc-200 rounded-xl flex flex-col gap-y-4 max-w-xl lg:-mx-3">
 			<h2>Add an item</h2>
 
 			<div>
 				<div className="mb-1">
 					<label htmlFor="name">Name</label>
-					{illegalCharacterWarnings.name && <span className="text-red-600 mt-2">Only letters, numbers, and {`',.!? -`} are allowed.</span>}
+					<ErrorMessage error={errors.name} />
 				</div>
 				<input
-					id="name"
+					id="name" //
 					type="text"
-					value={formData.name}
-					onChange={(event) => handleInputChange('name', event.target.value)}
+					{...register('name')}
+					aria-invalid={errors.name ? 'true' : 'false'}
 					className="w-full"
 				/>
 			</div>
@@ -85,13 +78,14 @@ export default function AddInventoryForm({ inventory, vat, addProduct, isSubmitt
 			<div>
 				<label htmlFor="description" className="block mb-1">
 					Description
+					<span className="ml-2 text-zinc-500">optional</span>
 				</label>
-				{illegalCharacterWarnings.description && <p className="text-red-600 mt-2">Only letters, numbers, and {`',.!? -`} are allowed.</p>}
+				<ErrorMessage error={errors.description} />
 				<input
-					id="description"
+					id="description" //
 					type="text"
-					value={formData.description}
-					onChange={(event) => handleInputChange('description', event.target.value)}
+					{...register('description')}
+					aria-invalid={errors.description ? 'true' : 'false'}
 					className="w-full"
 				/>
 			</div>
@@ -100,11 +94,12 @@ export default function AddInventoryForm({ inventory, vat, addProduct, isSubmitt
 				<label htmlFor="price" className="block mb-1">
 					Price in pence
 				</label>
+				<ErrorMessage error={errors.priceInMinorUnits} />
 				<input
-					id="priceInMinorUnits"
+					id="priceInMinorUnits" //
 					type="text"
-					value={formData.priceInMinorUnits}
-					onChange={(event) => handleInputChange('priceInMinorUnits', event.target.value)}
+					aria-invalid={errors.priceInMinorUnits ? 'true' : 'false'}
+					{...register('priceInMinorUnits')}
 					className="w-full"
 				/>
 			</div>
@@ -113,21 +108,23 @@ export default function AddInventoryForm({ inventory, vat, addProduct, isSubmitt
 				<label htmlFor="vat" className="block mb-1">
 					VAT percentage
 				</label>
+				<ErrorMessage error={errors.customVat} />
 				<input
-					id="customVat"
-					placeholder="20"
+					id="customVat" //
 					type="text"
-					value={formData.customVat}
-					onChange={(event) => handleInputChange('customVat', event.target.value)}
+					aria-invalid={errors.customVat ? 'true' : 'false'}
+					{...register('customVat')}
 					className="w-full"
 				/>
 			</div>
 
-			{errorMessage && <p className="text-red-600">{errorMessage}</p>}
-			<div className="flex justify-center mt-8 min-h-7">
-				<button type="submit" disabled={isSubmitting} className="button-secondary w-full">
-					<div className="w-full flex items-center justify-center min-h-7">{isSubmitting ? <Spinner /> : 'Add item'}</div>
-				</button>
+			<ErrorMessage error={errorMessage} />
+			<div className="flex justify-center mt-8">
+				<SubmitButton //
+					content="Add item"
+					formReady={isValid}
+					isSubmitting={isSubmitting}
+				/>
 			</div>
 		</form>
 	)
