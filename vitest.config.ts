df4b3c5954e9e6acb9b313ba85941e558bba27b0
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-18 11:04:56 +0100

    Remove comment

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-// import '@testing-library/jest-dom'
 
 export default defineConfig({
 	plugins: [tsconfigPaths(), react()],
