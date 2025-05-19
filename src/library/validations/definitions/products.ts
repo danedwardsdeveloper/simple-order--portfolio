ff2297f467fd19057367bbb332786cbfa3b1bcb2
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-19 16:45:24 +0100

    Add service constraints

--- src/library/validations/definitions/products.ts
+++ src/library/validations/definitions/products.ts
@@ -1,7 +1,13 @@
-// library/validations.ts
 import { serviceConstraints } from '@/library/constants'
+import { formatPrice } from '@/library/utilities/public'
 import { z } from 'zod'
 import { validCharacters } from './validCharacters'
+
+// ToDo List
+// - Change names to addProductFormInputSchema etc
+// Move types to /types
+
+const { maximumProductValueInMinorUnits: maxPrice, maximumProductDescriptionCharacters: maxDescriptionLength } = serviceConstraints
 
 // before transformations
 export const inventoryAddFormInputSchema = z.object({
@@ -9,16 +15,21 @@ export const inventoryAddFormInputSchema = z.object({
 
 	description: z
 		.string()
-		.max(serviceConstraints.maximumProductDescriptionCharacters, {
-			message: `Description must be less than ${serviceConstraints.maximumProductDescriptionCharacters} characters`,
+		.max(maxDescriptionLength, {
+			message: `Description must be less than ${maxDescriptionLength} characters`,
 		})
 		.pipe(validCharacters())
 		.optional()
 		.nullable(),
 
-	priceInMinorUnits: z.string().refine((val) => val === '' || !Number.isNaN(Number(val)), {
-		message: 'Price must be a valid number',
-	}),
+	priceInMinorUnits: z
+		.string()
+		.refine((val) => val === '' || !Number.isNaN(Number(val)), {
+			message: 'Price must be a valid number',
+		})
+		.refine((val) => val === '' || (Number(val) >= 0 && Number(val) <= maxPrice), {
+			message: `Price must be less than ${formatPrice(maxPrice)}`,
+		}),
 
 	customVat: z.string().refine((val) => !Number.isNaN(Number(val)), {
 		message: 'VAT must be a valid number',
