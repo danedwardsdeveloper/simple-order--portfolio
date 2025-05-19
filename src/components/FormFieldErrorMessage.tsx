763cc25aa466a43db1c5adf82a46fbbfbcf43ce4
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-19 17:58:42 +0100

    Update margins

--- src/components/FormFieldErrorMessage.tsx
+++ src/components/FormFieldErrorMessage.tsx
@@ -4,5 +4,5 @@ export default function FormFieldErrorMessage(props: { error: FieldError | strin
 	const { error } = props
 	if (!error) return null
 
-	return <p className="text-red-600 mt-2">{typeof error === 'string' ? error : error.message}</p>
+	return <p className="text-red-600 mb-2">{typeof error === 'string' ? error : error.message}</p>
 }
