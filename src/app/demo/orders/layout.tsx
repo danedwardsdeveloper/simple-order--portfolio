57717206a4452f4fea071e876f4d16960b53d5cc
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-19 11:45:26 +0100

    Update page titles

--- src/app/demo/orders/layout.tsx
+++ src/app/demo/orders/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
 
 export const metadata: Metadata = {
-	title: 'Orders [DEMO]', // + template
+	title: 'Orders DEMO', // + template
 	alternates: {
 		canonical: '/demo/orders',
 	},
