fc9bf58b6128c6efa5cd58fb26b6fbcadcbc0d3b
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-23 08:24:42 +0100

    Rename file, add invitationExpiryDate

--- src/library/utilities/public/definitions/dateTime.ts
+++ src/library/utilities/public/definitions/dateTime.ts
@@ -1,5 +1,10 @@
+import { durationSettings } from '@/library/constants'
 import type { DayNumber, Month, Year } from '@/types'
 
 export function createDate(day: DayNumber, month: Month, year: Year): Date {
 	return new Date(Date.UTC(year, month, day))
+}
+
+export function invitationExpiryDate() {
+	return new Date(Date.now() + durationSettings.acceptInvitationExpiry)
 }
