a47e335d63cc57ea3b18aa8f11fb68afe3cd356d
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-23 08:24:50 +0100

    Add developmentDelay

--- src/library/utilities/public/definitions/subtleDelay.ts
+++ src/library/utilities/public/definitions/subtleDelay.ts
@@ -1,3 +1,5 @@
+import { isDevelopment } from '@/library/environment/publicVariables'
+
 export function subtleDelay(minDelay = 500, maxDelay = 1500): Promise<void> {
 	const randomDelay = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay
 
@@ -6,4 +8,10 @@ export function subtleDelay(minDelay = 500, maxDelay = 1500): Promise<void> {
 			resolve()
 		}, randomDelay)
 	})
+}
+
+export async function developmentDelay() {
+	if (isDevelopment) {
+		await subtleDelay(400, 500)
+	}
 }
