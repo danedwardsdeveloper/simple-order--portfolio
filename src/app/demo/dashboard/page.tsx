086ae663def80d8169a0dc010b3c0ba4127e0f29
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-24 11:25:50 +0100

    Add demoMode to SignedInBreadCrumbs

--- src/app/demo/dashboard/page.tsx
+++ src/app/demo/dashboard/page.tsx
@@ -5,12 +5,14 @@ import { useDemoUser } from '@/components/providers/demo/user'
 import { useUi } from '@/components/providers/ui'
 
 export default function DemoDashboard() {
-	const { merchantMode } = useUi()
+	const { merchantMode, demoMode } = useUi()
 	const { demoUser } = useDemoUser()
+
+	// Somehow ensure merchantMode is true without annoying people exploring the trial...
 
 	return (
 		<>
-			<SignedInBreadCrumbs businessName={demoUser.businessName} currentPageTitle="Dashboard" demoMode />
+			<SignedInBreadCrumbs businessName={demoUser.businessName} currentPageTitle="Dashboard" isDemo={demoMode} />
 			<h1>Dashboard</h1>
 			<MessageContainer borderColour={'border-blue-300'}>
 				Welcome to the Simple Order demo!
