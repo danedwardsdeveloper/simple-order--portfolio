e3242a9e8ebda77b7890a451834b2d1aa6de8ab0
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-24 11:23:15 +0100

    Add default styles

--- src/components/ZebraContainer.tsx
+++ src/components/ZebraContainer.tsx
@@ -3,12 +3,18 @@ import type { ReactNode } from 'react'
 
 interface Props {
 	index: number
-	oddStyles: string
-	evenStyles: string
-	baseStyles: string
+	oddStyles?: string
+	evenStyles?: string
+	baseStyles?: string
 	children: ReactNode
 }
 
-export default function ZebraContainer({ index, oddStyles, evenStyles, baseStyles, children }: Props) {
+export default function ZebraContainer({
+	index,
+	oddStyles = 'bg-blue-50 border-blue-100',
+	evenStyles = 'bg-zinc-50 border-zinc-100',
+	baseStyles = 'flex flex-col gap-y-6 w-full p-4 border-2 rounded-xl',
+	children,
+}: Props) {
 	return <div className={mergeClasses(baseStyles, index % 2 ? oddStyles : evenStyles)}>{children}</div>
 }
