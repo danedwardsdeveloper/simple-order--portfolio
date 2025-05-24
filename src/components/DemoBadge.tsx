9d5da24e70dda48173cb47cb29146aec22cdcdf1
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-24 11:57:49 +0100

    Swap options around

--- src/components/DemoBadge.tsx
+++ src/components/DemoBadge.tsx
@@ -27,18 +27,18 @@ export default function DemoBadge() {
 					<Label as="span" className="mr-3">
 						<button
 							type="button"
-							onClick={() => setMerchantMode(false)}
-							disabled={!merchantMode}
+							onClick={() => setMerchantMode(true)}
+							disabled={merchantMode}
 							className={mergeClasses(
 								'transition-all duration-300 mr-1',
-								!merchantMode ? 'text-white cursor-default' : 'text-zinc-300 hover:text-zinc-200 active:text-zinc-100',
+								merchantMode ? 'text-white cursor-default' : 'text-zinc-300 hover:text-zinc-200 active:text-zinc-100',
 							)}
 						>
-							as customer
+							as merchant
 						</button>
 					</Label>
 					<Switch
-						checked={merchantMode}
+						checked={!merchantMode}
 						onChange={toggleMerchantMode}
 						className="group relative inline-flex h-6 w-11 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-orange-100 focus:ring-offset-2 bg-orange-100"
 					>
@@ -50,14 +50,14 @@ export default function DemoBadge() {
 					<Label as="span" className="ml-3 ">
 						<button
 							type="button"
-							onClick={() => setMerchantMode(true)}
-							disabled={merchantMode}
+							onClick={() => setMerchantMode(false)}
+							disabled={!merchantMode}
 							className={mergeClasses(
 								'transition-all duration-300 mr-1',
-								merchantMode ? 'text-white cursor-default' : 'cursor-pointer text-zinc-300  hover:text-zinc-200 active:text-zinc-100',
+								!merchantMode ? 'text-white cursor-default' : 'cursor-pointer text-zinc-300  hover:text-zinc-200 active:text-zinc-100',
 							)}
 						>
-							as merchant
+							as customer
 						</button>
 					</Label>
 				</Field>
