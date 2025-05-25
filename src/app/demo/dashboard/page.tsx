087b93aea371a2d0967710f075255f0bcc2edaf2
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-25 20:21:47 +0100

    Update copy

--- src/app/demo/dashboard/page.tsx
+++ src/app/demo/dashboard/page.tsx
@@ -2,10 +2,8 @@
 import { SignedInBreadCrumbs } from '@/components/BreadCrumbs'
 import MessageContainer from '@/components/MessageContainer'
 import { useDemoUser } from '@/components/providers/demo/user'
-import { useUi } from '@/components/providers/ui'
 
 export default function DemoDashboard() {
-	const { merchantMode } = useUi()
 	const { resolvedUser } = useDemoUser()
 
 	// Somehow ensure merchantMode is true without annoying people exploring the trial...
@@ -14,11 +12,7 @@ export default function DemoDashboard() {
 		<>
 			<SignedInBreadCrumbs businessName={resolvedUser.businessName} currentPageTitle="Dashboard" isDemo={true} />
 			<h1>Dashboard</h1>
-			<MessageContainer borderColour={'border-blue-300'}>
-				Welcome to the Simple Order demo!
-				<br />
-				{`This is the dashboard for ${merchantMode ? 'merchants' : 'customers'}.`}
-			</MessageContainer>
+			<MessageContainer borderColour={'border-blue-300'}>Welcome to the Simple Order demo!</MessageContainer>
 		</>
 	)
 }
