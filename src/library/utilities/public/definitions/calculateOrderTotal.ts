59e5c9c2e4413235503106dacdd3717c8e6e7cdb
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-25 20:22:56 +0100

    Add maximumOrderValueExceeded

--- src/library/utilities/public/definitions/calculateOrderTotal.ts
+++ src/library/utilities/public/definitions/calculateOrderTotal.ts
@@ -1,11 +1,15 @@
+import { serviceConstraints } from '@/library/constants'
 import type { BrowserOrderItem } from '@/types'
 
+// Add maximum order quantity exceeded
 export function calculateOrderTotal(orderItems: BrowserOrderItem[]): {
 	totalWithVAT: number
 	totalWithoutVAT: number
+	maximumOrderValueExceeded: boolean
 } {
 	let totalWithVAT = 0
 	let totalWithoutVAT = 0
+	let maximumOrderValueExceeded = false
 
 	for (const item of orderItems) {
 		const subtotalWithoutVat = item.priceInMinorUnitsWithoutVat * item.quantity
@@ -17,5 +21,9 @@ export function calculateOrderTotal(orderItems: BrowserOrderItem[]): {
 		totalWithVAT += subtotalWithoutVat + vatAmount
 	}
 
-	return { totalWithVAT, totalWithoutVAT }
+	if (totalWithVAT > serviceConstraints.maximumOrderValueInMinorUnits) {
+		maximumOrderValueExceeded = true
+	}
+
+	return { totalWithVAT, totalWithoutVAT, maximumOrderValueExceeded }
 }
