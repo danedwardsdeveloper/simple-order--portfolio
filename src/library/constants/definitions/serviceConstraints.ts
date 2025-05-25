be8d3bc8f63251e9a2297c28241cc4a9dc25c9aa
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-25 20:22:37 +0100

    Add maximumOrderValueInMinorUnits

--- src/library/constants/definitions/serviceConstraints.ts
+++ src/library/constants/definitions/serviceConstraints.ts
@@ -2,7 +2,8 @@ export const serviceConstraints = {
 	maximumCustomers: 100,
 	maximumProducts: 100,
 	highestVat: 50,
-	maximumProductValueInMinorUnits: 100000,
+	maximumProductValueInMinorUnits: 1_000_00, // £1,000
+	maximumOrderValueInMinorUnits: 10_000_00, // £10,000
 	maximumProductDescriptionCharacters: 500,
 	maximumCustomerNoteLength: 500,
 } as const
