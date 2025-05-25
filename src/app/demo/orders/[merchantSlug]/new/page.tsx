1d22fa45837a4d81884bf3248da5ebf22d3c2c89
Dan Edwards <daniel.edwards96@yahoo.com>
2025-05-25 08:45:45 +0100

    Implement createOrder function

--- src/app/demo/orders/[merchantSlug]/new/page.tsx
+++ src/app/demo/orders/[merchantSlug]/new/page.tsx
@@ -1,33 +1,98 @@
 'use client'
 import type { OrdersPOSTbody } from '@/app/api/orders/post'
-import { type CreateOrderFunction, NewOrderPageContent } from '@/app/orders/[merchantSlug]/new/Content'
+import { type CreateOrderFunction, NewOrderPageContent } from '@/app/orders/[merchantSlug]/new/NewOrderPageContent'
 import type { MerchantSlugParams } from '@/app/orders/[merchantSlug]/page'
+import { useDemoSettings } from '@/components/providers/demo/settings'
 import { useDemoUser } from '@/components/providers/demo/user'
-import { demoInventory, demoOrdersMade } from '@/library/constants/demo'
-import { use } from 'react'
+import { useUi } from '@/components/providers/ui'
+import { userMessages } from '@/library/constants'
+import { demoInventory, demoMerchant, floristInventory } from '@/library/constants/demo'
+import { isProduction } from '@/library/environment/publicVariables'
+import { getAvailableDeliveryDays } from '@/library/utilities/public'
+import type { BrowserOrderItem, OrderMade } from '@/types'
+import { useRouter } from 'next/navigation'
+import { use, useEffect } from 'react'
 
 export default function DemoNewOrderPage({ params }: MerchantSlugParams) {
 	const resolvedParams = use(params)
 	const merchantSlug = resolvedParams.merchantSlug
 
-	const { demoUser, setOrdersMade, confirmedMerchants, vat } = useDemoUser()
+	const { customer, ordersMade, setOrdersMade, setOrdersReceived, confirmedMerchants, vat, inventory } = useDemoUser()
+	const { merchantMode } = useUi()
+	const { acceptedWeekDayIndices, holidays } = useDemoSettings()
+	const router = useRouter()
 
-	const createOrder: CreateOrderFunction = async (_params: OrdersPOSTbody) => {
-		// merchantSlug: params.merchantSlug, products: params.products, requestedDeliveryDate: params.requestedDeliveryDate
-		const createdOrder = demoOrdersMade[0]
+	useEffect(() => {
+		if (merchantMode && isProduction) {
+			router.push('/demo/dashboard')
+		}
+	}, [merchantMode, router])
+
+	// Major ToDo: use the right inventory!
+	// Actually this might be a premature optimisation...
+
+	const createOrder: CreateOrderFunction = async (props: OrdersPOSTbody) => {
+		if (!inventory) return { userMessage: userMessages.serverError }
+
+		const orderId = (ordersMade?.length || 0) + 1
+		const orderItems: BrowserOrderItem[] = props.products.map((selectedProduct) => {
+			const inventoryProduct = inventory.find((item) => item.id === selectedProduct.productId)
+
+			if (!inventoryProduct) {
+				throw new Error(`Product with id ${selectedProduct.productId} not found in inventory`)
+			}
+
+			return {
+				id: inventoryProduct.id,
+				name: inventoryProduct.name,
+				description: inventoryProduct.description,
+				orderId: orderId, // You'll need to provide this
+				productId: selectedProduct.productId,
+				quantity: selectedProduct.quantity,
+				priceInMinorUnitsWithoutVat: inventoryProduct.priceInMinorUnits,
+				vat: inventoryProduct.customVat || vat,
+			}
+		})
+
+		const createdOrder: OrderMade = {
+			id: orderId,
+			statusName: 'Pending',
+			businessName: confirmedMerchants?.find((merchant) => merchant.slug === props.merchantSlug)?.businessName || '',
+			requestedDeliveryDate: props.requestedDeliveryDate,
+			products: orderItems,
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		}
+
+		if (props.merchantSlug === demoMerchant.slug) {
+			setOrdersReceived((prev) => [createdOrder, ...(prev ?? [])])
+		}
 
 		return { createdOrder }
 	}
 
-	const availableDeliveryDays = [new Date()]
+	const availableDeliveryDays = getAvailableDeliveryDays({
+		acceptedWeekDayIndices,
+		holidays,
+		lookAheadDays: 14,
+		cutOffTime: demoMerchant.cutOffTime,
+		leadTimeDays: demoMerchant.leadTimeDays,
+	})
+
+	if (!availableDeliveryDays || availableDeliveryDays.length === 0) {
+		// ToDo...
+		return <h1>No available delivery days</h1>
+	}
+
+	const products = merchantSlug === demoMerchant.slug ? demoInventory : floristInventory
 
 	return (
 		<NewOrderPageContent
 			isDemo={true}
-			user={demoUser}
+			user={customer}
 			merchantSlug={merchantSlug}
 			availableDeliveryDays={availableDeliveryDays}
-			products={demoInventory}
+			products={products}
 			createOrder={createOrder}
 			confirmedMerchants={confirmedMerchants}
 			setOrdersMade={setOrdersMade}
